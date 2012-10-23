0418c41e1ca62063c933264f4c9b268794215c95
Phillip Grice <phillip.grice@gatech.edu>
2012-10-23 13:00:11 -0700

    most of initial testing, with working ft display

--- assistive_teleop/www/js/force_level_widget.js
+++ assistive_teleop/www/js/force_level_widget.js
@@ -1,11 +1,7 @@
-function rezero_wrench(){
-    node.publish('netft_gravity_zeroing/rezero_wrench','std_msgs/Bool',json({'data':true}));
-    log("Sending command to Re-zero Force/Torque Sensor");
-};
-
-var ForceDisplay = function () {
+var ForceDisplay = function (ros) {
     'use strict';
     var forceDisplay = this;
+    forceDisplay.ros = ros;
     forceDisplay.wrench = {};
     forceDisplay.magnitude = function () {
         var x = forceDisplay.wrench.force.x
@@ -22,35 +18,37 @@ var ForceDisplay = function () {
             forceDisplay.stateSubCBList[i](msg);
         }
     };
-    forceDisplay.stateSub = new window.ros.Topic({
+    forceDisplay.stateSub = new forceDisplay.ros.Topic({
         name: 'wt_force_out_throttle',
         messageType: 'geometry_msgs/WrenchStamped'});
     forceDisplay.stateSub.subscribe(function (msg) {
         forceDisplay.stateSubCB(msg);
     });
 
-    forceDisplay.rezeroPub = new window.ros.Topic({
-        name: 'pr2_ft_zeroing/rezero_wrench',
+    forceDisplay.rezeroPub = new forceDisplay.ros.Topic({
+        name: 'netft_gravity_zeroing/rezero_wrench',
         messageType: 'std_msgs/Bool'});
     forceDisplay.rezeroPub.advertise();
     forceDisplay.rezero = function () {
-        var msg = new window.ros.Message({data:true});
+        var msg = new forceDisplay.ros.Message({data:true});
         forceDisplay.rezeroPub.publish(msg);
         console.log('Publishing rezero_wrench msg');
     };
     
-    forceDisplay.activityThresh = new window.ros.Param({
+    forceDisplay.activityThresh = new forceDisplay.ros.Param({
         name: 'face_adls_manager/activity_force_thresh'});
-    forceDisplay.dangerThresh = new window.ros.Param({
+    forceDisplay.dangerThresh = new forceDisplay.ros.Param({
         name: 'face_adls_manager/dangerous_force_thresh'});
 };
 
 var initFTDisplay = function (divId, options) {
-    window.ftDisplay = new ForceDisplay();
+    window.ftDisplay = new ForceDisplay(window.ros);
     var yellowPercent = options.yellowPercent || 50;
     var maxForce = options.maxForce || 15;
-    var height = options.height || '450px';
+    var height = options.height || '480px';
     var width = options.width || '20px';
+    
+    // Produce basic layout and default options
     $('#'+divId).append('<table><tr><td>'+
                         '<table id="'+divId+'FTRefTable">'+
                         '<tr id="'+divId+'FTDangerRef">'+
@@ -76,7 +74,7 @@ var initFTDisplay = function (divId, options) {
     $('#'+divId+'FTColorWrapper').css({'width':width,
                                        'height':'100%',
                                        'background-color':'blue'});
-    $('#'+divId+'FTColorBar').css({'height':'95%',
+    $('#'+divId+'FTColorBar').css({'height':'0%',
                                    'background-color':'white'});
     $('#'+divId+'FTDangerRef').css({'width':width,
                                     'height':'33%',
@@ -87,19 +85,42 @@ var initFTDisplay = function (divId, options) {
     $('#'+divId+'FTNullRef').css({'width':width,
                                   'height':'100%',
                                   'background-color':'gray'});
+    $('#'+divId+'FTDangerLabel').css('align','bottom');
+    $('#'+divId+'FTActivityLabel').css('align','bottom');
     $('#'+divId+'FTTextDisplay').html('## N');
     $('#'+divId+'FTRezeroButton').css('width','70px');
    
+    // Readjust layout based on parameters
+    window.ftDisplay.dangerThresh.get(function (val) {
+        window.ftDisplay.dangerThresh.value = val;
+        console.log('Param: '+ ftDisplay.dangerThresh.name +'\r\n'+
+                    ' Value: ' + val.toString());
+        window.ftDisplay.activityThresh.get(function (val) {
+            window.ftDisplay.activityThresh.value = val;
+            var dangerThr = window.ftDisplay.dangerThresh.value;
+            $('#'+divId+'FTDangerLabel').html(dangerThr.toString()+' N');
+            var dangerPct = 100*(maxForce-dangerThr)/maxForce;
+            $('#'+divId+'FTActivityLabel').html(val.toString()+' N');
+            var actPct = 100*(maxForce - val)/maxForce - dangerPct;
+            $('#'+divId+'FTDangerRef').css('height',dangerPct+'%'); 
+            $('#'+divId+'FTActivityRef').css('height',actPct+'%'); 
+            console.log('Param: '+ ftDisplay.activityThresh.name +'\r\n'+
+                        ' Value: ' + val.toString());
+        });
+    });
+
+    // Update Display based upon published data
     var updateReadout = function (ws) {
-       var pct = (window.ftDisplay.magnitude/maxForce)*100;
+       var mag = window.ftDisplay.magnitude();
+       var pct = (mag/maxForce)*100;
+       if (pct > 100.0) {pct = 100.0};
        $('#'+divId+'FTTextDisplay').html('<p>'+mag.toFixed(1)+' N </p>')
        var g = "FF";
        var r = "FF";
-       if (pct > yellowPercent){
+       if (pct > yellowPercent) {
            g = Math.round(255*(1-(pct-yellowPercent)/(100-yellowPercent))).toString(16);
            if (g.length==1){g="0"+g};
-       };
-       if (pct < yellowPercent){
+       } else if (pct < yellowPercent) {
            r = Math.round(255*(pct/yellowPercent)).toString(16);
            if (r.length==1){r="0"+r};
        };
@@ -107,74 +128,10 @@ var initFTDisplay = function (divId, options) {
        $('#'+divId+'FTColorWrapper').css('background-color', color);
        $('#'+divId+'FTColorBar').css('height', Math.round(100-pct)+'%');
        }; 
-
     window.ftDisplay.stateSubCBList.push(updateReadout);
 
-    window.ftDisplay.activityThresh.get(function (val) {
-        window.ftDisplay.activityThresh.value = val;
-        var dangerPct = (
-        $('#'+divId+'FTDangerRef').css('height',); 
-        $('#'+divId+'FTActivityRef').css('height',); 
-        console.log('Param: '+ ftDisplay.activityThresh.name +'\r\n'+
-                    ' Value: ' + val.toString());
+    $('#'+divId+'FTRezeroButton').click(function () {
+        window.ftDisplay.rezeroPub.publish({data:true});
+        log("Sending command to Re-zero Force/Torque Sensor");
     });
-    
-
 };
-
-          
-//    options:{
-//        yellow_pct:50,
-//        max_force:12
-//    },
-//    _create: function(){
-//        var self = this;
-//        this.element.html(ft_viewer_html);
-//        node.subscribe('/wt_force_out_throttle',
-//                        function(ws){self.element.ft_viewer('refresh',ws)});
-//        this.load_params();
-//    },
-//    load_params: function(){
-//            node.rosjs.callService('/rosbridge/get_param',
-//                                   '["face_adls_manager"]',
-//              function(params){
-//                  var dt = params['dangerous_force_thresh'];
-//                  var at = params['activity_force_thresh'];
-//                  var f_max = $('#ft_view_widget').ft_viewer('option','max_force');
-//                  var danger_pct=((f_max-dt)/f_max)*100;
-//                  var act_pct = ((f_max-at)/f_max)*100-danger_pct;
-//                  $("#ft_ref_danger").height(danger_pct.toString()+'%');
-//                  $("#ft_ref_danger_label").text("Danger\r\n >"+params.dangerous_force_thresh.toString()+" N");
-//                  $("#ft_ref_act").height(act_pct.toString()+'%');
-//                  $("#ft_ref_act_label").text("Activity\r\n  >"+params.activity_force_thresh.toString()+ "N");
-//                  window.get_param_free=true;
-//                  console.log('Force viewer has released lock on get_params');
-//    },
-
-//ft_viewer_html = '<table>
-//                    <tr>
-//                      <td>
-//                            <table border=1 style="height:450px; width:10px">
-//                              <tr id="ft_ref_danger" style="height:33%;background-color:red">\
-//                                <td id="ft_ref_danger_label">[Danger Force Level]</td>\
-//                              </tr>
-//                              <tr id="ft_ref_act" style="height:50%;background-color:green">\
-//                                <td id="ft_ref_act_label">
-//                                [Activity Force Level]
-//                                </td>\
-//                              </tr>
-//                              <tr id="ft_ref_null" style="height:100%;background-color:gray">\
-//                                <td>
-//                                  <div id="ft_readout"></div>
-//                                </td>
-//                              </tr>
-//                            </table>\
-//                      </td>
-//                      <td>
-//                          <div id="ft_bar_wrapper">\
-//                            <div id="ft_bar_value">
-//                            </div>
-//                          </div>
-//                      </td>
-//                    </tr>
-//                  </table>'
