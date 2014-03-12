adf33312096cd4f57baf7acd2652e8c4b237fc63
Ari Kapusta <ari@obie3.(none)>
2014-03-12 11:20:09 -0400

    service now works. client can be used to dummy generate

--- hrl_base_selection/src/base_selection_client.py
+++ hrl_base_selection/src/base_selection_client.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import sys, optparse
 
 import rospy
@@ -6,15 +7,53 @@ import numpy as np
 import math as m
 from hrl_base_selection.srv import *
 import roslib; roslib.load_manifest('hrl_haptic_mpc')
+import hrl_lib.transforms as tr
+import tf
 import rospy
+from helper_functions import createBMatrix
 
 from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped, Twist
 
-def select_base_client(current_loc, goal, head):
+def select_base_client():
+    angle = m.pi
+    pr2_B_head  =  np.matrix([[    m.cos(angle),     -m.sin(angle),                0.,              3.],
+                              [    m.sin(angle),      m.cos(angle),                0.,              .3],
+                              [              0.,                0.,                1.,             1.5],
+                              [              0.,                0.,                0.,              1.]])
+    pos_goal = [pr2_B_head[0,3],pr2_B_head[1,3],pr2_B_head[2,3]]
+    ori_goal = tr.matrix_to_quaternion(pr2_B_head[0:3,0:3])
+    psm_head = PoseStamped()
+    psm_head.header.frame_id = '/base_link'
+    psm_head.pose.position.x=pos_goal[0]
+    psm_head.pose.position.y=pos_goal[1]
+    psm_head.pose.position.z=pos_goal[2]
+    psm_head.pose.orientation.x=ori_goal[0]
+    psm_head.pose.orientation.y=ori_goal[1]
+    psm_head.pose.orientation.z=ori_goal[2]
+    psm_head.pose.orientation.w=ori_goal[3]
+
+    goal_angle = 0
+    pr2_B_goal  =  np.matrix([[    m.cos(goal_angle),     -m.sin(goal_angle),                0.,              2.5],
+                              [    m.sin(goal_angle),      m.cos(goal_angle),                0.,              .2],
+                              [                   0.,                     0.,                1.,             1.5],
+                              [                   0.,                     0.,                0.,              1.]])
+    pos_goal = [pr2_B_goal[0,3],pr2_B_goal[1,3],pr2_B_goal[2,3]]
+    ori_goal = tr.matrix_to_quaternion(pr2_B_goal[0:3,0:3])
+    psm_goal = PoseStamped()
+    psm_goal.header.frame_id = '/base_link'
+    psm_goal.pose.position.x=pos_goal[0]
+    psm_goal.pose.position.y=pos_goal[1]
+    psm_goal.pose.position.z=pos_goal[2]
+    psm_goal.pose.orientation.x=ori_goal[0]
+    psm_goal.pose.orientation.y=ori_goal[1]
+    psm_goal.pose.orientation.z=ori_goal[2]
+    psm_goal.pose.orientation.w=ori_goal[3]
+
+
     rospy.wait_for_service('select_base_position')
     try:
         select_base_position = rospy.ServiceProxy('select_base_position', BaseMove)
-        response = select_base_position(current_loc, goal, head)
+        response = select_base_position(psm_goal, psm_head)
         return response.BaseGoal
     except rospy.ServiceException, e:
         print "Service call failed: %s"%e
@@ -25,12 +64,12 @@ def usage():
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 3:
-        current_loc = PoseStamped(sys.argv[0])
-        goal = PoseStamped(sys.argv[1])
-	head = PoseStamped(sys.argv[2])
-    else:
-        print usage()
-        sys.exit(1)
+    #if len(sys.argv) == 3:
+    #    current_loc = PoseStamped(sys.argv[0])
+    #    goal = PoseStamped(sys.argv[1])
+    #    head = PoseStamped(sys.argv[2])
+    #else:
+    #    print usage()
+    #    sys.exit(1)
     print "Requesting Base Goal Position"
-    print "Base Goal Position is:"%(select_base_client(current_loc, goal, head))
+    print "Base Goal Position is:/n",select_base_client()
--- hrl_base_selection/src/base_selection_service.py
+++ hrl_base_selection/src/base_selection_service.py
@@ -21,6 +21,8 @@ import tf
 
 
 def handle_select_base(req):
+    listener = tf.TransformListener()
+    print 'My given inputs were: \n', req.goal, req.head
     pos_temp = [req.head.pose.position.x,req.head.pose.position.y,req.head.pose.position.z]
     ori_temp = [req.head.pose.orientation.x,req.head.pose.orientation.y,req.head.pose.orientation.z,req.head.pose.orientation.w]
     head = createBMatrix(pos_temp,ori_temp)
@@ -28,9 +30,11 @@ def handle_select_base(req):
     pos_temp = [req.goal.pose.position.x,req.goal.pose.position.y,req.goal.pose.position.z]
     ori_temp = [req.goal.pose.orientation.x,req.goal.pose.orientation.y,req.goal.pose.orientation.z,req.goal.pose.orientation.w]
     goal = createBMatrix(pos_temp,ori_temp)
+    print 'goal',goal
 
     print 'I will move to be able to reach the mouth.'
     env = op.Environment()
+    env.SetViewer('qtcoin')
     env.Load('robots/pr2-beta-static.zae')
     robot = env.GetRobots()[0]
     v = robot.GetActiveDOFValues()
@@ -39,43 +43,47 @@ def handle_select_base(req):
     v[robot.GetJoint('l_gripper_l_finger_joint').GetDOFIndex()] = .54
     v[robot.GetJoint('torso_lift_joint').GetDOFIndex()] = .3
     robot.SetActiveDOFValues(v)
+    robot_start = np.matrix([[m.cos(0.),-m.sin(0.),0.,0],[m.sin(0.),m.cos(0.),0.,0.],[0.,0.,1.,0.],[0.,0.,0.,1.]])
+    robot.SetTransform(np.array(robot_start))
     env.Load('../models/ADA_Wheelchair.dae')
     wheelchair = env.GetBodies()[1]
     wc_angle =  m.pi
-    pr2_B_wc =   np.matrix([[    head[0,0],     head[0,1],               0,    head[0,3]],
-                            [    head[1,0],     head[1,1],               0,    head[1,3]],
-                            [            0,             0,                1,           0],
-                            [            0,             0,                0,           1]])
+    pr2_B_wc =   np.matrix([[     head[0,0],      head[0,1],              0.,      head[0,3]],
+                            [     head[1,0],      head[1,1],              0.,      head[1,3]],
+                            [            0.,             0.,              1.,             0.],
+                            [            0.,             0.,              0.,             1]])
     
-    corner_B_head = np.matrix([[m.cos(0),-m.sin(0),0,.3],[m.sin(0),m.cos(0),0,.385],[0,0,1,0],[0,0,0,1]])
+    corner_B_head = np.matrix([[m.cos(0.),-m.sin(0.),0.,.4],[m.sin(0.),m.cos(0.),0.,.385],[0.,0.,1,0.],[0.,0.,0.,1]])
     wheelchair_location = pr2_B_wc * corner_B_head.I
     wheelchair.SetTransform(np.array(wheelchair_location))
-    for i in [0,.1,.2,-.1,-.2]:
-        for j in [0,.1,.2,-.1,-.2]:
-            for k in [0,m.pi/8,m.pi/4,-m.pi/8,-m.pi/4]:
+    for i in [0.,.1,-.1]:
+        for j in [0.,.1,-.1]:
+            for k in [0.,m.pi/8,-m.pi/8]:
                 #goal_pose = req.goal
                 angle = m.pi
-                head_B_goal =  np.matrix([[    m.cos(angle),     -m.sin(angle),                0,              .1],
-                                          [    m.sin(angle),      m.cos(angle),                0,               0],
-                                          [               0,                 0,                1,               0],
-                                          [               0,                 0,                0,               1]])
-                pr2_B_goal = head * head_B_goal
+                head_B_goal =  np.matrix([[     m.cos(angle),      -m.sin(angle),               0.,               .1],
+                                          [     m.sin(angle),       m.cos(angle),               0.,               0.],
+                                          [               0.,                 0.,               1.,               0.],
+                                          [               0.,                 0.,               0.,               1.]])
+                #pr2_B_goal = head * head_B_goal
+                pr2_B_goal = goal
                 angle_base = m.pi
-                wc_B_goalpr2  =   np.matrix([[    m.cos(angle_base),     -m.sin(angle_base),                0,             .5+i],
-                                             [    m.sin(angle_base),      m.cos(angle_base),                0,             .5+j],
-                                             [                    0,                      0,                1,                0],
-                                             [                    0,                      0,                0,                1]])
+                wc_B_goalpr2  =   np.matrix([[    m.cos(angle_base+k),     -m.sin(angle_base+k),                0.,             .8+i],
+                                             [    m.sin(angle_base+k),      m.cos(angle_base+k),                0.,             .8+j],
+                                             [                     0.,                       0.,                1,                0.],
+                                             [                     0.,                       0.,                0.,                1]])
 
                 base_position = pr2_B_wc * wc_B_goalpr2
-                robot.SetTransform(base_position)
+                print 'base position: /n',base_position
+                robot.SetTransform(np.array(base_position))
 
                 manip = robot.SetActiveManipulator('leftarm_torso')
-                ikmodel = op.databases.inversekinematics.InverseKinematicsModel(self.robot,iktype=op.IkParameterization.Type.Transform6D)
+                ikmodel = op.databases.inversekinematics.InverseKinematicsModel(robot,iktype=op.IkParameterization.Type.Transform6D)
 
                 if not ikmodel.load():
                     ikmodel.autogenerate()
                 with env:
-                    sol = manip.FindIKSolution(pr2_B_goal, op.IkFilterOptions.CheckEnvCollisions)
+                    sol = manip.FindIKSolution(np.array(pr2_B_goal), op.IkFilterOptions.CheckEnvCollisions)
                     if sol != None:
                         (trans,rot) = listener.lookupTransform('/odom_combined', '/base_link', rospy.Time(0))
                         odom_goal = createBMatrix(trans,rot)*base_position
@@ -102,7 +110,7 @@ def handle_select_base(req):
 
 def select_base_server():
     rospy.init_node('select_base_server')
-    listener = tf.TransformListener()
+    
     s = rospy.Service('select_base_position', BaseMove, handle_select_base)
     print "Ready to select base."
     rospy.spin()
