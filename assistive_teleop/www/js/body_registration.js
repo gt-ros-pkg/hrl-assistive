3eb4bb84dbbf632dddcc98bb7384eb3f875d9559
pgrice <Phillip.Grice@gmail.com>
2014-03-18 10:34:16 -0700

    adding side param button to interface as temporary hack

--- assistive_teleop/www/js/body_registration.js
+++ assistive_teleop/www/js/body_registration.js
@@ -2,7 +2,7 @@ var BodyRegistration = function (ros) {
     'use strict';
     var bodyReg = this;
     bodyReg.ros = ros;
-    bodyReg.head_reg_cb = function () {
+    bodyReg.headRegCB = function () {
         $('#img_act_select').val('seedReg');
         window.mjpeg.setCamera('head_registration/confirmation');
         window.log("Click on your check to begin head registration");
@@ -19,18 +19,36 @@ var BodyRegistration = function (ros) {
         });
     };
 
+    bodyReg.faceSideParam = new ros.Param({
+        name: "face_side"
+        });
 
-
-
+    bodyReg.setSideParam = function () {
+        var side = $("input[name=face_side]:checked").val();
+        bodyReg.faceSideParam.set(side);
+        console.log("Setting Param: " + bodyReg.faceSideParam.name + " = " + side);
+    };
 }
 
 var initBodyRegistration = function (tabDivId) {
     window.body_reg = new BodyRegistration(window.ros);
     divRef = "#"+tabDivId;
-    $(divRef).append('<table><tr><td id="'+tabDivId+'_R0C0"></td><td id="'+tabDivId+'_R0C1"></td></tr></table>');
+    $(divRef).append('<table id="' + tabDivId +
+                     '_T0"><tr><td id="' + tabDivId +
+                     '_R0C0"></td><td id="' + tabDivId +
+                     '_R0C1"></td></tr></table>');
+    $(divRef+'_T0').append('<tr><td id="' + tabDivId + '_R1C0"></td></tr>')
     $(divRef+'_R0C0').append('<button class="centered" id="reg_head"> Register Head </button>');
+    $(divRef+'_R1C0').append('<form id="face_side_form">' + 
+                             '<input id="face_radio_left" name="face_side" type="radio" value="left">'+
+                             '<label for="face_radio_left"> Left </label>' +
+                             '<input id="face_radio_right" name="face_side" type="radio" value="right">' +
+                             '<label for="face_radio_right"> Right </label>' +
+                             '</form>');
     $("#reg_head").button();
-    $(divRef+'_R0C0').click(window.body_reg.head_reg_cb);
+    $("#face_side_form").buttonset();
+    $("#face_side_form").change(window.body_reg.setSideParam);
+    $(divRef+'_R0C0').click(window.body_reg.headRegCB);
 
 }
 
