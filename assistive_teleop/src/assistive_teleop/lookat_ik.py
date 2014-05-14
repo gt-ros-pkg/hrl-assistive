d20728826e4ac70783571e13d3060587f5f85e7e
pgrice <phillip.grice@gatech.edu>
2014-05-14 10:24:28 -0400

    first steps to generate ik

--- assistive_teleop/src/assistive_teleop/lookat_ik.py
+++ assistive_teleop/src/assistive_teleop/lookat_ik.py
@@ -5,4 +5,13 @@ import openravepy as op
 
 
 if __name__=='__main__':
-    
+    env = op.Environment()
+    env.SetViewer('qtcoin')
+    env.Load('robots/pr2-beta-static.zae')
+    pr2 = env.GetRobots()[0]
+    pr2.SetActiveManipulator('rightarm_camera')
+    ikmodel = op.databases.inversekinematics.InverseKinematicsModel(pr2, iktype=op.IkParameterization.Type.Lookat3D)
+    if not ikmodel.load():
+        print "Generating new IK Model"
+        ikmodel.autogenerate()
+    print " Model Generated "
