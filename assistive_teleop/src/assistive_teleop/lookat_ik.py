78e57f17bc9fc38a404cda1ba0d390b84caedbf8
pgrice on monty1 <Phillip.Grice@gmail.com>
2014-05-19 10:48:23 -0700

    fixing lookat_ik to generate properly

--- assistive_teleop/src/assistive_teleop/lookat_ik.py
+++ assistive_teleop/src/assistive_teleop/lookat_ik.py
@@ -3,15 +3,23 @@
 import rospy
 import openravepy as op
 
-
 if __name__=='__main__':
     env = op.Environment()
  #   env.SetViewer('qtcoin')
     env.Load('robots/pr2-beta-static.zae')
     pr2 = env.GetRobots()[0]
-    pr2.SetActiveManipulator('rightarm_camera')
-    ikmodel = op.databases.inversekinematics.InverseKinematicsModel(pr2, iktype=op.IkParameterization.Type.Lookat3D)
+    manip = pr2.SetActiveManipulator('rightarm_camera')
+    freeindices = [27,28,30]
+    freejoints = [pr2.GetJoints()[ind].GetName() for ind in freeindices]
+    ikmodel = op.databases.inversekinematics.InverseKinematicsModel(pr2,
+                                                                    iktype=op.IkParameterization.Type.Lookat3D,
+                                                                    forceikfast=True,
+                                                                    freeindices=freeindices,
+                                                                    freejoints=freejoints)
     if not ikmodel.load():
-        print "Generating new IK Model"
-        ikmodel.autogenerate()
-    print " Model Generated "
+        print "Generating Lookat IK Model"
+        ikmodel.generate(freejoints=freejoints)
+        ikmodel.save()
+    else:
+        print "Lookat IK Model Loaded"
+
