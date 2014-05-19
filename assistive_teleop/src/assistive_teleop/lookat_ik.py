ccae943db3af79ee2cdd601e9d950f3e1fd0ed9e
pgrice <phillip.grice@gatech.edu>
2014-05-19 14:27:27 -0400

    working lookat ik example

--- assistive_teleop/src/assistive_teleop/lookat_ik.py
+++ assistive_teleop/src/assistive_teleop/lookat_ik.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 
+import numpy as np
+
 import rospy
 import openravepy as op
 
+class ArmLookatIk(object):
+
+
+
+
 if __name__=='__main__':
     env = op.Environment()
- #   env.SetViewer('qtcoin')
+    env.SetViewer('qtcoin')
     env.Load('robots/pr2-beta-static.zae')
     pr2 = env.GetRobots()[0]
     manip = pr2.SetActiveManipulator('rightarm_camera')
@@ -22,4 +29,14 @@ if __name__=='__main__':
         ikmodel.save()
     else:
         print "Lookat IK Model Loaded"
+
+    T = np.array([1., -0.5, 1.])
+    sol = ikmodel.manip.FindIKSolution(op.IkParameterization(T, op.IkParameterizationType.Lookat3D),
+                                                              op.IkFilterOptions.CheckEnvCollisions)
+    print "Found Solution: " , sol
+    pr2.SetDOFValues(sol, manip.GetArmIndices())
+    raw_input("Press Enter to Stop")
+
+
+
 
