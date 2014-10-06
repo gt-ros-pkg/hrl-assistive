b3dec8a3c1cd375e6e3ce4093d706cdbb455c3a2
dpark <dpark@pidi8.(none)>
2014-10-06 02:21:54 -0400

    modified:   learning_hmm.py

--- hrl_anomaly_detection/src/hrl_anomaly_detection/HMM/learning_hmm.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/HMM/learning_hmm.py
@@ -11,8 +11,8 @@ import random
 
 # Util
 import hrl_lib.util as ut
-import cPickle
-from sklearn.externals import joblib
+## import cPickle
+## from sklearn.externals import joblib
 
 # Matplot
 import matplotlib
@@ -21,6 +21,7 @@ import matplotlib.pyplot as plt
 ## import door_open_data as dod
 import ghmm
 import hrl_anomaly_detection.mechanism_analyse_daehyung as mad
+from scipy.stats import norm
 
 class learning_hmm():
     def __init__(self, data_path, nState, nStep):
@@ -127,21 +128,44 @@ class learning_hmm():
 
     #----------------------------------------------------------------------        
     #
-    def predict(self, X_test):
+    def predict(self, X_test, X_predict):
 
+        # Past profile
         final_ts_obj = ghmm.EmissionSequence(self.F,X_test) # is it neccessary?
 
-        alpha_Z_n = self.ml.forward(final_ts_obj)
-
+        ## print "\nForward"
+        ## logp1 = self.ml.loglikelihood(final_ts_obj)
+        ## print "logp = " + str(logp1) + "\n"
         
-        for x in alpha_Z_n:
-            print np.array(x).shape
-            
-        print len(final_ts_obj)
-        print len(alpha_Z_n)
-        ## path_obj = self.ml.viterbi(final_ts_obj)
+        # alpha: X_test length x #latent States
+        (alpha,scale) = self.ml.forward(final_ts_obj)
+        ## print "alpha: ", np.array(alpha).shape,"\n" + str(alpha) + "\n"
+        ## print "scale = " + str(scale) + "\n"
 
-        return path_obj
+        # beta
+        beta = self.ml.backward(final_ts_obj,scale)
+        ## print "beta", np.array(beta).shape, " = \n " + str(beta) + "\n"
+
+        pred_numerator = 0.0
+        pred_denominator = 0.0
+        for i in xrange(self.nState): # N+1
+
+            total = 0.0        
+            for j in xrange(self.nState): # N                  
+
+                total += self.ml.getTransition(j,i) * alpha[-1][j]
+                
+            (mu, sigma) = self.ml.getEmission(i)
+            
+            pred_numerator += norm(loc=mu,scale=sigma).pdf(X_predict) * total
+            pred_denominator += alpha[-1][i]*beta[-1][i]
+
+
+        for i in xrange(len(alpha)):
+            print X_test[i], alpha[i]
+            
+        return pred_numerator/pred_denominator
+            
 
     #----------------------------------------------------------------------        
     #
@@ -233,11 +257,9 @@ if __name__ == '__main__':
         data['data_mech'] = data_mech
         data['data_chunks'] = data_chunks
         ut.save_pickle(data,pkl_file)
-        
-        
+                
     data_vecs = np.array([data_vecs])
     data_vecs[0] = mad.approx_missing_value(data_vecs[0])
-
     
 
     ######################################################    
@@ -247,8 +269,24 @@ if __name__ == '__main__':
     lh.fit(data_vecs[0])    
     ## lh.path_plot(data_vecs[0], data_vecs[0,:,3])
 
-    x_test = data_vecs[0][1:10,5].tolist()
+    x_test    = data_vecs[0][:30,15].tolist()
 
-    traj = lh.predict(x_test)
+    # Future profile
+    ## future_obsrv = np.arange(0.0, 50.1, 10.0)
+    future_obsrv = [5]
+
+    future_prob = []
+    for x in future_obsrv:           
+        x_predict = x    
+        future_prob.append( lh.predict(x_test, x_predict) )
+        print "--------------"
+        
     ## print x_test
-    ## print traj
+    print x_test[-4:]
+
+    fig = plt.figure(1)
+    ax = fig.add_subplot(111)
+
+    ax.plot(future_obsrv, future_prob)
+    plt.show()
+
