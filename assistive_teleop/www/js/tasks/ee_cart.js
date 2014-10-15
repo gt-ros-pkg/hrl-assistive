53a65cdf1cffb227fe8a11e824f3697dedba004a
Phillip <phillip.grice@gatech.edu>
2014-10-14 17:34:09 -0700

    adding arrow icons and moving toward gripper control

--- assistive_teleop/www/js/tasks/ee_cart.js
+++ assistive_teleop/www/js/tasks/ee_cart.js
@@ -4,11 +4,14 @@ RFH.CartesianEEControl = function (options) {
     self.div = options.div || 'markers';
     self.arm = options.arm;
     self.tfClient = options.tfClient;
+    self.control = new RFH.EECartControlIcon({divId:self.arm.side[0]+'CtrlIcon',
+                                              parentId: self.divId,
+                                              arm: self.arm});
     self.buttonText = self.arm.side[0] === 'r' ? 'Right_Hand' : 'Left_Hand';
     self.buttonClass = 'hand-button';
     self.targetId = self.arm.side[0]+"PosControl";
     self.targetControl = jQuery('<div/>', {id: self.targetId,
-                                           class: ".position-control-outer"}).appendTo('#'+self.div);
+                                           class: "position-control-outer"}).appendTo('#'+self.div);
     jQuery('<div/>', {id: self.arm.side[0]+'PosControlInner',
                       class: ".position-control-inner"}).appendTo('#'+self.targetId);
     self.targetControl.hide();
@@ -45,4 +48,18 @@ RFH.CartesianEEControl = function (options) {
 
     };
     self.arm.stateCBList.push(self.updateTarget);
+}
+
+RFH.EECartControlIcon = function (options) {
+    'use strict';
+    var self = this;
+    self.divId = options.divId;
+    self.parentId = options.parentId;
+    self.arm = options.arm;
+    self.container = $('<div/>', {id: self.divId,
+                                  class: "cart-ctrl-container"}).appendTo('#'+self.parentId);
+    self.away = $('<div/>', {class: "in-out-button"}).appendTo('#'+self.divId).button();
+    self.target = $('<div/>', {class: "target-button"}).appendTo('#'+self.divId);
+    self.toward = $('<div/>', {class: "in-out-button"}).appendTo('#'+self.divId).button();
+    $('#'+self.divId).draggable({containment:"parent", handle: ".target"});
 }
