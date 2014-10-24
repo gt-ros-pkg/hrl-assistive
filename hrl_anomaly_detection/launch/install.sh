9e78b79c53e9c38cba40f4b7e374eaedbdd57660
dpark <dpark@colossus12.(none)>
2014-10-24 00:43:13 -0400

    modified:   install.sh

--- hrl_anomaly_detection/launch/install.sh
+++ hrl_anomaly_detection/launch/install.sh
@@ -9,7 +9,10 @@ sudo python setup.py install
 cd ~/svn
 svn checkout svn://svn.code.sf.net/p/ghmm/code/trunk/ghmm ghmm
 cd ghmm
-autogen.sh
+./autogen.sh
 ./configure
 make 
 sudo make install
+
+cd ~/svn
+svn co https://code.ros.org/svn/ros-pkg/stacks/laser_drivers/trunk/hokuyo_node/
