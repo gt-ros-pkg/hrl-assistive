bad4ae9545f903fe568a48d2335fbac974b2376c
dpark <dpark@brain.(none)>
2014-10-29 11:36:33 -0400

    modified:   learning_hmm.py

--- hrl_anomaly_detection/src/hrl_anomaly_detection/HMM/learning_hmm.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/HMM/learning_hmm.py
@@ -634,7 +634,7 @@ if __name__ == '__main__':
 
     ## Init variables    
     data_path = os.getcwd()
-    nState    = 5
+    nState    = 7
     nMaxStep     = 36 # total step of data. It should be automatically assigned...
     pkl_file  = "door_opening_data.pkl"    
     nFutureStep = 6
