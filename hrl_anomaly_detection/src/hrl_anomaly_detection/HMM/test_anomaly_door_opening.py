26227368030ccb6bb39d4cdaeaefb65f968d69b9
dpark <dpark@pidi8.(none)>
2014-11-05 03:12:46 -0500

    modified:   test_anomaly_door_opening.py

--- hrl_anomaly_detection/src/hrl_anomaly_detection/HMM/test_anomaly_door_opening.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/HMM/test_anomaly_door_opening.py
@@ -120,7 +120,7 @@ if __name__ == '__main__':
 
     ## Init variables    
     data_path = os.getcwd()
-    nState    = 9
+    nState    = 11
     nMaxStep  = 36 # total step of data. It should be automatically assigned...
     pkl_file  = "door_opening_data.pkl"    
     nFutureStep = 1
