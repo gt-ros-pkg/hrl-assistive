a23d6bfee1d1ca48c5b2c51a806220ba1b9b744d
dpark <dpark@pidi8.(none)>
2014-12-26 01:59:56 -0500

    modified:   test_cluster.sh

--- hrl_anomaly_detection/src/hrl_anomaly_detection/door_opening/test_cluster.sh
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/door_opening/test_cluster.sh
@@ -1,12 +1,12 @@
 #!/bin/bash                                                                                                                      
-su dpark -c 'source ~/.profile ; nohup rosrun hrl_anomaly_detection test_anomaly_door_opening.py --cv &'
+nohup rosrun hrl_anomaly_detection test_anomaly_door_opening.py --cv &
 sleep 3.0
 
-su dpark -c 'source ~/.profile ; nohup rosrun hrl_anomaly_detection test_anomaly_door_opening.py --cv &'
+nohup rosrun hrl_anomaly_detection test_anomaly_door_opening.py --cv &
 sleep 3.0
 
-su dpark -c 'source ~/.profile ; nohup rosrun hrl_anomaly_detection test_anomaly_door_opening.py --cv &'
+nohup rosrun hrl_anomaly_detection test_anomaly_door_opening.py --cv &
 sleep 3.0
 
-su dpark -c 'source ~/.profile ; nohup rosrun hrl_anomaly_detection test_anomaly_door_opening.py --cv &'
+nohup rosrun hrl_anomaly_detection test_anomaly_door_opening.py --cv &
 sleep 3.0
