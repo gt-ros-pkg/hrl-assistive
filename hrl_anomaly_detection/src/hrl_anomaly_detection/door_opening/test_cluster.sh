a11bc8e84d619b13d5caf3e953f8eebe4a6cbfdb
dpark <dpark@pidi8.(none)>
2014-12-26 03:04:12 -0500

    modified:   door_opening/test_cluster.sh

--- hrl_anomaly_detection/src/hrl_anomaly_detection/door_opening/test_cluster.sh
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/door_opening/test_cluster.sh
@@ -8,5 +8,5 @@ sleep 3.0
 nohup rosrun hrl_anomaly_detection test_anomaly_door_opening.py --cv &
 sleep 3.0
 
-nohup rosrun hrl_anomaly_detection test_anomaly_door_opening.py --cv &
-sleep 3.0
+#nohup rosrun hrl_anomaly_detection test_anomaly_door_opening.py --cv &
+#sleep 3.0
