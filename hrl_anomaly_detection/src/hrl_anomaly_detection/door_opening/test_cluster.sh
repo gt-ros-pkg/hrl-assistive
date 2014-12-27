e62fa849d0bac8ae5a3eb89a26dc2c1e7aa2de48
dpark <dpark@pidi8.(none)>
2014-12-27 00:17:22 -0500

    modified:   test_cluster.sh

--- hrl_anomaly_detection/src/hrl_anomaly_detection/door_opening/test_cluster.sh
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/door_opening/test_cluster.sh
@@ -1,16 +1,8 @@
 #!/bin/bash                                                                                                      
 
-su dpark -c 'source ~/.profile ; nohup rosrun hrl_anomaly_detection test_anomaly_door_opening.py --cv &' 
-sleep 3.0 ; 
+su dpark -c 'source ~/.profile ; nohup rosrun hrl_anomaly_detection test_anomaly_door_opening.py --cv & ; sleep 3.0 ; nohup rosrun hrl_anomaly_detection test_anomaly_door_opening.py --cv & ; sleep 3.0 ; nohup rosrun hrl_anomaly_detection test_anomaly_door_opening.py --cv & ; sleep 3.0 ; nohup rosrun hrl_anomaly_detection test_anomaly_door_opening.py --cv & ' 
+ 
 
-su dpark -c 'source ~/.profile ; nohup rosrun hrl_anomaly_detection test_anomaly_door_opening.py --cv &' 
-sleep 3.0 ; 
-
-su dpark -c 'source ~/.profile ; nohup rosrun hrl_anomaly_detection test_anomaly_door_opening.py --cv &' 
-sleep 3.0 ; 
-
-su dpark -c 'source ~/.profile ; nohup rosrun hrl_anomaly_detection test_anomaly_door_opening.py --cv &' 
-sleep 3.0 ; 
        
 #nohup rosrun hrl_anomaly_detection test_anomaly_door_opening.py --cv &
 #sleep 3.0
