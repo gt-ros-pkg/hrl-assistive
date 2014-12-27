2f63a696a943555255afe2f96c8d592731162250
dpark <dpark@pidi8.(none)>
2014-12-27 00:01:36 -0500

    modified:   test_cluster.sh

--- hrl_anomaly_detection/src/hrl_anomaly_detection/door_opening/test_cluster.sh
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/door_opening/test_cluster.sh
@@ -1,6 +1,16 @@
 #!/bin/bash                                                                                                      
 
-su dpark -c 'source ~/.profile ; nohup rosrun hrl_anomaly_detection test_anomaly_door_opening.py --cv > nohup_1.txt &; sleep 3.0 ; nohup rosrun hrl_anomaly_detection test_anomaly_door_opening.py --cv > nohup_2.txt &; sleep 3.0 ; nohup rosrun hrl_anomaly_detection test_anomaly_door_opening.py --cv > nohup_3.txt &; sleep 3.0 ; nohup rosrun hrl_anomaly_detection test_anomaly_door_opening.py --cv > nohup_4.txt &; sleep 3.0 ;'         
+echo pidi5252 | su dpark -c 'source ~/.profile ; nohup rosrun hrl_anomaly_detection test_anomaly_door_opening.py --cv > nohup_1.txt &;' 
+sleep 3.0 ; 
+
+echo pidi5252 | su dpark -c 'source ~/.profile ; nohup rosrun hrl_anomaly_detection test_anomaly_door_opening.py --cv > nohup_1.txt &;' 
+sleep 3.0 ; 
+
+echo pidi5252 | su dpark -c 'source ~/.profile ; nohup rosrun hrl_anomaly_detection test_anomaly_door_opening.py --cv > nohup_1.txt &;' 
+sleep 3.0 ; 
+
+echo pidi5252 | su dpark -c 'source ~/.profile ; nohup rosrun hrl_anomaly_detection test_anomaly_door_opening.py --cv > nohup_1.txt &;' 
+sleep 3.0 ; 
        
 #nohup rosrun hrl_anomaly_detection test_anomaly_door_opening.py --cv &
 #sleep 3.0
