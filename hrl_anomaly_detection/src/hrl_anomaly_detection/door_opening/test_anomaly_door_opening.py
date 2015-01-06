41d71e9ad4d3848557d485356bc14156107f0e1d
dpark <dpark@pidi8.(none)>
2015-01-06 03:33:35 -0500

    modified:   test_anomaly_door_opening.py

--- hrl_anomaly_detection/src/hrl_anomaly_detection/door_opening/test_anomaly_door_opening.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/door_opening/test_anomaly_door_opening.py
@@ -393,10 +393,10 @@ def get_threshold_by_cost(cross_data_path, cross_test_path, cost_ratios, nMaxSte
     X_test = np.arange(0.0, 36.0, 1.0)
     start_step = 2
 
-    ## score_n    = np.arange(0.3,1.01,0.1)
+    score_n    = np.arange(0.3,1.01,0.1)
     sig_mult   = np.arange(0.5, 10.0+0.00001, 0.1)
     sig_offset = np.arange(0.0, 1.5+0.00001, 0.1)
-    score_n    = [1.0]
+    ## score_n    = [1.0]
     ## sig_mult   = np.arange(0.5, 10.0+0.00001, 0.1)
     ## sig_offset = [0.0]
     
