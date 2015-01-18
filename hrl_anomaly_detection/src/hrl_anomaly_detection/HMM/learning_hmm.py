a15350ffa7ba55f50157a715b599931b6b87020a
dpark <dpark@pidi8.(none)>
2015-01-18 17:05:25 -0500

    modified:   ../HMM/learning_hmm.py 	modified:   test_anomaly_door_opening.py

--- hrl_anomaly_detection/src/hrl_anomaly_detection/HMM/learning_hmm.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/HMM/learning_hmm.py
@@ -132,7 +132,7 @@ class learning_hmm(learning_base):
         index = 0
         m,n = np.shape(vec)
         ## print m,n
-        mult = 4
+        mult = 6
 
         o_x = np.arange(0.0, self.nMaxStep, 1.0)
         o_mu  = scp.mean(vec, axis=0)
