c2d76623a07853cb0ac987985bf2412617f74d4f
dpark <dpark@pidi8.(none)>
2015-01-18 21:03:03 -0500

    modified:   test_anomaly_door_opening.py

--- hrl_anomaly_detection/src/hrl_anomaly_detection/door_opening/test_anomaly_door_opening.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/door_opening/test_anomaly_door_opening.py
@@ -343,11 +343,11 @@ def get_threshold_by_cost(cross_data_path, cross_test_path, cost_ratios, nMaxSte
     X_test = np.arange(0.0, 36.0, 1.0)
     start_step = 2
     
-    sig_mult        = np.arange(0.5, 60.0+0.00001, 1.0)
+    sig_mult        = np.arange(0.5, 70.0+0.00001, 1.0)
     sig_offset      = np.arange(0.0, 3.0+0.00001, 0.1)
 
     ## sig_mult   = np.arange(0.5, 10.0+0.00001, 0.1)
-    ## sig_offset = [0.0]
+    sig_offset = [0.0]
     
     param_list = []
     for a in sig_mult:
@@ -856,7 +856,7 @@ if __name__ == '__main__':
         cross_data_path = '/home/dpark/hrl_file_server/dpark_data/anomaly/RSS2015/door_'+ROC_target+'_cross_data'
         cross_test_path = os.path.join(cross_data_path,ROC_target+'_'+trans_type)        
 
-        future_steps = [1, 2, 4, 8]             
+        future_steps = [1, 8]             
         ## cost_ratios = [1.0]
         cost_ratios = [1.0, 0.9999, 0.999, 0.99, 0.98, 0.97, 0.95, 0.92, 0.9, 0.8, 0.7, 0.5, 0.3, 0.0]
         ang_interval = 1.0
