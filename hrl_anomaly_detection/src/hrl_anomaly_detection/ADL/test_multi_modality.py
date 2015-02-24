ff9ef7d74f774cee8a0fad79123e9598cf792bb1
dpark <dpark@pidi8.(none)>
2015-02-24 01:43:14 -0500

    modified:   test_multi_modality.py

--- hrl_anomaly_detection/src/hrl_anomaly_detection/ADL/test_multi_modality.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/ADL/test_multi_modality.py
@@ -597,7 +597,7 @@ if __name__ == '__main__':
     cross_root_path = '/home/dpark/hrl_file_server/dpark_data/anomaly/Humanoids2015/robot'
     
     class_num = 0
-    task  = 2
+    task  = 0
     if class_num == 0:
         class_name = 'door'
         task_names = ['microwave_black', 'microwave_white', 'lab_cabinet']
