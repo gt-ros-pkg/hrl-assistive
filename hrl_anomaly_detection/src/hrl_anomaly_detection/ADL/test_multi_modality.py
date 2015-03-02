cc439392c15b4e72567e67c41a75ea2f9a521f46
dpark <dpark@brain.(none)>
2015-03-02 13:45:32 -0500

    modified:   test_multi_modality.py

--- hrl_anomaly_detection/src/hrl_anomaly_detection/ADL/test_multi_modality.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/ADL/test_multi_modality.py
@@ -63,7 +63,7 @@ def fig_roc_offline_sim(cross_data_path, \
     # K random training-test set
     K = 10
     splits = []
-    for i in xrange(20):
+    for i in xrange(30):
         
         print os.path.join(cross_data_path,"train_dataSet_"+str(i))
         
@@ -949,7 +949,7 @@ if __name__ == '__main__':
         print "ROC Offline Robot with simulated anomalies"
         cross_data_path = os.path.join(cross_root_path, 'multi_sim_'+task_names[task])
         nState          = nState_l[task]
-        threshold_mult  = np.arange(0.0, 15.001, 0.25)    
+        threshold_mult  = np.arange(0.0, 20.001, 0.25)    
         attr            = 'id'
 
         fig_roc_offline_sim(cross_data_path, \
