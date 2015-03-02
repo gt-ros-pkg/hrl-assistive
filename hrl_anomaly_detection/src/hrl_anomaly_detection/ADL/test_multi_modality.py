9914de83040915b5c21268322ddcde7d4535adf6
dpark <dpark@pidi8.(none)>
2015-03-02 03:49:21 -0500

    modified:   ADL/test_multi_modality.py

--- hrl_anomaly_detection/src/hrl_anomaly_detection/ADL/test_multi_modality.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/ADL/test_multi_modality.py
@@ -949,7 +949,7 @@ if __name__ == '__main__':
         print "ROC Offline Robot with simulated anomalies"
         cross_data_path = os.path.join(cross_root_path, 'multi_sim_'+task_names[task])
         nState          = nState_l[task]
-        threshold_mult  = np.arange(0.0, 20.001, 0.5)    
+        threshold_mult  = np.arange(0.0, 40.001, 0.5)    
         attr            = 'id'
 
         fig_roc_offline_sim(cross_data_path, \
