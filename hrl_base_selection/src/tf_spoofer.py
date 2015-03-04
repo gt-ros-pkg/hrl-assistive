9ac6222419187819f302403a68088ebfb6fc345c
mycroft <mycroft@mycroft-marvin2.(none)>
2015-03-03 19:30:06 -0500

    many changes to tf spoofer

--- hrl_base_selection/src/tf_spoofer.py
+++ hrl_base_selection/src/tf_spoofer.py
@@ -28,18 +28,21 @@ class TF_Spoofer(object):
         self.lock = Lock()
         self.robot_pose = None
         self.target_pose = None
-        self.world_B_robot = None
+        self.robot_B_world = np.matrix(np.eye(4))
         self.world_B_reference = None
-        self.robot_center_pub = rospy.Publisher('/robot_center', PoseStamped, latch=True)
-        self.head_center_pub = rospy.Publisher('/head_center', PoseStamped, latch=True)
+        now = rospy.Time.now()
+        self.tf_listener.waitForTransform('/base_link', '/r_forearm_cam_optical_frame', rospy.Time(), rospy.Duration(15.0))
+        (trans, rot) = self.tf_listener.lookupTransform('/base_link', '/r_forearm_cam_optical_frame', rospy.Time())
+        self.base_link_B_camera = createBMatrix(trans, rot)
+        self.robot_center_pub = rospy.Publisher('/robot_frame', PoseStamped, latch=True)
+        self.head_center_pub = rospy.Publisher('/head_frame', PoseStamped, latch=True)
         self.reference_pub = rospy.Publisher('/reference', PoseStamped, latch=True)
-        self.ar_pub = rospy.Publisher('/ar_marker', ARMarker, latch=True)
+        self.ar_pub = rospy.Publisher("/l_pr2_ar_pose_marker", ARMarker, latch=True)
         self.robot_sub = rospy.Subscriber('/robot_back/pose', TransformStamped, self.robot_cb)
         self.head_sub = rospy.Subscriber('/head_back/pose', TransformStamped, self.head_cb)
         self.reference_sub = rospy.Subscriber('/reference_back/pose', TransformStamped, self.reference_cb)
-        self.tf_listener.waitForTransform('/base_link', '/r_arm_camera', now, rospy.Duration(15))
-        (trans, rot) = self.tf_listener.lookupTransform('/base_link', '/head_frame', now)
-        self.base_link_B_camera = createBMatrix(trans, rot)
+        
+        
         print 'The tf_spoofer has initialized without a problem, as far as I can tell!'
 
     def robot_cb(self, data):
@@ -52,16 +55,32 @@ class TF_Spoofer(object):
                    data.transform.rotation.z,
                    data.transform.rotation.w]
             world_B_robot_back = createBMatrix(trans, rot)
-            robot_back_B_base_link = np.matrix([[1., 0., 0., 0.30],
+            robot_back_B_robot_center = np.matrix([[1., 0., 0., (.665/2+.02)],
                                                 [0., 1., 0., 0.],
                                                 [0., 0., 1., 0.],
                                                 [0., 0., 0., 1.]])
-            world_B_robot = world_B_robot_back*robot_back_B_base_link
-            pos, ori = Bmat_to_pos_quat(world_B_robot)
+            #world_B_robot = world_B_robot_back  # *robot_back_B_base_link
+
+            z_origin = np.array([0, 0, 1])
+            x_robot = np.array([world_B_robot_back[0, 0], world_B_robot_back[1, 0], world_B_robot_back[2, 0]])
+            y_robot_project = np.cross(z_origin, x_robot)
+            y_robot_project = y_robot_project/np.linalg.norm(y_robot_project)
+            x_robot_project = np.cross(y_robot_project, z_origin)
+            x_robot_project = x_robot_project/np.linalg.norm(x_robot_project)
+            world_B_robot_back_project = np.eye(4)
+            for i in xrange(3):
+                world_B_robot_back_project[i, 0] = x_robot_project[i]
+                world_B_robot_back_project[i, 1] = y_robot_project[i]
+                world_B_robot_back_project[i, 3] = world_B_robot_back[i, 3]
+            world_B_robot = np.matrix(world_B_robot_back_project)*robot_back_B_robot_center
+            world_B_robot[2, 3] = 0.
+
+
+            pos, ori = Bmat_to_pos_quat(world_B_robot.I)
             pos[2] = 0.
-            self.world_B_robot = createBMatrix(pos, ori)
+            self.robot_B_world = createBMatrix(pos, ori)
             psm = PoseStamped()
-            psm.header.frame_id = data.header.frame_id
+            psm.header.frame_id = '/base_link'
             psm.pose.position.x = pos[0]
             psm.pose.position.y = pos[1]
             psm.pose.position.z = pos[2]
@@ -71,7 +90,7 @@ class TF_Spoofer(object):
             psm.pose.orientation.w = ori[3]
             self.robot_center_pub.publish(psm)
             self.tf_broadcaster.sendTransform((pos[0], pos[1], 0.), (ori[0], ori[1], ori[2], ori[3]),
-                                              rospy.Time.now(), '/base_link', '/optitrak')
+                                              rospy.Time.now(), '/optitrak', '/base_link')
             # world_B_pr2 = createBMatrix(trans, rot)
             # self.robot_pose = world_B_pr2
             # self.update_feedback()
@@ -87,9 +106,9 @@ class TF_Spoofer(object):
                    data.transform.rotation.z,
                    data.transform.rotation.w]
             world_B_head_back = createBMatrix(trans, rot)
-            head_back_B_head_center = np.matrix([[1., 0., 0., 0.07],
+            head_back_B_head_center = np.matrix([[1., 0., 0., -0.07],
                                                  [0., 1., 0., 0.],
-                                                 [0., 0., 1., 0.],
+                                                 [0., 0., 1., -0.075],
                                                  [0., 0., 0., 1.]])
             pos, ori = Bmat_to_pos_quat(world_B_head_back*head_back_B_head_center)
             psm = PoseStamped()
@@ -105,6 +124,8 @@ class TF_Spoofer(object):
             # world_B_pr2 = createBMatrix(trans, rot)
             # self.robot_pose = world_B_pr2
             # self.update_feedback()
+            self.tf_broadcaster.sendTransform((pos[0], pos[1], pos[2]), (ori[0], ori[1], ori[2], ori[3]),
+                                              rospy.Time.now(), '/head_frame', "/optitrak")
 
     def reference_cb(self, data):
         with self.lock:
@@ -117,15 +138,32 @@ class TF_Spoofer(object):
                    data.transform.rotation.z,
                    data.transform.rotation.w]
             world_B_reference_back = createBMatrix(trans, rot)
-            reference_back_B_reference = np.matrix([[1., 0., 0., 0.],
-                                                    [0., 1., 0., 0.],
-                                                    [0., 0., 1., 0.],
-                                                    [0., 0., 0., 1.]])
-            world_B_reference = world_B_reference_back*reference_back_B_reference
-            robot_B_reference = (self.world_B_robot*self.base_link_B_camera).I*world_B_reference
-            pos, ori = Bmat_to_pos_quat(robot_B_reference)
+
+            reference_back_B_reference_center = np.matrix([[1., 0., 0., 0.06],
+                                                           [0., 1., 0., 0.],
+                                                           [0., 0., 1., -0.74],
+                                                           [0., 0., 0., 1.]])
+
+            z_origin = np.array([0, 0, 1])
+            x_ref = np.array([world_B_reference_back[0, 0], world_B_reference_back[1, 0], world_B_reference_back[2, 0]])
+            y_ref_project = np.cross(z_origin, x_ref)
+            y_ref_project = y_ref_project/np.linalg.norm(y_ref_project)
+            x_ref_project = np.cross(y_ref_project, z_origin)
+            x_ref_project = x_ref_project/np.linalg.norm(x_ref_project)
+            world_B_ref_back_project = np.eye(4)
+            for i in xrange(3):
+                world_B_ref_back_project[i, 0] = x_ref_project[i]
+                world_B_ref_back_project[i, 1] = y_ref_project[i]
+                world_B_ref_back_project[i, 3] = world_B_reference_back[i, 3]
+            world_B_ref = np.matrix(world_B_ref_back_project)*reference_back_B_reference_center
+            world_B_ref[2, 3] = 0.
+
+            
+            # world_B_reference = world_B_reference_back*reference_back_B_reference
+            # camera_B_reference = ((self.robot_B_world.I)*self.base_link_B_camera).I*world_B_reference
+            pos, ori = Bmat_to_pos_quat(world_B_ref)
             psm = PoseStamped()
-            psm.header.frame_id = '/r_arm_camera'
+            psm.header.frame_id = '/optitrak'
             psm.pose.position.x = pos[0]
             psm.pose.position.y = pos[1]
             psm.pose.position.z = pos[2]
@@ -135,9 +173,10 @@ class TF_Spoofer(object):
             psm.pose.orientation.w = ori[3]
             self.reference_pub.publish(psm)
             self.tf_broadcaster.sendTransform((pos[0], pos[1], pos[2]), (ori[0], ori[1], ori[2], ori[3]),
-                                              rospy.Time.now(), '/reference_location', "/r_arm_camera")
+                                              rospy.Time.now(), '/reference', "/optitrak")
+            
             ar_m = ARMarker()
-            ar_m.header.frame_id = '/r_arm_camera'
+            ar_m.header.frame_id = '/r_forearm_cam_optical_frame'
             ar_m.pose.pose.position.x = pos[0]
             ar_m.pose.pose.position.y = pos[1]
             ar_m.pose.pose.position.z = pos[2]
@@ -146,6 +185,7 @@ class TF_Spoofer(object):
             ar_m.pose.pose.orientation.z = ori[2]
             ar_m.pose.pose.orientation.w = ori[3]
             self.ar_pub.publish(ar_m)
+            
             # self.tf_broadcaster.sendTransform((pos[0], pos[1], pos[2]), (ori[0], ori[1], ori[2], ori[3]),
             #                                   rospy.Time.now(), '/ar_marker', "/r_arm_camera")
 
