c1ef32b852007b74ed3019ea2b3a0a13a6704cf1
Hyder Hasnain <hhasnain3@gatech.edu>
2015-03-13 15:48:09 -0400

    Last scooping attempt didn't work, arm went crazy. Going back to original to try again - Hyder

--- hrl_feeding_task/src/hrl_feeding_task/arm_reacher_server.py
+++ hrl_feeding_task/src/hrl_feeding_task/arm_reacher_server.py
@@ -61,77 +61,77 @@ class armReachAction(mpcBaseAction):
 
         #----MAIN STUFF
 
-        # print "MOVES1"
-        # (pos.x, pos.y, pos.z) = (1.0, 0.27, 0.005)
-        # (quat.x, quat.y, quat.z, quat.w) = (0.253, -0.141, -0.252, 0.923)
-        # timeout = 1.0
-        # self.setOrientGoal(pos, quat, timeout)
-        # ## print self.getEndeffectorPose()
+        print "MOVES1"
+        (pos.x, pos.y, pos.z) = (1.0, 0.27, 0.005)
+        (quat.x, quat.y, quat.z, quat.w) = (0.253, -0.141, -0.252, 0.923)
+        timeout = 1.0
+        self.setOrientGoal(pos, quat, timeout)
+        print self.getEndeffectorPose()
         
-        # print "MOVES2"
-        # (pos.x, pos.y, pos.z) = (1.0, 0.27, 0.005-0.2)
-        # (quat.x, quat.y, quat.z, quat.w) = (0.253, -0.141, -0.252, 0.923)
-        # timeout = 1.0 
-        # self.setOrientGoal(pos, quat, timeout)
+        print "MOVES2"
+        (pos.x, pos.y, pos.z) = (1.0, 0.27, 0.005-0.2)
+        (quat.x, quat.y, quat.z, quat.w) = (0.253, -0.141, -0.252, 0.923)
+        timeout = 1.0 
+        self.setOrientGoal(pos, quat, timeout)
 
-        # print "MOVES3"
-        # (pos.x, pos.y, pos.z) = (1.0, 0.27, 0.005)
-        # (quat.x, quat.y, quat.z, quat.w) = (0.253, -0.141, -0.252, 0.923)
-        # timeout = 1.0
-        # self.setOrientGoal(pos, quat, timeout)
-        # #raw_input("Enter anything to start: ")
+        print "MOVES3"
+        (pos.x, pos.y, pos.z) = (1.0, 0.27, 0.005)
+        (quat.x, quat.y, quat.z, quat.w) = (0.253, -0.141, -0.252, 0.923)
+        timeout = 1.0
+        self.setOrientGoal(pos, quat, timeout)
+        raw_input("Enter anything to start: ")
 
         #!!---- HYDER TASK 1 BASIC SPOON IN BOWL
 
-        print "MOVES0"
-        (pos.x, pos.y, pos.z) = (0.586, 0.165, 0.175)
-        (quat.x, quat.y, quat.z, quat.w) = (-0.591, -0.378, -0.385, 0.600)
-        timeout = 1.0
-        self.setOrientGoal(pos, quat, timeout)
-        raw_input("Enter anything to start: ")
+        # print "MOVES0"
+        # (pos.x, pos.y, pos.z) = (0.586, 0.165, 0.175)
+        # (quat.x, quat.y, quat.z, quat.w) = (-0.591, -0.378, -0.385, 0.600)
+        # timeout = 1.0
+        # self.setOrientGoal(pos, quat, timeout)
+        # raw_input("Enter anything to start: ")
 
-        print "MOVES1"
-        (pos.x, pos.y, pos.z) = (0.450, 0.725, 0.212)
-        (quat.x, quat.y, quat.z, quat.w) = (-0.508, -0.453, -0.113, 0.723)
-        timeout = 1.0
-        self.setOrientGoal(pos, quat, timeout)
-        raw_input("Enter anything to start: ")
+        # print "MOVES1"
+        # (pos.x, pos.y, pos.z) = (0.450, 0.725, 0.212)
+        # (quat.x, quat.y, quat.z, quat.w) = (-0.508, -0.453, -0.113, 0.723)
+        # timeout = 1.0
+        # self.setOrientGoal(pos, quat, timeout)
+        # raw_input("Enter anything to start: ")
 
 
-        print "MOVES2"
-        (pos.x, pos.y, pos.z) = (0.702, 0.694, -0.111)
-        (quat.x, quat.y, quat.z, quat.w) = (-0.747, 0.003, 0.019, 0.665)
-        timeout = 1.0
-        self.setOrientGoal(pos, quat, timeout)
-        raw_input("Enter anything to start: ")
+        # print "MOVES2"
+        # (pos.x, pos.y, pos.z) = (0.702, 0.694, -0.111)
+        # (quat.x, quat.y, quat.z, quat.w) = (-0.747, 0.003, 0.019, 0.665)
+        # timeout = 1.0
+        # self.setOrientGoal(pos, quat, timeout)
+        # raw_input("Enter anything to start: ")
 
-        print "MOVES3"
-        (pos.x, pos.y, pos.z) = (0.805, 0.711, -0.249)
-        (quat.x, quat.y, quat.z, quat.w) = (-0.724, 0.070, 0.121, 0.676)
-        timeout = 1.0
-        self.setOrientGoal(pos, quat, timeout)
-        raw_input("Enter anything to start: ")
+        # print "MOVES3"
+        # (pos.x, pos.y, pos.z) = (0.805, 0.711, -0.249)
+        # (quat.x, quat.y, quat.z, quat.w) = (-0.724, 0.070, 0.121, 0.676)
+        # timeout = 1.0
+        # self.setOrientGoal(pos, quat, timeout)
+        # raw_input("Enter anything to start: ")
 
-        print "MOVES4"
-        (pos.x, pos.y, pos.z) = (0.747, 0.719, 0.050)
-        (quat.x, quat.y, quat.z, quat.w) = (-0.734, -0.052, -0.018, 0.677)
-        timeout = 1.0
-        self.setOrientGoal(pos, quat, timeout)
-        raw_input("Enter anything to start: ")
+        # print "MOVES4"
+        # (pos.x, pos.y, pos.z) = (0.747, 0.719, 0.050)
+        # (quat.x, quat.y, quat.z, quat.w) = (-0.734, -0.052, -0.018, 0.677)
+        # timeout = 1.0
+        # self.setOrientGoal(pos, quat, timeout)
+        # raw_input("Enter anything to start: ")
 
-        print "MOVES5"
-        (pos.x, pos.y, pos.z) = (0.591, 0.206, 0.208)
-        (quat.x, quat.y, quat.z, quat.w) = (-0.674, 0.207, -0.304, 0.641)
-        timeout = 1.0
-        self.setOrientGoal(pos, quat, timeout)
-        raw_input("Enter anything to start: ")
+        # print "MOVES5"
+        # (pos.x, pos.y, pos.z) = (0.591, 0.206, 0.208)
+        # (quat.x, quat.y, quat.z, quat.w) = (-0.674, 0.207, -0.304, 0.641)
+        # timeout = 1.0
+        # self.setOrientGoal(pos, quat, timeout)
+        # raw_input("Enter anything to start: ")
 
-        print "MOVES6"
-        (pos.x, pos.y, pos.z) = (0.790, -0.292, 0.059)
-        (quat.x, quat.y, quat.z, quat.w) = (-0.617, 0.343, -0.409, 0.579)
-        timeout = 1.0
-        self.setOrientGoal(pos, quat, timeout)
-        raw_input("Enter anything to start: ")
+        # print "MOVES6"
+        # (pos.x, pos.y, pos.z) = (0.790, -0.292, 0.059)
+        # (quat.x, quat.y, quat.z, quat.w) = (-0.617, 0.343, -0.409, 0.579)
+        # timeout = 1.0
+        # self.setOrientGoal(pos, quat, timeout)
+        # raw_input("Enter anything to start: ")
 
 
 
