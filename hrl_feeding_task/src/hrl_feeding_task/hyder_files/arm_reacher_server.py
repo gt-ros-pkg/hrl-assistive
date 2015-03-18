14f9fe1ad56b3e493441b23e8f2af7523a29cc80
Daehyung Park <deric.park@gatech.edu>
2015-03-18 12:44:45 -0700

    Added subcriber/publisher to robot_state topic, attempted to manually set initial joint angles to prevent weird motions during pose estimation. Didn't work well, needs work. Changed bowl_location_publisher to a PoseStamped message instead of Pose - Hyder

--- hrl_feeding_task/src/hrl_feeding_task/hyder_files/arm_reacher_server.py
+++ hrl_feeding_task/src/hrl_feeding_task/hyder_files/arm_reacher_server.py
@@ -12,21 +12,32 @@ import hrl_haptic_mpc.haptic_mpc_util as haptic_mpc_util
 from hrl_srvs.srv import None_Bool, None_BoolResponse
 from geometry_msgs.msg import Pose, PoseStamped, Point, Quaternion
 from sandbox_dpark_darpa_m3.lib.hrl_mpc_base import mpcBaseAction
+import std_msgs.msg
 
+import hrl_haptic_manipulation_in_clutter_msgs.msg as haptic_msgs
 
 class armReachAction(mpcBaseAction):
     def __init__(self, d_robot, controller, arm = 'l'):
 
         mpcBaseAction.__init__(self, d_robot, controller, arm)
         
-        rospy.Subscriber('bowl_location', Pose, self.bowlPoseCallback)
+	#Subscribers to publishers of bowl location data
+        rospy.Subscriber('bowl_location', PoseStamped, self.bowlPoseCallback)
 	rospy.Subscriber('RYDS_CupLocation', PoseStamped, self.bowlPoseKinectCallback)	
+	#Be able to read the current state of the robot, used for setting new joint angles for init
+	rospy.Subscriber('haptic_mpc/robot_state', haptic_msgs.RobotHapticState, self.robotStateCallback)
+
+	#Be able to publish new data to the robot state, especially new joint angles
+	self.robotState_pub = rospy.Publisher('haptic_mpc/robot_state', haptic_msgs.RobotHapticState)
 
         self.tfListener = tf.TransformListener()
         self.tfBroadcaster = tf.TransformBroadcaster()
 
         # service request
         self.reach_service = rospy.Service('/arm_reach_enable', None_Bool, self.start_cb)
+
+	self.pose1JointAngles = (0.02204231193041639, 0.72850849623194, 0.08302486916827911, -2.051374187142846, -3.1557218713638484, -1.2799710435005978, 10.952306846165152)
+	#These joint angles correspond to 'Pose 1' - See Keep note
 
         rate = rospy.Rate(100) # 25Hz, nominally.                
         while not rospy.is_shutdown():
@@ -50,9 +61,13 @@ class armReachAction(mpcBaseAction):
             return None_BoolResponse(False)
 
     def bowlPoseCallback(self, data):
-	#Takes in a Pose() type message, contains only Point() and Quaternion(), from manual bowl location publisher
-        self.bowl_pos = np.matrix([ [data.position.x], [data.position.y], [data.position.z] ])
-        self.bowl_quat = np.matrix([ [data.orientation.x], [data.orientation.y], [data.orientation.z], [data.orientation.w] ])
+	self.bowl_frame = data.header.frame_id
+	self.bowl_pos = np.matrix([ [data.pose.position.x], [data.pose.position.y], [data.pose.position.z] ])
+        self.bowl_quat = np.matrix([ [data.pose.orientation.x], [data.pose.orientation.y], [data.pose.orientation.z], [data.pose.orientation.w] ])
+
+	#rospy.loginfo(self.bowl_pos)
+        #rospy.loginfo(self.bowl_quat)
+
 
     def bowlPoseKinectCallback(self, data):
 	#Takes in a PointStamped() type message, contains Header() and Pose(), from Kinect bowl location publisher
@@ -64,77 +79,101 @@ class armReachAction(mpcBaseAction):
 	#rospy.loginfo(self.bowl_pos)
 	#rospy.loginfo(self.bowl_quat)	
 
+    def robotStateCallback(self, data): #Read from RobotState topic and saves the current data, including joint angles, used to create new message containing initialized joint angles to publish to RobotState topic
+	self.robotData = data
+	self.joint_angles_raw = data.joint_angles
+	#rospy.loginfo("Raw Robot State Data: ")
+	#rospy.loginfo(self.robotData)
+	#rospy.loginfo("Raw Robot Joint Angles: ")
+	#rospy.loginfo(self.joint_angles_raw)
+	#rospy.sleep(1)
+	
     def run(self):
 
         pos  = Point()
         quat = Quaternion()
 
-        ## confirm = False
-        ## while not confirm:
-        ##     print "Current pose"
-        ##     print self.getEndeffectorPose()
-        ##     ans = raw_input("Enter y to confirm to start: ")
-        ##     if ans == 'y':
-        ##         confirm = True
-        ## print self.getJointAngles()        
+        confirm = False
+        while not confirm:
+            print "Current pose"
+            print self.getEndeffectorPose()
+            ans = raw_input("Enter y to confirm to start: ")
+            if ans == 'y':
+                confirm = True
+        print self.getJointAngles()        
         
+	print "Setting initial joint angles... "
+	
+	self.robotData.joint_angles = self.pose1JointAngles
+	self.robotState_pub.publish(self.robotData)
+	#Publishes initial joint angles to robot_state topic, in theory supposed to make robot left arm go to specified joint angles, but doesn't work well. New method needed...
 
         #!!---- BASIC SCOOPING MOTION WITH BOWL POSITION OFFSET
-	#Flat Gripper Rotation Values:
+	#Flat Gripper Orientation Values:
 	#(0.642, 0.150, 0.154, 0.736)
+
+	#raw_input("Enter anything to go to initial position: ")
+	#print "MOVE0, Initial Position"
+        #(pos.x, pos.y, pos.z) = (0.803, 0.182, 0.067)
+        #(quat.x, quat.y, quat.z, quat.w) = (0.696, 0.035, -0.008, 0.717)
+        #timeout = 1.0
+        #self.setOrientGoal(pos, quat, timeout)
+        raw_input("Enter anything to start: ")
+
 
         print "MOVES1"
         (pos.x, pos.y, pos.z) = (self.bowl_pos[0] + -0.2920, self.bowl_pos[1] + -0.7260, self.bowl_pos[2] + 0.2600)
-        (quat.x, quat.y, quat.z, quat.w) = (0.740, 0.052, -0.100, 0.663)  # (self.bowl_quat[0], self.bowl_quat[1], self.bowl_quat[2], self.bowl_quat[3]) # (0.573, -0.451, -0.534, 0.428)
+        (quat.x, quat.y, quat.z, quat.w) = (0.696, 0.035, -0.008, 0.717)  # (self.bowl_quat[0], self.bowl_quat[1], self.bowl_quat[2], self.bowl_quat[3]) # (0.573, -0.451, -0.534, 0.428)
         timeout = 1.0
         self.setOrientGoal(pos, quat, timeout)
         raw_input("Enter anything to start: ")
 
         print "MOVES2"
         (pos.x, pos.y, pos.z) = (self.bowl_pos[0] + -0.1080, self.bowl_pos[1] + -0.1410, self.bowl_pos[2] + 0.2550)
-        (quat.x, quat.y, quat.z, quat.w) = (0.740, 0.052, -0.100, 0.663)  # (self.bowl_quat[0], self.bowl_quat[1], self.bowl_quat[2], self.bowl_quat[3]) #  (0.690, -0.092, -0.112, 0.709)
+        (quat.x, quat.y, quat.z, quat.w) = (0.696, 0.035, -0.008, 0.717)  # (self.bowl_quat[0], self.bowl_quat[1], self.bowl_quat[2], self.bowl_quat[3]) #  (0.690, -0.092, -0.112, 0.709)
         timeout = 1.0
         self.setOrientGoal(pos, quat, timeout)
         raw_input("Enter anything to start: ")
 
         print "MOVES3"
         (pos.x, pos.y, pos.z) = (self.bowl_pos[0] + -0.0920, self.bowl_pos[1] + 0.0310, self.bowl_pos[2] + 0.0950)
-        (quat.x, quat.y, quat.z, quat.w) = (0.601, 0.397, -0.412, 0.558)  #  (self.bowl_quat[0], self.bowl_quat[1], self.bowl_quat[2], self.bowl_quat[3]) #  (0.713, 0.064, -0.229, 0.659)
+        (quat.x, quat.y, quat.z, quat.w) = (0.696, 0.035, -0.008, 0.717)  #  (self.bowl_quat[0], self.bowl_quat[1], self.bowl_quat[2], self.bowl_quat[3]) #  (0.713, 0.064, -0.229, 0.659)
         timeout = 1.0
         self.setOrientGoal(pos, quat, timeout)
         raw_input("Enter anything to start: ")
 
         print "MOVES4"
         (pos.x, pos.y, pos.z) = (self.bowl_pos[0] + -0.0140, self.bowl_pos[1] +  0, self.bowl_pos[2] + 0.00900)
-        (quat.x, quat.y, quat.z, quat.w) = (0.601, 0.397, -0.412, 0.558)  # (self.bowl_quat[0], self.bowl_quat[1], self.bowl_quat[2], self.bowl_quat[3]) #  (0.700, 0.108, -0.321, 0.629)
+        (quat.x, quat.y, quat.z, quat.w) = (0.696, 0.035, -0.008, 0.717)  # (self.bowl_quat[0], self.bowl_quat[1], self.bowl_quat[2], self.bowl_quat[3]) #  (0.700, 0.108, -0.321, 0.629)
         timeout = 1.0
         self.setOrientGoal(pos, quat, timeout)
         raw_input("Enter anything to start: ")
 
         print "MOVES5"
         (pos.x, pos.y, pos.z) = (self.bowl_pos[0] + 0, self.bowl_pos[1] + 0, self.bowl_pos[2] +  0) #REACHED THE ACTUAL BOWL
-        (quat.x, quat.y, quat.z, quat.w) = (0.700, 0.201, -0.229, 0.646)  # (self.bowl_quat[0], self.bowl_quat[1], self.bowl_quat[2], self.bowl_quat[3]) #  (0.706, 0.068, -0.235, 0.664)
+        (quat.x, quat.y, quat.z, quat.w) = (0.696, 0.035, -0.008, 0.717)  # (self.bowl_quat[0], self.bowl_quat[1], self.bowl_quat[2], self.bowl_quat[3]) #  (0.706, 0.068, -0.235, 0.664)
         timeout = 1.0
         self.setOrientGoal(pos, quat, timeout)
         raw_input("Enter anything to start: ")
 
         print "MOVES6"
         (pos.x, pos.y, pos.z) = (self.bowl_pos[0] + 0.0120, self.bowl_pos[1] + -0.00100, self.bowl_pos[2] +  0.0130)
-        (quat.x, quat.y, quat.z, quat.w) = (0.740, 0.052, -0.100, 0.663)  # (self.bowl_quat[0], self.bowl_quat[1], self.bowl_quat[2], self.bowl_quat[3]) #  (0.672, 0.037, -0.137, 0.727)
+        (quat.x, quat.y, quat.z, quat.w) = (0.696, 0.035, -0.008, 0.717)  # (self.bowl_quat[0], self.bowl_quat[1], self.bowl_quat[2], self.bowl_quat[3]) #  (0.672, 0.037, -0.137, 0.727)
         timeout = 1.0
         self.setOrientGoal(pos, quat, timeout)
         raw_input("Enter anything to start: ")
 
         print "MOVES7"
+        print "MOVES7"
         (pos.x, pos.y, pos.z) = (self.bowl_pos[0] + -0.122, self.bowl_pos[1] + -0.0319, self.bowl_pos[2] + 0.300)
-        (quat.x, quat.y, quat.z, quat.w) = (0.740, 0.052, -0.100, 0.663)  # (self.bowl_quat[0], self.bowl_quat[1], self.bowl_quat[2], self.bowl_quat[3]) #  (0.699, -0.044, -0.085, 0.709)
+        (quat.x, quat.y, quat.z, quat.w) = (0.696, 0.035, -0.008, 0.717)  # (self.bowl_quat[0], self.bowl_quat[1], self.bowl_quat[2], self.bowl_quat[3]) #  (0.699, -0.044, -0.085, 0.709)
         timeout = 1.0
         self.setOrientGoal(pos, quat, timeout)
         raw_input("Enter anything to start: ")
 
         print "MOVES8"
         (pos.x, pos.y, pos.z) = (self.bowl_pos[0] + -0.0210, self.bowl_pos[1] +  -0.0519, self.bowl_pos[2] +  0.410)
-        (quat.x, quat.y, quat.z, quat.w) = (0.740, 0.052, -0.100, 0.663)  # (self.bowl_quat[0], self.bowl_quat[1], self.bowl_quat[2], self.bowl_quat[3]) #  (0.677, -0.058, -0.004, 0.733)
+        (quat.x, quat.y, quat.z, quat.w) = (0.696, 0.035, -0.008, 0.717)  # (self.bowl_quat[0], self.bowl_quat[1], self.bowl_quat[2], self.bowl_quat[3]) #  (0.677, -0.058, -0.004, 0.733)
         timeout = 1.0
         self.setOrientGoal(pos, quat, timeout)
         raw_input("Enter anything to start: ")
@@ -214,4 +253,3 @@ if __name__ == '__main__':
     # (quat.x, quat.y, quat.z, quat.w) = (0.677, -0.058, -0.004, 0.733)
     # timeout = 1.0
     # self.setOrientGoal(pos, quat, timeout)
-    # raw_input("Enter anything to start: ")
--- hrl_feeding_task/src/hrl_feeding_task/hyder_files/bowl_location_publisher.py
+++ hrl_feeding_task/src/hrl_feeding_task/hyder_files/bowl_location_publisher.py
@@ -5,30 +5,38 @@ roslib.load_manifest("hrl_feeding_task")
 roslib.load_manifest("hrl_haptic_mpc")
 import rospy
 from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion
+import std_msgs.msg
 
 
 class bowlPublisher():
 	def __init__(self):
-		self.bowl_pub = rospy.Publisher('bowl_location', Pose, latch = True)
+		self.bowl_pub = rospy.Publisher('bowl_location', PoseStamped, latch = True)
 		rospy.init_node('bowl_location_publisher', anonymous = True)
 		self.rate = rospy.Rate(10)
+		self.i = 0
 
 	def publish(self):
 		while not rospy.is_shutdown():
+			hdr = std_msgs.msg.Header()
+			hdr.frame_id = '/torso_lift_link'
+			hdr.stamp = rospy.Time.now()
+			hdr.seq = self.i
+
 			position = Point()
 			orientation = Quaternion()
 
 			#Actual bowl location, set manually... 
-			position.x, position.y, position.z = 0.766, 0.333, -0.287
+			position.x, position.y, position.z = 0.692, 0.802, -0.313
 			#ORIGINAL HARD CODED BOWL POSITION: 0.763, 0.592, -0.301
-			orientation.x, orientation.y, orientation.z, orientation.w = 0.686, 0.177, -0.141, 0.691
+			orientation.x, orientation.y, orientation.z, orientation.w = 0.528, 0.469, -0.006, 0.708
 			#ORIGINAL HARD CODE BOWL ORIENTATION:  0.706, 0.068, -0.235, 0.664
-			pose_msg = Pose(position, orientation)
+			pose_msg = PoseStamped( header = hdr, pose =  Pose(position, orientation) )
 			rospy.loginfo(pose_msg)
 
 			#self.pose_msg = PoseStamped(header = self.hdr, pose = Pose(self.pos, self.quat))
 
 			self.bowl_pub.publish(pose_msg)
+			self.i = self.i + 1
 			self.rate.sleep()
 
 
