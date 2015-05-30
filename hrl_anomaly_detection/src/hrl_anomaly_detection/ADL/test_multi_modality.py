01277373c01c822e38eaaa96a7e0f020701e7ebc
dpark <dpark@brain.(none)>
2015-05-30 02:20:43 -0400

    modified:   test_multi_modality.py

--- hrl_anomaly_detection/src/hrl_anomaly_detection/ADL/test_multi_modality.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/ADL/test_multi_modality.py
@@ -110,7 +110,7 @@ def fig_roc_sim(test_title, cross_data_path, nDataSet, onoff_type, check_methods
                     elif hcu.is_file(cross_test_path, mutex_file_part): continue
                     elif os.path.isfile(mutex_file): 
                         if rm_run == True:
-                            os.system('rm '+mutex_file)
+                            os.system('rm running*')
                         else:                           
                             continue
                     os.system('touch '+mutex_file)
