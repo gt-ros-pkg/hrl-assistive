c46b5fd9e01bdea1a3e6686969890aeaefd5dc46
Zackory <erickson.zack@uwlax.edu>
2015-06-22 15:28:09 -0400

    PointCloud2 reading updates.

--- hrl_multimodal_anomaly_detection/src/kanadeLucasPoint.py
+++ hrl_multimodal_anomaly_detection/src/kanadeLucasPoint.py
@@ -187,6 +187,7 @@ class kanadeLucasPoint:
         # Retrieve 3d location of feature from PointCloud2
         if self.pointCloud is None:
             print 'AHH! The PointCloud2 data is not available!'
+        print self.pointCloud.row_step, self.pointCloud.point_step, self.pointCloud.width, self.pointCloud.height
         points = pc2.read_points(self.pointCloud, field_names=('x', 'y', 'z'), skip_nans=False, uvs=[[x, y]])
         for point in points:
             px, py, depth = point
