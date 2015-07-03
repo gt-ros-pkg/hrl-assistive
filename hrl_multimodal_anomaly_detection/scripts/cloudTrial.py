b5d7d035019901d02028e24b655b5d0936f0fbad
Zackory <erickson.zack@uwlax.edu>
2015-07-03 09:18:31 -0400

    Trial of PointCloud retrieval time.

---
+++ hrl_multimodal_anomaly_detection/scripts/cloudTrial.py
@@ -0,0 +1,18 @@
+__author__ = 'zerickson'
+
+import time
+import rospy
+from sensor_msgs.msg import PointCloud2
+
+class cloudTrial:
+    def __init__(self):
+        self.cloudTime = time.time()
+        rospy.Subscriber('/head_mount_kinect/depth_registered/points', PointCloud2, self.cloudCallback)
+        rospy.spin()
+
+    def cloudCallback(self, data):
+        start = time.time()
+        print 'Time between cloud calls:', start - self.cloudTime
+
+if __name__ == '__main__':
+    cloudTrial()
