48ade55bd38ec35fe43fc908bd595572b4798099
Zackory <erickson.zack@uwlax.edu>
2015-07-04 15:57:42 -0400

    cloud data

--- hrl_multimodal_anomaly_detection/scripts/cloudTrial.py
+++ hrl_multimodal_anomaly_detection/scripts/cloudTrial.py
@@ -2,7 +2,7 @@
 
 import time
 import rospy
-from sensor_msgs.msg import PointCloud2
+from sensor_msgs.msg import PointCloud2, Image
 
 __author__ = 'zerickson'
 
@@ -10,12 +10,14 @@ class cloudTrial:
     def __init__(self):
         self.cloudTime = time.time()
         rospy.init_node('listener_cloud')
-        rospy.Subscriber('/head_mount_kinect/depth_registered/points', PointCloud2, self.cloudCallback)
+        # rospy.Subscriber('/head_mount_kinect/depth_registered/points', PointCloud2, self.cloudCallback)
+        rospy.Subscriber('/head_mount_kinect/ir/image', Image, self.cloudCallback)
         rospy.spin()
 
     def cloudCallback(self, data):
         print 'Time between cloud calls:', time.time() - self.cloudTime
         self.cloudTime = time.time()
+        print help(data)
 
 if __name__ == '__main__':
     cloudTrial()
