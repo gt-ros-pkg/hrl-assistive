c3612632977ffeb0a5f9e539d46d31050ee11576
Daehyung Park <deric.park@gatech.edu>
2015-07-08 15:45:55 -0700

    WORKING scooping steps times

--- hrl_multimodal_anomaly_detection/src/record_data.py
+++ hrl_multimodal_anomaly_detection/src/record_data.py
@@ -134,6 +134,7 @@ class ADL_log:
         data['head_position'] = self.headPos
 
         data['scooping_steps_times'] = self.scooping_steps_times
+	self.scooping_steps_times = []
 
         flag = raw_input('Enter trial\'s status (e.g. 1:success, 2:failure, 3: exit): ')
         if flag == '1': status = 'success'
@@ -169,10 +170,7 @@ class ADL_log:
         self.headPos = np.matrix([data.pose.position.x, data.pose.position.y, data.pose.position.z])
 
     def scoopingStepsTimesCallback(self, data):
-        print "RECORD DATA GOT SCOOPINGS STEPS TIMES CALLBACK!"
         self.scooping_steps_times.append(rospy.get_time() - self.init_time)
-        print "FROM RECORD DATA, SCOOPING STEPS TIMES = "
-        print self.scooping_steps_times
         return None_BoolResponse(True)
 
 if __name__ == '__main__':
