7ddc4836d32504a1d49e50a1a0b46d10d8c35390
Marc Killpack <mkillpack3@gatech.edu>
2015-07-16 20:24:54 -0400

    Updated for feeding some

--- hrl_multimodal_anomaly_detection/src/local_data_record.py
+++ hrl_multimodal_anomaly_detection/src/local_data_record.py
@@ -8,9 +8,9 @@ from hrl_multimodal_anomaly_detection.srv import String_String
 class dataRecord:
 
     AUDIO = False
-    FT = True
-    VISION = True
-    KINEMATICS = True
+    FT = False
+    VISION = False
+    KINEMATICS = False
 
 
     def __init__(self):
@@ -18,6 +18,8 @@ class dataRecord:
         rospy.wait_for_service("/arm_reach_enable")
         self.armReachAction = rospy.ServiceProxy("/arm_reach_enable", String_String)
         rospy.loginfo("arm reach server connected!!")
+        #This service takes requests in the form of string commands
+        #Refer to arm_reacher_feeding.py for list of commands
 
         subject = raw_input("Enter subject name: ")
         self.task = raw_input("Run scooping, feeding, or exit? [s/f/x] ")
