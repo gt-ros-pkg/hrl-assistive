6764f05b1e57d81b8b60aa0ad7d23e917253e7b4
Zackory <erickson.zack@uwlax.edu>
2015-07-29 13:05:07 -0400

    Online detection with Kinematics

--- hrl_multimodal_anomaly_detection/src/onlineAnomalyDetection.py
+++ hrl_multimodal_anomaly_detection/src/onlineAnomalyDetection.py
@@ -232,13 +232,13 @@ class onlineAnomalyDetection(Thread):
 
         if index < len(self.forces):
             distanceDiff = np.abs(self.distances[index] - distance)
-            if distanceDiff > 0.6:
+            if distanceDiff > 0.5:
                 if distance < self.distances[index]:
                     distance -= distanceDiff
                 else:
                     distance += distanceDiff
             angleDiff = np.abs(self.angles[index] - angle)
-            if angleDiff > 0.6:
+            if angleDiff > 0.5:
                 if angle < self.angles[index]:
                     angle -= angleDiff
                 else:
