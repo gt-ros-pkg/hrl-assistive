302c92a39c31c4eaaeecfa2744d4c0a9c4ac1b12
Zackory <erickson.zack@uwlax.edu>
2015-07-28 22:30:35 -0400

    Original head position

--- hrl_multimodal_anomaly_detection/src/manual_bowl_head_pose_publisher.py
+++ hrl_multimodal_anomaly_detection/src/manual_bowl_head_pose_publisher.py
@@ -50,7 +50,7 @@ class bowlPublisher:
 
         (self.head_pose_manual.pose.position.x,
             self.head_pose_manual.pose.position.y,
-            self.head_pose_manual.pose.position.z) = (0.797, -0.365, 0.197) # Mannequin: 0.742, -0.450, -0.049
+            self.head_pose_manual.pose.position.z) = (0.797, -0.365, 0.097) # Mannequin: 0.742, -0.450, -0.049 | 0.797, -0.365, 0.097
 
         (self.head_pose_manual.pose.orientation.x,
             self.head_pose_manual.pose.orientation.y,
