15ac5481f9b03cadd5149a00c53d0944d03a6042
Zackory <erickson.zack@uwlax.edu>
2015-07-29 20:07:27 -0400

    Online feeding anomaly detection

--- hrl_multimodal_anomaly_detection/src/onlineAnomalyDetection.py
+++ hrl_multimodal_anomaly_detection/src/onlineAnomalyDetection.py
@@ -86,7 +86,7 @@ class onlineAnomalyDetection(Thread):
 
         self.soundHandle = SoundClient()
 
-        self.hmm, self.minVals, self.maxVals, self.forces, self.distances, self.angles, self.pdfs, self.times, self.forcesList, self.distancesList, self.anglesList, self.pdfList, self.timesList = onlineHMM.setupMultiHMM()
+        self.hmm, self.minVals, self.maxVals, self.forces, self.distances, self.angles, self.pdfs, self.times, self.forcesList, self.distancesList, self.anglesList, self.pdfList, self.timesList = onlineHMM.setupMultiHMM(isScooping=self.isScooping)
         # self.forces, self.distances, self.angles, self.pdfs = self.forcesList[1], self.distancesList[1], self.anglesList[1], self.pdfList[1]
         self.times = np.array(self.times)
         self.anomalyOccured = False
