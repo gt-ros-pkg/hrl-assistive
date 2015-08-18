015a9dc51cb30f19ded6943da5ecf2e9b5f48a0e
Zackory <erickson.zack@uwlax.edu>
2015-08-18 11:49:58 -0500

    Decreased tool audio chunk size

--- hrl_multimodal_anomaly_detection/src/hrl_multimodal_anomaly_detection/audio/tool_audio_slim.py
+++ hrl_multimodal_anomaly_detection/src/hrl_multimodal_anomaly_detection/audio/tool_audio_slim.py
@@ -6,7 +6,7 @@ from threading import Thread
 
 class tool_audio_slim(Thread):
     MAX_INT = 32768.0
-    CHUNK   = 4096 # frame per buffer
+    CHUNK   = 1024 # frame per buffer
     RATE    = 44100 # sampling rate
     UNIT_SAMPLE_TIME = 1.0 / float(RATE)
     CHANNEL = 2 # number of channels
