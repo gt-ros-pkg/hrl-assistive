27cd98c7ccec7aee7eb0d55e55fb8a733ad7c593
Zackory <erickson.zack@uwlax.edu>
2015-08-26 09:42:40 -0500

    Revert

--- hrl_multimodal_anomaly_detection/src/hrl_multimodal_anomaly_detection/hmm/launcher_4d.py
+++ hrl_multimodal_anomaly_detection/src/hrl_multimodal_anomaly_detection/hmm/launcher_4d.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import os, sys
 import math
 import struct
 import numpy as np
@@ -7,6 +8,7 @@ import cPickle as pickle
 from scipy import interpolate
 import matplotlib.pyplot as plt
 from sklearn import preprocessing
+from contextlib import contextmanager
 from plotGenerator import plotGenerator
 from mvpa2.datasets.base import Dataset
 from learning_hmm_multi_1d import learning_hmm_multi_1d
@@ -17,48 +19,18 @@ import roslib
 roslib.load_manifest('hrl_multimodal_anomaly_detection')
 import tf
 
-from contextlib import contextmanager
-import ctypes
-import io
-import os, sys
-import tempfile
-
-libc = ctypes.CDLL(None)
-c_stdout = ctypes.c_void_p.in_dll(libc, 'stdout')
-
 @contextmanager
 def suppress_output():
-    stream = io.BytesIO()
-    # The original fd stdout points to. Usually 1 on POSIX systems.
-    original_stdout_fd = sys.stdout.fileno()
-
-    def _redirect_stdout(to_fd):
-        """Redirect stdout to the given file descriptor."""
-        # Flush the C-level buffer stdout
-        libc.fflush(c_stdout)
-        # Flush and close sys.stdout - also closes the file descriptor (fd)
-        sys.stdout.close()
-        # Make original_stdout_fd point to the same file as to_fd
-        os.dup2(to_fd, original_stdout_fd)
-        # Create a new sys.stdout that points to the redirected fd
-        sys.stdout = io.TextIOWrapper(os.fdopen(original_stdout_fd, 'wb'))
-
-    # Save a copy of the original stdout fd in saved_stdout_fd
-    saved_stdout_fd = os.dup(original_stdout_fd)
-    try:
-        # Create a temporary file and redirect stdout to it
-        tfile = tempfile.TemporaryFile(mode='w+b')
-        _redirect_stdout(tfile.fileno())
-        # Yield to caller, then redirect stdout back to the saved fd
-        yield
-        _redirect_stdout(saved_stdout_fd)
-        # Copy contents of temporary file to the given stream
-        tfile.flush()
-        tfile.seek(0, io.SEEK_SET)
-        stream.write(tfile.read())
-    finally:
-        tfile.close()
-        os.close(saved_stdout_fd)
+    with open(os.devnull, "w") as devnull:
+        old_stdout = sys.stdout
+        old_stderr = sys.stderr
+        sys.stdout = devnull
+        sys.stderr = devnull
+        try:
+            yield
+        finally:
+            sys.stdout = old_stdout
+            sys.stderr = old_stderr
 
 def forceKinematics(fileName):
     with open(fileName, 'rb') as f:
