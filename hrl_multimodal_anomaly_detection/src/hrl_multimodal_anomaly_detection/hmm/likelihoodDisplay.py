531427d675c3cf6fe207e01a08f2d8089d6f4f2e
Zackory <erickson.zack@uwlax.edu>
2015-09-01 23:43:54 -0500

    Update to handle new and old formats for online data within likelihood animation

--- hrl_multimodal_anomaly_detection/src/hrl_multimodal_anomaly_detection/hmm/likelihoodDisplay.py
+++ hrl_multimodal_anomaly_detection/src/hrl_multimodal_anomaly_detection/hmm/likelihoodDisplay.py
@@ -8,6 +8,7 @@ import icra2015Batch as onlineHMM
 import matplotlib.animation as animation
 
 fileName = '/home/dpark/git/hrl-assistive/hrl_multimodal_anomaly_detection/src/hrl_multimodal_anomaly_detection/onlineDataRecordings/t2_f_success.pkl'
+isNewFormat = False
 
 parts = fileName.split('/')[-1].split('_')
 subject = parts[0]
@@ -25,30 +26,36 @@ with open(fileName, 'rb') as f:
     audiosRaw = data['audioRaw']
     times = data['times']
     anomalyOccured = data['anomalyOccured']
+    if isNewFormat:
+        minThresholds = data['minThreshold']
+        likelihoods = data['likelihoods']
 
-# Predefined settings
-downSampleSize = 100 #200
-scale = 1.0 #10
-nState = 10 #20
-cov_mult = 5.0
-cutting_ratio = [0.0, 0.7] #[0.0, 0.7]
-isScooping = task == 's' or task == 'b'
-if isScooping: ml_thres_pkl='ml_scooping_thres.pkl'
-else: ml_thres_pkl='ml_feeding_thres.pkl'
+if isNewFormat:
+    ll_likelihood = [x[0] for x in likelihoods]
+else:
+    # Predefined settings
+    downSampleSize = 100 #200
+    scale = 1.0 #10
+    nState = 10 #20
+    cov_mult = 5.0
+    cutting_ratio = [0.0, 0.7] #[0.0, 0.7]
+    isScooping = task == 's' or task == 'b'
+    if isScooping: ml_thres_pkl='ml_scooping_thres.pkl'
+    else: ml_thres_pkl='ml_feeding_thres.pkl'
 
-saveDataPath = '/home/dpark/git/hrl-assistive/hrl_multimodal_anomaly_detection/src/hrl_multimodal_anomaly_detection/hmm/batchDataFiles/%s_%d_%d_%d_%d.pkl'
-# Setup HMM to perform online anomaly detection
-hmm, minVals, maxVals, minThresholds \
-= onlineHMM.iteration(downSampleSize=downSampleSize,
-                      scale=scale, nState=nState,
-                      cov_mult=cov_mult, verbose=False,
-                      isScooping=isScooping, use_pkl=False,
-                      train_cutting_ratio=cutting_ratio,
-                      findThresholds=True, ml_pkl=ml_thres_pkl,
-                      savedDataFile=saveDataPath % (('scooping' if isScooping else 'feeding'),
-                                    downSampleSize, scale, nState, int(cov_mult)))
+    saveDataPath = '/home/dpark/git/hrl-assistive/hrl_multimodal_anomaly_detection/src/hrl_multimodal_anomaly_detection/hmm/batchDataFiles/%s_%d_%d_%d_%d.pkl'
+    # Setup HMM to perform online anomaly detection
+    hmm, minVals, maxVals, minThresholds \
+    = onlineHMM.iteration(downSampleSize=downSampleSize,
+                          scale=scale, nState=nState,
+                          cov_mult=cov_mult, verbose=False,
+                          isScooping=isScooping, use_pkl=False,
+                          train_cutting_ratio=cutting_ratio,
+                          findThresholds=True, ml_pkl=ml_thres_pkl,
+                          savedDataFile=saveDataPath % (('scooping' if isScooping else 'feeding'),
+                                        downSampleSize, scale, nState, int(cov_mult)))
 
-ll_likelihood, ll_state_idx, ll_likelihood_mu, ll_likelihood_std = hmm.allLikelihoods(forces, distances, angles, audios)
+    ll_likelihood, ll_state_idx, ll_likelihood_mu, ll_likelihood_std = hmm.allLikelihoods(forces, distances, angles, audios)
 
 print 'Times length:', len(times), 'Likelihood length:', len(ll_likelihood)
 
@@ -76,9 +83,9 @@ def init():
     return line,
 
 interval = 1000 / len(ll_likelihood) * times[-1]
-fps = times[-1] / len(ll_likelihood)
+fps = int(len(ll_likelihood) / times[-1])
 print 'Max time:', times[-1], 'Interval:', interval, 'FPS:', fps
-ani = animation.FuncAnimation(fig, animate, np.arange(1, len(ll_likelihood)), init_func=init, interval=interval, blit=True)
+ani = animation.FuncAnimation(fig, animate, np.arange(1, len(ll_likelihood)), init_func=init, interval=25, blit=True)
 ani.save(time.strftime('likelihood_%m-%d-%Y_%H-%M-%S.mp4'), fps=fps)
 # plt.show()
 
