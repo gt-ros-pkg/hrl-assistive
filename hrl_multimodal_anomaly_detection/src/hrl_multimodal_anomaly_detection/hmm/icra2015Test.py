7adcb15a3dc27e508bc67772193007d55f3576c7
pidipidi <deric.park@gatech.edu>
2015-09-12 01:25:40 -0400

    test

--- hrl_multimodal_anomaly_detection/src/hrl_multimodal_anomaly_detection/hmm/icra2015Test.py
+++ hrl_multimodal_anomaly_detection/src/hrl_multimodal_anomaly_detection/hmm/icra2015Test.py
@@ -1697,10 +1697,10 @@ if __name__ == '__main__':
         check_methods  = ['change', 'global', 'progress']        
         data_root_path = '/home/dpark/svn/robot1/src/projects/anomaly/feeding'
         data_target_path = '/home/dpark/hrl_file_server/dpark_data/anomaly/ICRA2016'
-        kFold = 10
+        kFold = 6
         anomaly_offset = 0.0 #only for progress?
         cutting_ratio  = [0.0, 0.8] #[0.0, 0.7]        
-        downSampleSize = 120        
+        downSampleSize = 100        
         ## threshold_mult = (np.logspace(-0.5, 1.0, 30, endpoint=True) -0.0)
         nDataSet = None
         nThres   = 30
