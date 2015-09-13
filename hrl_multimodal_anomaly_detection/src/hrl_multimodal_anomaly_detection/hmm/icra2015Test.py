a33696454ed429730d210561682498399bc270c9
pidipidi <deric.park@gatech.edu>
2015-09-12 21:31:28 -0400

    test

--- hrl_multimodal_anomaly_detection/src/hrl_multimodal_anomaly_detection/hmm/icra2015Test.py
+++ hrl_multimodal_anomaly_detection/src/hrl_multimodal_anomaly_detection/hmm/icra2015Test.py
@@ -1781,7 +1781,7 @@ if __name__ == '__main__':
         data_target_path = '/home/dpark/hrl_file_server/dpark_data/anomaly/ICRA2016'
         kFold = 6
         anomaly_offset = 0.0 #only for progress?
-        cutting_ratio  = [0.0, 0.85] #[0.0, 0.7]        
+        cutting_ratio  = [0.0, 0.9] #[0.0, 0.7]        
         downSampleSize = 120        
         ## threshold_mult = (np.logspace(-0.5, 1.0, 30, endpoint=True) -0.0)
         nDataSet = 6
