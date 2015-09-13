454e6a4c645137945d66a9be5a8a1e676801ebb0
pidipidi <deric.park@gatech.edu>
2015-09-12 20:02:27 -0400

    test

--- hrl_multimodal_anomaly_detection/src/hrl_multimodal_anomaly_detection/hmm/icra2015Test.py
+++ hrl_multimodal_anomaly_detection/src/hrl_multimodal_anomaly_detection/hmm/icra2015Test.py
@@ -1780,7 +1780,7 @@ if __name__ == '__main__':
         ## threshold_mult = (np.logspace(-0.5, 1.0, 30, endpoint=True) -0.0)
         nDataSet = 10
         nThres   = 100
-        nState   = 8
+        nState   = 12
         cov_mult = 5.0
         tot_data = None
 
