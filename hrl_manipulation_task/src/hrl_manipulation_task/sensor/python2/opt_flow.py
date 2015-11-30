2b149822d8debccae6d52546866fe41edd1202cc
dpark <dpark@pidi8.(none)>
2015-11-30 03:11:26 -0500

    modified:   opt_flow.py

--- hrl_manipulation_task/src/hrl_manipulation_task/sensor/python2/opt_flow.py
+++ hrl_manipulation_task/src/hrl_manipulation_task/sensor/python2/opt_flow.py
@@ -14,7 +14,9 @@ Keys:
  2 - toggle glitch
 
 '''
+init_center = None
 last_center = None
+last_label  = None
 color_list = [[0,0,255],
               [255,0,0],
               [0,255,0],
@@ -23,7 +25,7 @@ for i in xrange(10):
     color_list.append([random.randint(0,255),
                        random.randint(0,255),
                        random.randint(0,255) ])
-
+cluster_centers = None
 
 def draw_flow(img, flow, step=16):
     h, w = img.shape[:2]
@@ -37,8 +39,11 @@ def draw_flow(img, flow, step=16):
         cv2.circle(vis, (x1, y1), 1, (0, 255, 0), -1)
     return vis
 
-def draw_clustered_flow(img, flow):
-    global last_center, color_list
+def draw_clustered_flow(img, flow, counter):
+    global last_center, last_label, color_list
+    global init_center
+    global cluster_centers
+    
     
     h, w = flow.shape[:2]
 
@@ -52,27 +57,92 @@ def draw_clustered_flow(img, flow):
     ## end = time.clock()
     ## print "%.2gs" % (end-start)
 
-    n_clusters = 10
+    n_clusters = 3
     if len(flow_filt) < n_clusters: return img
 
     if last_center is not None:
-        clt = KMeans(n_clusters = n_clusters, init=last_center)
+        clt = KMeans(n_clusters = n_clusters, init=init_center)
     else:
         clt = KMeans(n_clusters = n_clusters)
     clt.fit(flow_filt)
-    last_center = clt.cluster_centers_
+    init_center = clt.cluster_centers_
+    
+    #----------------------------------------------------------
+    time_array = np.ones((n_clusters, 1))*counter
+    if cluster_centers is None:
+        cluster_centers = clt.cluster_centers_
+        ## cluster_centers = np.hstack([time_array, clt.cluster_centers_])
+    else:
+        cluster_centers = np.vstack([ cluster_centers, clt.cluster_centers_ ])
+        ## cluster_centers = np.vstack([ cluster_centers, np.hstack([time_array, clt.cluster_centers_]) ])
 
-    cluster_centers.append(clt.cluster_centers_)
+    if len(cluster_centers) > n_clusters*20:
+        cluster_centers = cluster_centers[-n_clusters*20:]
 
+    clt2 = KMeans(n_clusters = n_clusters)
+    clt2.fit(cluster_centers)
+
+    if last_label is None: 
+        last_center = clt.cluster_centers_[-n_clusters:].tolist()
+        last_label  = clt.labels_[-n_clusters:].tolist()
+        print "JJJJJJJJJJJJJJJJump"
+        return img
+    cur_centers = clt.cluster_centers_[-n_clusters:]
+    cur_labels  = clt.labels_[-n_clusters:]
+
+    max_label = max(last_label)
+    for ii, (center, label) in enumerate(zip(cur_centers, cur_labels)):
+        min_dist = 1000
+        min_label= 0
+        min_idx  = 0
+        for jj, (c, l) in enumerate(zip(last_center, last_label)):
+            dist = np.linalg.norm(center-c)
+            if dist < min_dist:
+                min_dist = dist
+                min_label= l
+                min_idx  = jj
+
+        print ii, min_dist
+        # new label
+        if min_dist > 300:
+            cur_labels[ii] = max_label+1
+            max_label += 1
+        else:
+            del last_center[min_idx]
+            del last_label[min_idx]
+            cur_labels[ii] = min_label
+
+    ######################### Update last centers and labels             
+    last_center = cur_centers.tolist()
+    last_label  = cur_labels.tolist()
+    print "-----------------"
+
+    # cluster center
     overlay = img.copy()
-    for ii, center in enumerate(clt.cluster_centers_):
+    for ii, (center, label) in enumerate(zip(cur_centers, cur_labels)):
         x = int(center[1])
         y = int(center[0])
-        c = color_list[ii]
-        cv2.circle(overlay, (x, y), 8, (c[0], c[1], int(c[2]*center[2])), -1)
+        color_idx = label if label < len(color_list) else label%len(color_list)
+        c = color_list[color_idx]
+        cv2.circle(overlay, (x, y), 8, (c[0], c[1], c[2]), -1)
+        ## cv2.circle(overlay, (x, y), 8, (c[0], c[1], int(c[2]*center[2])), -1)
 
-    opacity = 0.4
+    opacity = 0.5
     cv2.addWeighted(overlay, opacity, img, 1 - opacity, 0, img)
+
+    # moving direction
+    vis = img.copy()
+    for ii, center in enumerate(cur_centers):
+        x = int(center[1])
+        y = int(center[0])
+        flow = np.sum(flow_array[clt.labels_ == ii], axis=0)
+        lines = np.vstack([x, y, x+flow[0], y+flow[1]]).T.reshape(-1, 2, 2)
+        lines = np.int32(lines + 0.5)        
+        cv2.polylines(vis, lines, 0, (0, 255, 0))
+    
+    opacity = 1.0
+    cv2.addWeighted(vis, opacity, img, 1 - opacity, 0, img)
+
         
     return img
 
@@ -112,6 +182,8 @@ if __name__ == '__main__':
     show_glitch = False
     cur_glitch = prev.copy()
 
+    ## video = cv2.VideoWriter("temp.avi", -1, 1, (np.shape(prev)[0],np.shape(prev)[1]) )
+    counter = 0
     while True:
         ret, img = cam.read()
         img = cv2.resize(img, (0,0), fx=scale, fy=scale) 
@@ -120,8 +192,9 @@ if __name__ == '__main__':
         flow = cv2.calcOpticalFlowFarneback(prevgray, gray, 0.5, 3, 15, 3, 5, 1.2, 0)
         prevgray = gray
         
-        cluster_img = draw_clustered_flow(img, flow)
+        cluster_img = draw_clustered_flow(img, flow, counter)
         cv2.imshow('flow cluster', cluster_img)
+        ## video.write(cluster_img)
         
         ## cv2.imshow('flow', draw_flow(gray, flow))
         if show_hsv:
@@ -141,6 +214,7 @@ if __name__ == '__main__':
             if show_glitch:
                 cur_glitch = img.copy()
             print 'glitch is', ['off', 'on'][show_glitch]
-
+        counter += 1.0
             
     cv2.destroyAllWindows()
+    ## video.release()
