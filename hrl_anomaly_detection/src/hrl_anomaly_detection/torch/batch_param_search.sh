659d6d0e4be47d69a327ebf5f47ff4dd8669fed0
pidipidi <deric.park@gatech.edu>
2016-02-19 01:34:59 -0500

    modified:   batch_param_search.sh 	modified:   outputs/model.net 	modified:   run.lua 	modified:   viz.lua

--- hrl_anomaly_detection/src/hrl_anomaly_detection/torch/batch_param_search.sh
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/torch/batch_param_search.sh
@@ -2,14 +2,25 @@
 
 
 
-
+eta_list=(1e-2 1e-3 1e-4 1e-5 1e-6)
+etadecay_list=(1e-5 1e-6 1e-7)
+lambda_list=(0.5 1.0 1.5 2.0)
 timewindow_list=(2 4 6)
 
-for i in "${timewindow_list[@]}"
+
+for i in "${eta_list[@]}"
 do
+    for j in "${etadecay_list[@]}"
+    do
+        for k in "${lambda_list[@]}"
+        do
+            for l in "${timewindow_list[@]}"
+            do
 
-    FILENAME=$timewindow_list
+                FILENAME=./log/E_{i}_ED_{j}_L_{k}_TW_{l}.log
 
-    timeout 10s th run.lua -model three -midoutputsize 20 -midoutput2size 10 -outputsize 5 -lambda 0.5 -eta 1e-3 -etadecay 1e-5 -batchsize 16 -timewindow 4 >> $FILENAME.log
-
+                timeout 180s th run.lua -model three -midoutputsize 20 -midoutput2size 10 -outputsize 5 -lambda ${k} -eta ${i} -etadecay ${j} -batchsize 16 -timewindow ${l} >> $FILENAME
+            done
+        done
+    done
 done
