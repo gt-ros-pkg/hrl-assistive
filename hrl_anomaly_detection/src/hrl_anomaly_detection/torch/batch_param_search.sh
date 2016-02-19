74df222d7fbfdf847e235dc9daac9512419a4e31
pidipidi <deric.park@gatech.edu>
2016-02-19 01:42:37 -0500

    modified:   batch_param_search.sh

--- hrl_anomaly_detection/src/hrl_anomaly_detection/torch/batch_param_search.sh
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/torch/batch_param_search.sh
@@ -17,7 +17,7 @@ do
             for l in "${timewindow_list[@]}"
             do
 
-                FILENAME=./log/E_{i}_ED_{j}_L_{k}_TW_{l}.log
+                FILENAME=./log/E_${i}_ED_${j}_L_${k}_TW_${l}.log
 
                 timeout 180s th run.lua -model three -midoutputsize 20 -midoutput2size 10 -outputsize 5 -lambda ${k} -eta ${i} -etadecay ${j} -batchsize 16 -timewindow ${l} >> $FILENAME
             done
