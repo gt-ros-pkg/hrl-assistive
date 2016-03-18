dc3aa7bb38542e036652c359d2b3ab6e12250b66
pidipidi <deric.park@gatech.edu>
2016-03-18 00:10:32 -0400

    modified:   rss2016test.py

--- hrl_anomaly_detection/src/hrl_anomaly_detection/rss2016test.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/rss2016test.py
@@ -2084,9 +2084,9 @@ if __name__ == '__main__':
     # Dectection TEST 
     local_range    = 10.0    
 
-    if False:
+    if True:
         ## subjects = ['gatsbii']
-        subjects = ['Wonyoung', 'Tom', 'lin', 'Ashwin', 'Song']
+        subjects = ['Wonyoung', 'Tom', 'lin', 'Ashwin', 'Song', 'Henry']
         task     = 'scooping'    
         ## feature_list = ['unimodal_ftForce', 'crossmodal_targetEEDist', \
         ##                 'crossmodal_targetEEAng']
@@ -2110,7 +2110,7 @@ if __name__ == '__main__':
         raw_data_path  = '/home/dpark/hrl_file_server/dpark_data/anomaly/RSS2016/'
 
         data_param_dict= {'renew': opt.bDataRenew, 'rf_center': rf_center, 'local_range': local_range,\
-                          'downSampleSize': 200, 'cut_data': [0,200], 'nNormalFold':2, 'nAbnormalFold':2,\
+                          'downSampleSize': 200, 'cut_data': [0,150], 'nNormalFold':2, 'nAbnormalFold':2,\
                           'feature_list': feature_list, 'nAugment': 0, 'lowVarDataRemv': False}
         AE_param_dict  = {'renew': False, 'switch': False, 'time_window': 4, 'filter': True, \
                           'layer_sizes':[64,32,16], 'learning_rate':1e-6, 'learning_rate_decay':1e-6, \
