2193f6243a0f4bde913ae81adbabea65fc6d0f33
pidipidi <deric.park@gatech.edu>
2016-03-23 11:08:29 -0400

    modified:   rss2016test.py

--- hrl_anomaly_detection/src/hrl_anomaly_detection/rss2016test.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/rss2016test.py
@@ -2254,7 +2254,7 @@ if __name__ == '__main__':
 
         nPoints        = 20
         ROC_param_dict = {'methods': ['progress_time_cluster', 'svm','fixed'],\
-                          'update_list': [],\
+                          'update_list': ['progress_time_cluster'],\
                           'nPoints': nPoints,\
                           'progress_param_range':-np.linspace(0.8, 6, nPoints), \
                           'svm_param_range': np.logspace(-4, 1.2, nPoints),\
