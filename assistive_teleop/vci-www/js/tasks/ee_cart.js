9d29675b16343a38128be79eb517a021ae318e9d
Phillip Grice <phillip.grice@gatech.edu>
2016-04-07 09:43:29 -0400

    moving gripper 3d display to separate file

--- assistive_teleop/vci-www/js/tasks/ee_cart.js
+++ assistive_teleop/vci-www/js/tasks/ee_cart.js
@@ -27,18 +27,16 @@ RFH.CartesianEEControl = function (options) {
     self.mode = "table"; // "wall", "free"
     self.active = false;
     self.$viewer = $('#viewer-canvas').css('zIndex',1);
-//    var goalMarkerGeom = new THREE.SphereGeometry(0.01,10,10);
-//    var goalMarkerMat = new THREE.MeshBasicMaterial({color: 0x208840});
-//    self.goalMarker = new THREE.Mesh(goalMarkerGeom, goalMarkerMat);
-//    self.goalMarker.transparent = true;
-//    self.goalMarker.opacity=0.45;
-//    RFH.viewer.scene.add(self.goalMarker);
 
     self.pixel23d = new RFH.Pixel23DClient({
         ros: ros,
         cameraInfoTopic: self.camera.infoTopic,
         serviceName: '/pixel_2_3d'
     });
+
+    self.gripperDisplay = new RFH.EEDisplay({side: self.side,
+                                             ros: ros,
+                                             tfClient: self.tfClient});
 
     $('#touchspot-toggle, #toward-button, #away-button').button();
     self.$pickAndPlaceButton = $('.'+self.side[0]+'-arm-ctrl.pick-and-place').button();
@@ -124,112 +122,6 @@ RFH.CartesianEEControl = function (options) {
     };
     $('.camera-swing.'+self.side[0]+'-arm-ctrl').button().on('click.rfh', cameraSwing);
 
-
-    /*////////////  Load Gripper Model ////////////*/
-    var colladaLoadProgress = function (data) {
-        console.log("Loading Collada Mesh: ", data.loaded/data.total);
-    };
-
-//    var gripperMaterial = new THREE.MeshLambertMaterial();
-    var gripperMaterial = new THREE.MeshBasicMaterial();
-    gripperMaterial.transparent = true;
-    gripperMaterial.opacity = 0.25;
-    gripperMaterial.depthTest = true;
-    gripperMaterial.depthWrite = true;
-    gripperMaterial.color.setRGB(1.6,1.6,1.6);
-
-    var updateGripperPalmTF = function (tf) {
-        palmMesh.position.set(tf.translation.x, tf.translation.y, tf.translation.z);
-        palmMesh.quaternion.set(tf.rotation.x, tf.rotation.y, tf.rotation.z, tf.rotation.w);
-        RFH.viewer.renderer.render(RFH.viewer.scene, RFH.viewer.camera);
-    };
-
-    var updateGripperFingerTF = function (side, tf) {
-        if (side === 'r') {
-            rFingerMesh.position.set(tf.translation.x, tf.translation.y, tf.translation.z);
-            var quat = new THREE.Quaternion(tf.rotation.x, tf.rotation.y, tf.rotation.z, tf.rotation.w);
-            var qx_rot = new THREE.Quaternion(1,0,0,0);
-            quat = quat.multiplyQuaternions(quat, qx_rot);
-            rFingerMesh.quaternion.set(quat.x, quat.y, quat.z, quat.w);
-        } else {
-            lFingerMesh.position.set(tf.translation.x, tf.translation.y, tf.translation.z);
-            lFingerMesh.quaternion.set(tf.rotation.x, tf.rotation.y, tf.rotation.z, tf.rotation.w);
-        };
-        RFH.viewer.renderer.render(RFH.viewer.scene, RFH.viewer.camera);
-    };
-
-    var updateGripperFingerTipTF = function (side, tf) {
-        if (side === 'r') {
-            rFingerTipMesh.position.set(tf.translation.x, tf.translation.y, tf.translation.z);
-            var quat = new THREE.Quaternion(tf.rotation.x, tf.rotation.y, tf.rotation.z, tf.rotation.w);
-            var qx_rot = new THREE.Quaternion(1,0,0,0);
-            quat = quat.multiplyQuaternions(quat, qx_rot);
-            rFingerTipMesh.quaternion.set(quat.x, quat.y, quat.z, quat.w);
-        } else {
-            lFingerTipMesh.position.set(tf.translation.x, tf.translation.y, tf.translation.z);
-            lFingerTipMesh.quaternion.set(tf.rotation.x, tf.rotation.y, tf.rotation.z, tf.rotation.w);
-        };
-        RFH.viewer.renderer.render(RFH.viewer.scene, RFH.viewer.camera);
-
-    };
-        
-    var palmMesh = new THREE.Mesh();
-    var lFingerMesh = new THREE.Mesh();
-    var rFingerMesh = new THREE.Mesh();
-    var lFingerTipMesh = new THREE.Mesh();
-    var rFingerTipMesh = new THREE.Mesh();
-
-    var palmOnLoad = function (collada) {
-        // Set transforms + callback update
-        var palmGeom = collada.dae.geometries.palm3_M1000Shape.mesh.geometry3js.clone();
-        palmMesh.geometry = palmGeom;
-        palmMesh.material = gripperMaterial;
-        palmMesh.scale.set(0.1, 0.1, 0.1);
-        RFH.viewer.scene.add(palmMesh);
-        self.tfClient.subscribe(self.side[0]+'_gripper_palm_link', updateGripperPalmTF);
-    }
-    var fingerOnLoad = function (collada) {
-        // Set transforms + callback update
-        var fingerGeom = collada.dae.geometries.finger_M2K_modShape.mesh.geometry3js.clone();
-        lFingerMesh = new THREE.Mesh(fingerGeom, gripperMaterial);
-        lFingerMesh.scale.set(0.1, 0.1, 0.1);
-        rFingerMesh = new THREE.Mesh(fingerGeom, gripperMaterial);
-        rFingerMesh.scale.set(0.1, 0.1, 0.1);
-        RFH.viewer.scene.add(rFingerMesh);
-        RFH.viewer.scene.add(lFingerMesh);
-        self.tfClient.subscribe(self.side[0]+'_gripper_l_finger_link', function(tf){updateGripperFingerTF('l', tf)});
-        self.tfClient.subscribe(self.side[0]+'_gripper_r_finger_link', function(tf){updateGripperFingerTF('r', tf)});
-    }
-    var fingerTipOnLoad = function (collada) {
-        // Set transforms + callback update
-        var fingerTipGeom =collada.dae.geometries.finger_tip_MShape.mesh.geometry3js.clone();
-        lFingerTipMesh = new THREE.Mesh(fingerTipGeom, gripperMaterial);
-        lFingerTipMesh.scale.set(0.1, 0.1, 0.1);
-        rFingerTipMesh = new THREE.Mesh(fingerTipGeom, gripperMaterial);
-        rFingerTipMesh.scale.set(0.1, 0.1, 0.1);
-        rFingerTipMesh.rotation.x = Math.PI;
-        RFH.viewer.scene.add(lFingerTipMesh);
-        RFH.viewer.scene.add(rFingerTipMesh);
-        self.tfClient.subscribe(self.side[0]+'_gripper_l_finger_tip_link', function (tf){updateGripperFingerTipTF('l', tf)});
-        self.tfClient.subscribe(self.side[0]+'_gripper_r_finger_tip_link', function (tf){updateGripperFingerTipTF('r', tf)});
-    }
-
-    var gripperColladaLoader = new THREE.ColladaLoader();
-    gripperColladaLoader.load('./data/gripper_model/gripper_palm.dae', palmOnLoad, colladaLoadProgress);
-    gripperColladaLoader.load('./data/gripper_model/l_finger.dae', fingerOnLoad, colladaLoadProgress);
-    gripperColladaLoader.load('./data/gripper_model/l_finger_tip.dae', fingerTipOnLoad, colladaLoadProgress);
-    /*////////////  END Load Gripper Model ////////////*/
-
-    var displayGoalPose = function (ps_msg) {
-        RFH.viewer.renderer.render(RFH.viewer.scene, RFH.viewer.camera);
-    };
-
-    var handGoalSubscriber = new ROSLIB.Topic({
-        ros: ros,
-        name: self.side + '_arm/haptic_mpc/goal_pose', 
-        messageType: 'geometry_msgs/PoseStamped'
-    });
-    handGoalSubscriber.subscribe(displayGoalPose);
 
     self.getStepSize = function () {
         return $('input[name=speedOption]:checked').attr('id');
@@ -752,7 +644,6 @@ RFH.CartesianEEControl = function (options) {
         $('#'+self.side[0]+'-posrot-pos').click();
         self.active = true;
         self.$viewer.show();
-        self.goalMarker.visible = true;
         self.updateCtrlRingViz();
     };
 
@@ -771,7 +662,6 @@ RFH.CartesianEEControl = function (options) {
         self.trackHand(false);
         self.active = false;
         self.rotationControl.hide();
-        self.goalMarker.visible = false;
 //        for (var dir in self.rotArrows) {
 //            self.rotArrows[dir].mesh.visible = false;
 //            self.rotArrows[dir].edges.visible = false;
