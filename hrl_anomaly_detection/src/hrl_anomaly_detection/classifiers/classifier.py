fixed symmetric KL-divergence floating error. 	modified:   classifiers/classifier.py 	modified:   rss2016test.py

hrl_anomaly_detection/src/hrl_anomaly_detection/classifiers/classifier.py
@@ -207,13 +207,12 @@ class classifier(learning_base):
                 nState = len(post)
                 ## c_time = float(nState - (min_index+1) )/float(nState) + 1.0
                 ## c_time = np.logspace(0,-0.9,nState)[min_index]
-                c_time = 1.0
 
                 if (type(self.ths_mult) == list or type(self.ths_mult) == np.ndarray or \
                     type(self.ths_mult) == tuple) and len(self.ths_mult)>1:
-                    err = (self.ll_mu[min_index] + c_time * self.ths_mult[min_index]*self.ll_std[min_index]) - logp - self.logp_offset
+                    err = (self.ll_mu[min_index] + self.ths_mult[min_index]*self.ll_std[min_index]) - logp - self.logp_offset
                 else:
-                    err = (self.ll_mu[min_index] + c_time * self.ths_mult*self.ll_std[min_index]) - logp - self.logp_offset
+                    err = (self.ll_mu[min_index] + self.ths_mult*self.ll_std[min_index]) - logp - self.logp_offset
                 l_err.append(err)
             return l_err
         
@@ -379,8 +378,7 @@ def symmetric_entropy(p,q):
     '''
     Return the sum of KL divergences
     '''
-
-    return min(entropy(p,q), entropy(q,p)) + 1e-6
+    return min(entropy(p,np.array(q)+1e-6), entropy(q,np.array(p)+1e-6)) + 1e-6
 
 
 def findBestPosteriorDistribution(post, l_statePosterior):
@@ -390,6 +388,7 @@ def findBestPosteriorDistribution(post, l_statePosterior):
 
     for j in xrange(len(l_statePosterior)):
         dist = symmetric_entropy(post, l_statePosterior[j])
+            
         if min_dist > dist:
             min_index = j
             min_dist  = dist

hrl_anomaly_detection/src/hrl_anomaly_detection/rss2016test.py
@@ -174,7 +174,7 @@ def likelihoodOfSequences(subject_names, task_name, raw_data_path, processed_dat
         print "-------------------------"
         return (-1,-1,-1,-1)
 
-    if decision_boundary_viz and False:
+    if decision_boundary_viz:
         testDataX = np.vstack([np.swapaxes(normalTestData, 0, 1), np.swapaxes(abnormalTestData, 0, 1)])
         testDataX = np.swapaxes(testDataX, 0, 1)
         testDataY = np.hstack([ -np.ones(len(normalTestData[0])), \
@@ -216,16 +216,15 @@ def likelihoodOfSequences(subject_names, task_name, raw_data_path, processed_dat
         # discriminative classifier
         if decision_boundary_viz:
             dtc = cf.classifier( method='progress_time_cluster', nPosteriors=nState, \
-                                 nLength=len(normalTestData[0,0]), ths_mult=-1.0 )
-            dtc.fit(X_train_org, Y_train_org, idx_train_org)
-
+                                 nLength=len(normalTestData[0,0]), ths_mult=0.0 )
+            dtc.fit(X_train_org, Y_train_org, idx_train_org, parallel=True)
 
     print "----------------------------------------------------------------------------"
     fig = plt.figure()
     min_logp = 0.0
     max_logp = 0.0
-    target_idx = 0
-        
+    target_idx = 1
+
     # training data
     if useTrain:
 
@@ -245,8 +244,7 @@ def likelihoodOfSequences(subject_names, task_name, raw_data_path, processed_dat
                     if j>=len(ll_logp[i]): continue
                     l_X = [ll_logp[i][j]] + ll_post[i][j].tolist()
 
-                    print dtc.predict(l_X), type(dtc.predict(l_X)), ll_logp[i][j]
-                    exp_logp = dtc.predict(l_X) + ll_logp[i][j]
+                    exp_logp = dtc.predict(l_X)[0] + ll_logp[i][j]
                     exp_log_ll[i].append(exp_logp)
 
 
@@ -270,7 +268,8 @@ def likelihoodOfSequences(subject_names, task_name, raw_data_path, processed_dat
         plt.plot(log_ll[target_idx], 'k-', lw=3.0)
         if decision_boundary_viz:
             plt.plot(exp_log_ll[target_idx], 'm-', lw=3.0)            
-                                             
+
+            
     # normal test data
     ## if useNormalTest and False:
 
@@ -1501,7 +1500,7 @@ if __name__ == '__main__':
             HMM_param_dict = {'renew': opt.bHMMRenew, 'nState': 25, 'cov': 4.0, 'scale': 8.0}
         if AE_param_dict['switch']:            
             SVM_param_dict = {'renew': False, 'w_negative': 6.0, 'gamma': 0.173, 'cost': 4.0}
-            HMM_param_dict = {'renew': opt.bHMMRenew, 'nState': 25, 'cov': 8.0, 'scale': 6.0}
+            HMM_param_dict = {'renew': opt.bHMMRenew, 'nState': 25, 'cov': 2.0, 'scale': 2.5}
         else:
             SVM_param_dict = {'renew': False, 'w_negative': 6.0, 'gamma': 0.173, 'cost': 4.0}
             HMM_param_dict = {'renew': opt.bHMMRenew, 'nState': 25, 'cov': 4.0, 'scale': 5.0}
@@ -1590,9 +1589,6 @@ if __name__ == '__main__':
 
 
     elif opt.bLikelihoodPlot:
-        threshold    = 0.0
-        smooth       = False
-
         likelihoodOfSequences(subjects, task, raw_data_path, save_data_path, param_dict,\
                               decision_boundary_viz=True, \
                               useTrain=True, useNormalTest=False, useAbnormalTest=True,\