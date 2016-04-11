9bf7b9c8c7b3627718a5d427042f9e15a6ae092d
pidipidi <deric.park@gatech.edu>
2016-04-10 23:30:45 -0400

    modified:   params.py

--- hrl_anomaly_detection/src/hrl_anomaly_detection/params.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/params.py
@@ -49,11 +49,9 @@ def getPushingMicrowave(task, data_renew, AE_renew, HMM_renew, rf_center,local_r
         HMM_param_dict = {'renew': HMM_renew, 'nState': 20, 'cov': 3.0, 'scale': 0.5}
         ## SVM_param_dict = {'renew': False, 'w_negative': 0.5, 'gamma': 0.334, 'cost': 4.0}
         ## HMM_param_dict = {'renew': HMM_renew, 'nState': 25, 'cov': 4.0, 'scale': 8.0}
-    elif AE_param_dict['switch'] and AE_param_dict['add_option'] is ['audioWristRMS']:            
+    elif AE_param_dict['switch'] and AE_param_dict['add_option'] is ['ftForce_mag']:            
         SVM_param_dict = {'renew': False, 'w_negative': 6.0, 'gamma': 0.173, 'cost': 4.0}
         HMM_param_dict = {'renew': HMM_renew, 'nState': 20, 'cov': 3.0, 'scale': 0.5}
-        ## SVM_param_dict = {'renew': False, 'w_negative': 0.5, 'gamma': 0.334, 'cost': 4.0}
-        ## HMM_param_dict = {'renew': HMM_renew, 'nState': 25, 'cov': 4.0, 'scale': 8.0}
     elif AE_param_dict['switch']:            
         SVM_param_dict = {'renew': False, 'w_negative': 3.0, 'gamma': 0.334, 'cost': 1.0}
         HMM_param_dict = {'renew': HMM_renew, 'nState': 20, 'cov': 2.0, 'scale': 2.0}
