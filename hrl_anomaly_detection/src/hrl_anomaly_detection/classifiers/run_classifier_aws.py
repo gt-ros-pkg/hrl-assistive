d86f78488e676a5ed97b33d8e4b869f7d27f290c
pidipidi <deric.park@gatech.edu>
2016-04-15 03:13:03 -0400

    modified:   classifiers/run_classifier_aws.py

--- hrl_anomaly_detection/src/hrl_anomaly_detection/classifiers/run_classifier_aws.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/classifiers/run_classifier_aws.py
@@ -473,9 +473,9 @@ if __name__ == '__main__':
         ##               'gamma': np.linspace(0.0001, 1.0, 4).tolist(), \
         ##               'w_negative': [0.5,3.0,6.0] }
         parameters = {'method': ['svm'], 'svm_type': [0], 'kernel_type': [2], \
-                      'cost': [1.,2.,3.,4.,5.],\
+                      'cost': [1.],\
                       'gamma': [2.0], \
-                      'w_negative': [6.0] }
+                      'w_negative': [1.0, 6.0, 10.0] }
 
     else:
         print "Selected task name is not available."
