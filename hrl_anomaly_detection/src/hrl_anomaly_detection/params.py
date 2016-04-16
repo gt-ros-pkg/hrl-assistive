8a9e892fd0050988978f768f858b4e6570e0d1b3
pidipidi <deric.park@gatech.edu>
2016-04-16 02:02:58 -0400

    modified:   params.py

--- hrl_anomaly_detection/src/hrl_anomaly_detection/params.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/params.py
@@ -202,8 +202,8 @@ def getPushingMicroWhite(task, data_renew, AE_renew, HMM_renew, rf_center,local_
     ROC_param_dict = {'methods': [ 'progress_time_cluster', 'svm' ],\
                       'update_list': [],\
                       'nPoints': nPoints,\
-                      'progress_param_range':np.linspace(-1., -10., nPoints), \
-                      'svm_param_range': np.logspace(-4, 1.2, nPoints),\
+                      'progress_param_range':np.linspace(10., -40., nPoints), \
+                      'svm_param_range': np.logspace(-1, 0.2, nPoints),\
                       'fixed_param_range': np.linspace(1.0, -3.0, nPoints),\
                       'cssvm_param_range': np.logspace(-4.0, 2.0, nPoints),
                       'svm1_param_range': np.logspace(-4, 1.2, nPoints)}        
