3400c12624e0f98177e794c1f23fcab30549d930
pidipidi <deric.park@gatech.edu>
2016-04-17 00:08:56 -0400

    modified:   classifiers/run_classifier_aws.py

--- hrl_anomaly_detection/src/hrl_anomaly_detection/classifiers/run_classifier_aws.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/classifiers/run_classifier_aws.py
@@ -465,7 +465,7 @@ if __name__ == '__main__':
         ROC_param_dict = {'methods': ['svm'],\
                           'nPoints': nPoints,\
                           'progress_param_range':np.linspace(-1., -10., nPoints), \
-                          'svm_param_range': np.logspace(-4, 1.2, nPoints),\
+                          'svm_param_range': np.logspace(-4, 0.1, nPoints),\
                           'fixed_param_range': np.linspace(1.0, -3.0, nPoints),\
                           'cssvm_param_range': np.logspace(-4, 1.2, nPoints) }
         param_dict['ROC'] = ROC_param_dict
