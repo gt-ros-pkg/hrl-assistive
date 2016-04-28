3a43d7754142951aa552f8aa261200cf919c3095
pidipidi <deric.park@gatech.edu>
2016-04-28 12:26:30 -0400

    Added different score sorting method in run_hmm_cpu

--- hrl_anomaly_detection/src/hrl_anomaly_detection/hmm/run_hmm_cpu.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/hmm/run_hmm_cpu.py
@@ -223,8 +223,8 @@ def tune_hmm(parameters, cv_dict, param_dict, processed_data_path, verbose=False
     idx_list = np.argsort(score_array)
 
     for i in idx_list:
-        print("%0.3f (+/-%0.03f) for %r"
-              % (mean_list[i], std_list[i], param_list[i]))
+        print("%0.3f : %0.3f (+/-%0.03f) for %r"
+              % (score_array[i], mean_list[i], std_list[i], param_list[i]))
 
 
     ## # Get sorted results
