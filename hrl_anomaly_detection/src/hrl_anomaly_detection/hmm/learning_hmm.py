9ade1f3280bd09c1fb02a5ad497f1fa39ec2714b
pidipidi <deric.park@gatech.edu>
2016-05-10 12:52:26 -0400

    modified:   hmm/learning_hmm.py 	modified:   onlineTest.py

--- hrl_anomaly_detection/src/hrl_anomaly_detection/hmm/learning_hmm.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/hmm/learning_hmm.py
@@ -216,6 +216,8 @@ class learning_hmm(learning_base):
         final_ts_obj = ghmm.EmissionSequence(self.F, X_test)
         (alpha, scale) = self.ml.forward(final_ts_obj)
         beta = self.ml.backward(final_ts_obj, scale)
+
+        
         
 
 
