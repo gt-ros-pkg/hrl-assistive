b2d3a4f93e18643eea70e6f31731083c18146dcf
pidipidi <deric.park@gatech.edu>
2016-05-10 12:37:00 -0400

    modified:   onlineTest.py

--- hrl_anomaly_detection/src/hrl_anomaly_detection/onlineTest.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/onlineTest.py
@@ -438,7 +438,7 @@ def run_classifiers(idx, save_data_path, task, method, ROC_data, ROC_dict, AE_di
                 ## sample_weight = np.linspace(0.,1.0,len(X_ptrain))
                 sample_weight = np.linspace(0.,1.0,len(X_ptrain))
                 sample_weight/=np.sum(sample_weight)
-                sample_weight += 0.5
+                sample_weight += 0.2
                 dtc.partial_fit(X_ptrain, Y_ptrain, sample_weight=sample_weight)
 
             # 2) test classifier
