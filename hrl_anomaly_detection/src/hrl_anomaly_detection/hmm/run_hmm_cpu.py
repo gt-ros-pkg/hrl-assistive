168fc90d160c42f31ef1734029f03f5b54b23b2d
pidipidi <deric.park@gatech.edu>
2016-05-25 22:51:37 -0400

    modified:   hmm/run_hmm_cpu.py

--- hrl_anomaly_detection/src/hrl_anomaly_detection/hmm/run_hmm_cpu.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/hmm/run_hmm_cpu.py
@@ -215,9 +215,10 @@ def tune_hmm(parameters, cv_dict, param_dict, processed_data_path, verbose=False
             new_abnorm_logp = [logp for logp in abnorm_logp if logp > 0.0]
 
             from scipy.stats import norm
-            score = 0.0; c1=1.0; c2=5.0
+            score = 0.0; c1=1.0; c2=5.0; c3=1.e+4
             score += c1*l_sig
             score += c2*np.sum([ norm.pdf(logp,loc=l_mu,scale=l_sig) for logp in new_abnorm_logp ])
+            score += c3/max_logp
 
             ## # score 1 - 
             ## diff_vals = -abnorm_logp + np.mean(norm_logp)
