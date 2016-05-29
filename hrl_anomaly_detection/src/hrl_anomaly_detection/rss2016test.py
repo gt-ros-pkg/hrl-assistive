d53c5518c175f8cf5f6c4ec3b9c9f8533f0e93a0
pidipidi <deric.park@gatech.edu>
2016-05-29 02:44:05 -0400

    modified:   rss2016test.py

--- hrl_anomaly_detection/src/hrl_anomaly_detection/rss2016test.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/rss2016test.py
@@ -940,6 +940,7 @@ def run_classifiers(idx, processed_data_path, task_name, method, ROC_data, ROC_d
         # flatten the data
         X_train_org, Y_train_org, _ = flattenSample(ll_classifier_train_X, \
                                                     ll_classifier_train_Y)
+        nState = 0
     else:
         if AE_dict['switch'] and AE_dict['add_option'] is not None:
             tag = ''
