a511a28042cc582708ba1b1e0d38002675e494f4
pidipidi <deric.park@gatech.edu>
2016-05-28 21:03:35 -0400

    modified:   params.py

--- hrl_anomaly_detection/src/hrl_anomaly_detection/params.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/params.py
@@ -319,7 +319,7 @@ def getPushingMicroBlack(task, data_renew, AE_renew, HMM_renew, rf_center,local_
                         'unimodal_audioWristRMS'] #'unimodal_audioPower', ,
         HMM_param_dict = {'renew': HMM_renew, 'nState': 35, 'cov': 7.75, 'scale': 5.5, \
                           'add_logp_d': True}
-        SVM_param_dict = {'renew': False, 'w_negative': 0.27, 'gamma': 2.7, 'cost': 3.0}
+        SVM_param_dict = {'renew': False, 'w_negative': 0.534, 'gamma': 1.444, 'cost': 0.825}
         ## SVM_param_dict = {'renew': False, 'w_negative': 0.27, 'gamma': 1.89, 'cost': 2.27}
         
         nPoints        = 20  # 'progress_time_cluster',,'fixed' , 'svm' , 
