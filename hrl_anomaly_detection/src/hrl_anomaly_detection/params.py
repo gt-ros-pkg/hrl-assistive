3879b7c0d29b0cdabc47c0a196b00860fa17055f
pidipidi <deric.park@gatech.edu>
2016-05-31 01:26:53 -0400

    modified:   hmm/run_hmm_cpu.py 	modified:   params.py

--- hrl_anomaly_detection/src/hrl_anomaly_detection/params.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/params.py
@@ -340,7 +340,7 @@ def getPushingMicroBlack(task, data_renew, AE_renew, HMM_renew, rf_center,local_
     elif dim == 2:
         handFeatures = ['unimodal_ftForce',\
                         'unimodal_audioWristRMS'] #'unimodal_audioPower', ,
-        HMM_param_dict = {'renew': HMM_renew, 'nState': 25, 'cov': 10.0, 'scale': 5.0}
+        HMM_param_dict = {'renew': HMM_renew, 'nState': 25, 'cov': 5.5, 'scale': 5.0}
         SVM_param_dict = {'renew': False, 'w_negative': 0.325, 'gamma': 2.0, 'cost': 5.0}
 
         nPoints        = 20  # 'progress_time_cluster',,'fixed' , 'svm' , 
