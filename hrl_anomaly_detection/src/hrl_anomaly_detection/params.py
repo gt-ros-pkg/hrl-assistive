ae19afb6261ef6fd253217022761afe30393f904
pidipidi <deric.park@gatech.edu>
2016-06-02 12:50:19 -0400

    modified:   params.py

--- hrl_anomaly_detection/src/hrl_anomaly_detection/params.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/params.py
@@ -99,7 +99,7 @@ def getPushingMicroWhite(task, data_renew, AE_renew, HMM_renew, rf_center,local_
                         'crossmodal_subArtagEEDist',\
                         'unimodal_audioWristRMS'] #'unimodal_audioPower', ,
         HMM_param_dict = {'renew': HMM_renew, 'nState': 25, 'cov': 1.577, 'scale': 4.0}
-        SVM_param_dict = {'renew': False, 'w_negative': 0.525, 'gamma': 1., 'cost': 5.0}
+        SVM_param_dict = {'renew': False, 'w_negative': 1.175, 'gamma': 0.507, 'cost': 7.5}
 
         nPoints        = 20  # 'progress_time_cluster',,'fixed' , 'svm' , 
         ROC_param_dict = {'methods': [ 'fixed', 'progress_time_cluster', 'svm','osvm' ],\
