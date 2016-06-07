d840857a5a69453c4aba55aa0ad5bbf563ce4a28
pidipidi <deric.park@gatech.edu>
2016-06-07 11:20:10 -0400

    modified:   run_classifier_aws.py

--- hrl_anomaly_detection/src/hrl_anomaly_detection/classifiers/run_classifier_aws.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/classifiers/run_classifier_aws.py
@@ -356,8 +356,8 @@ if __name__ == '__main__':
 
         nFiles = 5 #9
         parameters = {'method': ['osvm'], 'svm_type': [2], 'kernel_type': [2], \
-                      'pca_gamma': [1.0]
-                      'cost': [10.0]
+                      'pca_gamma': [1.0],
+                      'cost': [10.0],
                       'gamma': np.logspace(-2,2.0,5)                      
                       }
         ## parameters = {'method': ['svm'], 'svm_type': [0], 'kernel_type': [2], \
