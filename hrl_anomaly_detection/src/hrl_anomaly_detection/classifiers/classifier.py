c5cdb23c71f41d83a40e96954f4edd1a5edd71c1
pidipidi <deric.park@gatech.edu>
2016-06-07 10:03:40 -0400

    modified:   classifier.py

--- hrl_anomaly_detection/src/hrl_anomaly_detection/classifiers/classifier.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/classifiers/classifier.py
@@ -541,7 +541,7 @@ def run_classifier(j, X_train, Y_train, idx_train, X_test, Y_test, idx_test, \
         weights = ROC_dict['osvm_param_range']
         dtc.set_params( svm_type=2 )
         dtc.set_params( kernel_type=0 ) # temp
-        dtc.set_params( nu=0.5 )
+        dtc.set_params( nu=weights[j] )
         ## dtc.set_params( gamma=weights[j] )
         dtc.set_params( cost=1.0 )
         ret = dtc.fit(X_train, np.array(Y_train)*-1.0, parallel=False)
