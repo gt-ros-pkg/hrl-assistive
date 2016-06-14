afc3af736b31f7c85fa42c784211fb8d60d3a43d
pidipidi <deric.park@gatech.edu>
2016-06-13 23:00:34 -0400

    modified:   params.py

--- hrl_anomaly_detection/src/hrl_anomaly_detection/params.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/params.py
@@ -130,7 +130,7 @@ def getFeeding(task, data_renew, AE_renew, HMM_renew, rf_center,local_range, ae_
         handFeatures = ['unimodal_ftForce', \
                         'crossmodal_artagEEDist', 'crossmodal_artagEEAng']
         HMM_param_dict = {'renew': HMM_renew, 'nState': 25, 'cov': 3.13, 'scale': 5.0}
-        SVM_param_dict = {'renew': False, 'w_negative': 0.749, 'gamma': 0.1, 'cost': 4.0,\
+        SVM_param_dict = {'renew': False, 'w_negative': 0.749, 'gamma': 1.0, 'cost': 4.0,\
                           'hmmosvm_nu': 0.003}
 
         nPoints        = 20
