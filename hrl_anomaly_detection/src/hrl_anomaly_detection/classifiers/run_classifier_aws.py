b91047bf6c2fe3166b0ebb8761c6b96f1cde82ea
pidipidi <deric.park@gatech.edu>
2016-06-14 00:11:29 -0400

    modified:   classifiers/run_classifier_aws.py

--- hrl_anomaly_detection/src/hrl_anomaly_detection/classifiers/run_classifier_aws.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/classifiers/run_classifier_aws.py
@@ -489,7 +489,7 @@ if __name__ == '__main__':
                           'svm_param_range': np.logspace(-2, 0, nPoints),\
                           'fixed_param_range': np.linspace(1.0, -3.0, nPoints),\
                           'hmmosvm_param_range': np.logspace(-3.5, 0.5, nPoints),\
-                          'osvm_param_range': np.logspace(-4., 1.5, nPoints),\
+                          'osvm_param_range': np.logspace(-6., 1.5, nPoints),\
                           'cssvm_param_range': np.logspace(-4, 1.2, nPoints),\
                           'hmmsvm_diag_param_range': np.logspace(-4, 1.2, nPoints),\
                           'sgd_param_range': np.logspace(-1.0, -0.0, nPoints)}
