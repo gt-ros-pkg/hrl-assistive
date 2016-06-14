ab1ac5e4db897f959e60f2acb9ca7881193d48b8
pidipidi <deric.park@gatech.edu>
2016-06-14 18:37:52 -0400

    modified:   classifiers/run_classifier_aws.py

--- hrl_anomaly_detection/src/hrl_anomaly_detection/classifiers/run_classifier_aws.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/classifiers/run_classifier_aws.py
@@ -469,7 +469,7 @@ if __name__ == '__main__':
                           }
         elif opt.method == 'osvm':
             parameters = {'method': ['osvm'], 'svm_type': [2], 'kernel_type': [2], \
-                          'osvm_nu': np.logspace(-5.5,-4.5,10),
+                          'osvm_nu': np.logspace(-8.0,-5.0,10),
                           }
             
         ## if opt.dim == 4:
