c484e02f57f938a2ee640f0aba8f63b7015deacc
pidipidi <deric.park@gatech.edu>
2016-06-27 16:23:23 -0400

    modified:   ../params.py

--- hrl_anomaly_detection/src/hrl_anomaly_detection/params.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/params.py
@@ -152,7 +152,7 @@ def getFeeding(task, data_renew, AE_renew, HMM_renew, rf_center,local_range, ae_
 
         nPoints        = 20 #'svm','hmmosvm'
         ROC_param_dict = {'methods': ['progress_time_cluster', 'fixed', 'svm', 'hmmosvm'],\
-                          'update_list': ['progress_time_cluster', 'svm', 'fixed'],\
+                          'update_list': ['progress_time_cluster', 'svm', 'fixed', 'hmmosvm'],\
                           'nPoints': nPoints,\
                           'progress_param_range': -np.logspace(0., 1.5, nPoints),\
                           'svm_param_range': np.logspace(-1.0, 0.25, nPoints),\
