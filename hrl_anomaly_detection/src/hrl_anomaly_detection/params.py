ab1caf6269adb174b25668d80e84a6c7bc189f46
pidipidi <deric.park@gatech.edu>
2016-06-27 23:54:08 -0400

    modified:   ../params.py

--- hrl_anomaly_detection/src/hrl_anomaly_detection/params.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/params.py
@@ -586,7 +586,7 @@ def getPushingToolCase(task, data_renew, AE_renew, HMM_renew, rf_center,local_ra
         ROC_param_dict = {'methods': [ 'fixed', 'progress_time_cluster', 'svm', 'hmmosvm' ],\
                           'update_list': ['progress_time_cluster'],\
                           'nPoints': nPoints,\
-                          'progress_param_range':np.linspace(0.2, -7., nPoints), \
+                          'progress_param_range':np.linspace(0.2, -9., nPoints), \
                           'svm_param_range': np.logspace(-2, 0.5, nPoints),\
                           'fixed_param_range': np.linspace(0.5, -3.0, nPoints),\
                           'hmmosvm_param_range': np.logspace(-4.0, 1.0, nPoints),\
