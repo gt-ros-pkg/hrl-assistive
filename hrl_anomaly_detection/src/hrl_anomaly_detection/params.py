1cf25d414cc37236872428aa5b0dd31330c9c613
pidipidi <deric.park@gatech.edu>
2016-06-28 12:28:59 -0400

    modified:   ../params.py

--- hrl_anomaly_detection/src/hrl_anomaly_detection/params.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/params.py
@@ -433,12 +433,12 @@ def getPushingMicroBlack(task, data_renew, AE_renew, HMM_renew, rf_center,local_
                           'update_list': ['svm'],\
                           'nPoints': nPoints,\
                           'progress_param_range':np.linspace(0.0, -8., nPoints), \
-                          'svm_param_range': np.logspace(-1.5, -0.5, nPoints),\
                           'fixed_param_range': np.linspace(1.0, -3.0, nPoints),\
+                          'svm_param_range': np.logspace(-2, 0.2, nPoints)
                           'cssvm_param_range': np.logspace(-4.0, 2.0, nPoints),
                           'hmmosvm_param_range': np.logspace(-4.0, 1.0, nPoints),\
                           'osvm_param_range': np.linspace(0.1, 2.0, nPoints),\
-                          'svm_param_range': np.logspace(-4, 1.2, nPoints)}        
+                          }        
         
     elif dim == 4:
         handFeatures = ['unimodal_ftForce',\
