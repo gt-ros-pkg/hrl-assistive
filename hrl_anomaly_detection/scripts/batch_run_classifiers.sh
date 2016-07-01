903f21735cd75e8b452633b90f98b878b1014021
pidipidi <deric.park@gatech.edu>
2016-07-01 16:23:27 -0400

    new file:   batch_run_classifiers.sh

---
+++ hrl_anomaly_detection/scripts/batch_run_classifiers.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#kill -9 `ps aux | grep gazebo | awk '{print $2}'`
+#sleep 1
+
+#SAVE_PATH=N1=/home/$USER/hrl_file_server/dpark_data/
+
+#if [ "$1" -eq "renew" ] ; then
+    
+#fi
+
+for ((I=2;I<=5;I++)); do
+    echo ${I}
+    #python ../src/hrl_anomaly_detection/rss2016test.py --task $1 --dim $I --ea --hr --np;
+
+    if [ $I -eq 2 ]; then
+        METHOD=('svm' 'hmmosvm');
+    elif [ $I -eq 3 ]; then        
+        METHOD=('svm' 'hmmosvm' );
+    elif [ $I -eq 4 ]; then        
+        METHOD=('svm' 'hmmosvm' 'hmmsvm_diag' 'hmmsvm_dL' 'hmmsvm_LSLS' );
+    elif [ $I -eq 5 ]; then        
+        METHOD=('svm' 'hmmosvm' );
+    fi
+
+    for method in "${METHOD[@]}"
+    do
+        python ../src/hrl_anomaly_detection/classifiers/run_classifier_aws.py --task $1 --dim $I --save --method ${method} \;
+    done
+
+done
