100a26eadb63897629373628567be3cbf4cb1f30
pidipidi <deric.park@gatech.edu>
2016-07-02 00:59:11 -0400

    modified:   batch_run_classifiers.sh

--- hrl_anomaly_detection/scripts/batch_run_classifiers.sh
+++ hrl_anomaly_detection/scripts/batch_run_classifiers.sh
@@ -26,7 +26,7 @@ for ((I=2;I<=5;I++)); do
         fi 
     fi
 
-    if [ $HMM_RENEW -eq 1]; then
+    if [ $HMM_RENEW -eq 1 ]; then
         python ../src/hrl_anomaly_detection/rss2016test.py --task $1 --dim $I --ea --hr --np;
     fi
 
