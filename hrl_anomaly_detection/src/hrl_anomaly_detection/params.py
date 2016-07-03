4263959affa1cc39c3a66b9c51691cb84bf02966
pidipidi <deric.park@gatech.edu>
2016-07-02 21:05:00 -0400

    modified:   ../src/hrl_anomaly_detection/params.py

--- hrl_anomaly_detection/src/hrl_anomaly_detection/params.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/params.py
@@ -680,7 +680,7 @@ def getPushingToolCase(task, data_renew, AE_renew, HMM_renew, rf_center,local_ra
                           'update_list': ['progress_state'  ],\
                           'nPoints': nPoints,\
                           'progress_param_range':np.linspace(-0.5, -11., nPoints), \
-                          'progress_state_param_range':np.logspace(-0.1, 3.3., nPoints)*-1.0, \
+                          'progress_state_param_range':np.logspace(-0.1, 3.3, nPoints)*-1.0, \
                           'svm_param_range': np.logspace(-2.5, 0.8, nPoints),\
                           'fixed_param_range': np.linspace(-1.0, 0.8, nPoints),\
                           'change_param_range': np.logspace(-0.2, 1.5, nPoints)*-1.0,\
