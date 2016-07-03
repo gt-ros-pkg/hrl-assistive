473ce47f404761a7e627ce15661dfe499e01ee75
pidipidi <deric.park@gatech.edu>
2016-07-03 16:56:14 -0400

    modified:   params.py

--- hrl_anomaly_detection/src/hrl_anomaly_detection/params.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/params.py
@@ -544,15 +544,15 @@ def getPushingMicroBlack(task, data_renew, AE_renew, HMM_renew, rf_center,local_
         
         nPoints        = 20  # 'progress_time_cluster',,'fixed' , 'svm' , 
         ROC_param_dict = {'methods': [ 'fixed', 'progress_time_cluster', 'svm','hmmosvm' ],\
-                          'update_list': [ 'svm', 'progress_time_cluster', 'fixed' ],\
+                          'update_list': [ 'svm', 'fixed' ],\
                           'nPoints': nPoints,\
                           'progress_param_range':np.linspace(-0.8, -5.6, nPoints), \
-                          'svm_param_range': np.logspace(-0.7, -0.5, nPoints),\
-                          'fixed_param_range': np.linspace(0.3, -1.5, nPoints),\
+                          'fixed_param_range': np.linspace(0.2, -1., nPoints),\
+                          'svm_param_range': np.logspace(-2.5, 0.2, nPoints),\
                           'cssvm_param_range': np.logspace(-4.0, 2.0, nPoints),
                           'hmmosvm_param_range': np.logspace(-4.0, 1.0, nPoints),\
                           'osvm_param_range': np.linspace(0.1, 2.0, nPoints),\
-                          'svm_param_range': np.logspace(-4, 1.2, nPoints)}        
+                          }        
     elif dim == 2:
         handFeatures = ['unimodal_ftForce',\
                         'unimodal_audioWristRMS'] #'unimodal_audioPower', ,
@@ -563,7 +563,7 @@ def getPushingMicroBlack(task, data_renew, AE_renew, HMM_renew, rf_center,local_
 
         nPoints        = 20  # 'progress_time_cluster',,'fixed' , 'svm' , 
         ROC_param_dict = {'methods': [ 'fixed', 'progress_time_cluster', 'svm','hmmosvm' ],\
-                          'update_list': [ 'svm', 'progress_time_cluster', 'fixed' ],\
+                          'update_list': [ ],\
                           'nPoints': nPoints,\
                           'progress_param_range':np.logspace(0.0, 1.5, nPoints)*-1.0, \
                           'fixed_param_range': np.linspace(-1.73, -1.81, nPoints),\
