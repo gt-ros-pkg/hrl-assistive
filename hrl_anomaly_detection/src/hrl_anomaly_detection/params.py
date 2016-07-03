4d92c099fbee538946c0177d3c424cbf4cd35c3e
pidipidi <deric.park@gatech.edu>
2016-07-03 11:42:47 -0400

    modified:   params.py

--- hrl_anomaly_detection/src/hrl_anomaly_detection/params.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/params.py
@@ -28,8 +28,8 @@ def getScooping(task, data_renew, AE_renew, HMM_renew, rf_center,local_range, pr
                           }
         
         nPoints        = 20  # 'progress_time_cluster',,'fixed' , 'svm' , , 'hmmosvm', 'hmmsvm_diag', 'osvm', 'hmmsvm_dL', 'hmmsvm_LSLS' 
-        ROC_param_dict = {'methods': [ 'change', 'fixed', 'progress_time_cluster', 'svm', 'progress_state', 'bpsvm', 'osvm'],\
-                          'update_list': [],\
+        ROC_param_dict = {'methods': [ 'progress_time_cluster', 'svm', 'fixed', 'change', 'hmmsvm_dL', 'hmmosvm', 'hmmsvm_diag', 'hmmsvm_LSLS', 'progress_state' ],\
+                          'update_list': [ 'change', 'hmmsvm_dL', 'hmmosvm', 'hmmsvm_diag', 'hmmsvm_LSLS', 'svm' ],\
                           'nPoints': nPoints,\
                           'progress_param_range':np.linspace(0.0, -7., nPoints), \
                           'progress_state_param_range':np.logspace(0, 2., nPoints)*-1+2.0, \
@@ -58,7 +58,7 @@ def getScooping(task, data_renew, AE_renew, HMM_renew, rf_center,local_range, pr
 
         nPoints        = 20  # 'progress_time_cluster',,'fixed' , 'svm' , 
         ROC_param_dict = {'methods': [ 'fixed', 'progress_time_cluster', 'svm', 'hmmosvm'],\
-                          'update_list': ['hmmosvm'],\
+                          'update_list': ['hmmosvm', 'svm'],\
                           'nPoints': nPoints,\
                           'progress_param_range':np.linspace(-0.8, -5., nPoints), \
                           'svm_param_range': np.logspace(-2.5, 0, nPoints),\
@@ -79,7 +79,7 @@ def getScooping(task, data_renew, AE_renew, HMM_renew, rf_center,local_range, pr
 
         nPoints        = 20  # 'progress_time_cluster',,'fixed' , 'svm' , 
         ROC_param_dict = {'methods': [ 'fixed', 'progress_time_cluster', 'svm', 'hmmosvm'],\
-                          'update_list': ['hmmosvm'],\
+                          'update_list': ['hmmosvm', 'svm'],\
                           'nPoints': nPoints,\
                           'progress_param_range':np.linspace(-0.8, -8., nPoints), \
                           'svm_param_range': np.logspace(-1.5, 1, nPoints),\
