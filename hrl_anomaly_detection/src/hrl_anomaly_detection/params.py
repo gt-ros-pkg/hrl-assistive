b9be6f326a0ee17fd2dbe785ec20629a900a2168
pidipidi <deric.park@gatech.edu>
2016-07-03 16:50:39 -0400

    modified:   params.py

--- hrl_anomaly_detection/src/hrl_anomaly_detection/params.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/params.py
@@ -480,8 +480,8 @@ def getPushingMicroBlack(task, data_renew, AE_renew, HMM_renew, rf_center,local_
                           'update_list': [ 'svm', 'fixed'],\
                           'nPoints': nPoints,\
                           'progress_param_range':np.logspace(-0.2, 1.2, nPoints)*-1.0, \
-                          'fixed_param_range': np.linspace(0.3, -1.3, nPoints),\
-                          'svm_param_range': np.logspace(-2.5, 0., nPoints),\
+                          'fixed_param_range': np.linspace(0.8, -1.0, nPoints),\
+                          'svm_param_range': np.logspace(-2.5, -0.4, nPoints),\
                           'cssvm_param_range': np.logspace(-4.0, 2.0, nPoints),
                           'hmmosvm_param_range': np.logspace(-6.0, 0.6, nPoints),\
                           'osvm_param_range': np.linspace(0.1, 2.0, nPoints),\
@@ -563,11 +563,11 @@ def getPushingMicroBlack(task, data_renew, AE_renew, HMM_renew, rf_center,local_
 
         nPoints        = 20  # 'progress_time_cluster',,'fixed' , 'svm' , 
         ROC_param_dict = {'methods': [ 'fixed', 'progress_time_cluster', 'svm','hmmosvm' ],\
-                          'update_list': [ 'svm', 'fixed' ],\
+                          'update_list': [ 'svm', 'progress_time_cluster', 'fixed' ],\
                           'nPoints': nPoints,\
                           'progress_param_range':np.logspace(0.0, 1.5, nPoints)*-1.0, \
-                          'fixed_param_range': np.linspace(-1.7, -1.79, nPoints),\
-                          'svm_param_range': np.logspace(-1.5, 0.2, nPoints),\
+                          'fixed_param_range': np.linspace(-1.73, -1.81, nPoints),\
+                          'svm_param_range': np.logspace(-1.5, 0.5, nPoints),\
                           'cssvm_param_range': np.logspace(-4.0, 2.0, nPoints),\
                           'hmmosvm_param_range': np.logspace(-2.7, 1.2, nPoints),\
                           'osvm_param_range': np.linspace(0.1, 2.0, nPoints)
