144ff30aa46ff7dbec501a5aad565ea1f754e117
pidipidi <deric.park@gatech.edu>
2016-07-12 03:18:12 -0400

    modified:   rss2016test.py

--- hrl_anomaly_detection/src/hrl_anomaly_detection/rss2016test.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/rss2016test.py
@@ -2490,7 +2490,7 @@ if __name__ == '__main__':
         Change into different sampling frequency
         '''
         param_dict['ROC']['methods'] = ['svm', 'hmmsvm_LSLS', 'hmmsvm_dL']
-        param_dict['ROC']['update_list'] = ['hmmsvm_dL']
+        param_dict['ROC']['update_list'] = []
         if opt.bNoUpdate: param_dict['ROC']['update_list'] = []
         param_dict['HMM']['renew'] = False
         param_dict['SVM']['renew'] = False
@@ -2507,9 +2507,15 @@ if __name__ == '__main__':
               str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
 
             if sampleSize > 200:
-                param_dict['ROC']['hmmsvm_dL_param_range'] *= 15.0
-                param_dict['ROC']['hmmsvm_LSLS_param_range'] *= 15.0
-                param_dict['ROC']['svm_param_range'] *= 15.0
+                ## param_dict['ROC']['update_list'] = ['svm', 'hmmsvm_LSLS', 'hmmsvm_dL']
+                if opt.task == "pushing_microblack":
+                    param_dict['ROC']['hmmsvm_dL_param_range'] *= 1.0
+                    param_dict['ROC']['hmmsvm_LSLS_param_range'] *= 10.0
+                    param_dict['ROC']['svm_param_range'] *= 5.0
+                if opt.task == "pushing_microwhite":
+                    param_dict['ROC']['hmmsvm_dL_param_range'] *= 15.0
+                    param_dict['ROC']['hmmsvm_LSLS_param_range'] *= 15.0
+                    param_dict['ROC']['svm_param_range'] *= 10.0
 
             evaluation_freq(subjects, opt.task, raw_data_path, save_data_path, param_dict, \
                             refSampleSize,\
