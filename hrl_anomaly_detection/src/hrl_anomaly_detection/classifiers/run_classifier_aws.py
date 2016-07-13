85add247947197726c2c76ad4b0c7e63f6d7091a
pidipidi <deric.park@gatech.edu>
2016-07-13 18:23:58 -0400

    modified:   run_classifier_aws.py 	modified:   ../params.py

--- hrl_anomaly_detection/src/hrl_anomaly_detection/classifiers/run_classifier_aws.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/classifiers/run_classifier_aws.py
@@ -477,8 +477,8 @@ if __name__ == '__main__':
         elif opt.method == 'hmmsvm_no_dL':
             parameters = {'method': ['hmmsvm_no_dL'], 'svm_type': [0], 'kernel_type': [2], \
                           'hmmsvm_no_dL_cost': np.linspace(1.5,6.0,5),\
-                          'hmmsvm_no_dL_gamma': np.logspace(-3,-1,3), \
-                          'hmmsvm_no_dL_w_negative': np.linspace(1.5,3.5,5)
+                          'hmmsvm_no_dL_gamma': np.logspace(-2.5,0,5), \
+                          'hmmsvm_no_dL_w_negative': np.linspace(1.5,5.0,5)
                           }
         elif opt.method == 'bpsvm':
             parameters = {'method': ['bpsvm'], 'svm_type': [0], 'kernel_type': [2], \
--- hrl_anomaly_detection/src/hrl_anomaly_detection/params.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/params.py
@@ -155,16 +155,16 @@ def getFeeding(task, data_renew, AE_renew, HMM_renew, rf_center,local_range, ae_
 
         nPoints        = 20 #, 'osvm' 'bpsvm', 'progress_state' 'progress_time_cluster', 'svm','fixed', 
         ROC_param_dict = {'methods': ['fixed', 'progress_time_cluster', 'hmmosvm', 'svm', 'change', 'hmmsvm_diag', 'hmmsvm_dL', 'hmmsvm_LSLS', 'bpsvm', 'hmmsvm_no_dL'],\
-                          'update_list': [ 'hmmsvm_no_dL'],\
+                          'update_list': [ 'hmmosvm', 'hmmsvm_LSLS'],\
                           'nPoints': nPoints,\
                           'progress_param_range': -np.logspace(0., 1.2, nPoints),\
                           'progress_state_param_range':np.linspace(-1, -40., nPoints), \
                           'svm_param_range': np.logspace(1.0, -2.2, nPoints),\
                           'hmmsvm_diag_param_range': np.logspace(-4, 1.2, nPoints),\
                           'hmmsvm_dL_param_range': np.logspace(-4, 1.2, nPoints),\
-                          'hmmsvm_LSLS_param_range': np.logspace(-4, 1.2, nPoints),\
+                          'hmmsvm_LSLS_param_range': np.logspace(-3, 1.2, nPoints),\
                           'hmmsvm_no_dL_param_range': np.logspace(-3, 1.2, nPoints),\
-                          'hmmosvm_param_range': np.linspace(0,10.0,nPoints),\
+                          'hmmosvm_param_range': np.linspace(0,0.3,nPoints),\
                           'change_param_range': np.logspace(0.2, 1.4, nPoints)*-1.0,\
                           'osvm_param_range': np.logspace(-5., 0.0, nPoints),\
                           'bpsvm_param_range': np.logspace(-2.2, 0.5, nPoints),\
