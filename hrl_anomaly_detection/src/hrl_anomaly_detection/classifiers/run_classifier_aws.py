2dfe625d5d6689694f846e79467215767b8afa58
pidipidi <deric.park@gatech.edu>
2016-07-12 23:26:48 -0400

    modified:   classifiers/run_classifier_aws.py

--- hrl_anomaly_detection/src/hrl_anomaly_detection/classifiers/run_classifier_aws.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/classifiers/run_classifier_aws.py
@@ -633,7 +633,7 @@ if __name__ == '__main__':
             parameters = {'method': ['hmmsvm_dL'], 'svm_type': [0], 'kernel_type': [2], \
                           'hmmsvm_dL_cost': np.linspace(5,15.0,5),\
                           'hmmsvm_dL_gamma': np.linspace(0.01,2.0,5), \
-                          'hmmsvm_dL_w_negative': np.linspace(0.2,1.5,5)
+                          'hmmsvm_dL_w_negative': np.linspace(0.8,2.0,5)
                           }
         elif opt.method == 'hmmsvm_LSLS':
             parameters = {'method': ['hmmsvm_LSLS'], 'svm_type': [0], 'kernel_type': [2], \
