dc8a3380f7e0eb0faffa66b51ac8dc529b3ec9d3
pidipidi <deric.park@gatech.edu>
2016-07-15 21:52:28 -0400

    modified:   rss2016test.py

--- hrl_anomaly_detection/src/hrl_anomaly_detection/rss2016test.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/rss2016test.py
@@ -1710,7 +1710,7 @@ def find_ROC_param_range(method, task_name, processed_data_path, param_dict, deb
                                                                           ROC_dict, AE_dict, \
                                                                           SVM_dict, HMM_dict, \
                                                                           startIdx=startIdx, nState=nState,\
-                                                                          modeling_pkl_prefix=modeling_pkl_prefix\) \
+                                                                          modeling_pkl_prefix=modeling_pkl_prefix) \
                                                                           for idx in xrange(nFiles) \
                                                                           )
 
