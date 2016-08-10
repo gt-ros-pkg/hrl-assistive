87e67e7a5fc411484afe862bc769887d69a2a5a1
pidipidi <deric.park@gatech.edu>
2016-08-10 18:26:39 -0400

    Added hmm param save option for online HMM

--- hrl_anomaly_detection/src/hrl_anomaly_detection/ICRA2017_params.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/ICRA2017_params.py
@@ -158,7 +158,8 @@ def getFeeding(task, data_renew, AE_renew, HMM_renew, rf_center='kinEEPos',local
         ## HMM_param_dict = {'renew': HMM_renew, 'nState': 25, 'cov': 2.0, 'scale': 4.66,\
         ##                   'add_logp_d': True}
         ## HMM_param_dict = {'renew': HMM_renew, 'nState': 25, 'cov': 3.5, 'scale': 4.111}
-        SVM_param_dict = {'renew': False, 'w_negative': 1.05, 'gamma': 3.61, 'cost': 0.1,\
+        ## SVM_param_dict = {'renew': False, 'w_negative': 1.05, 'gamma': 3.61, 'cost': 0.1,\
+        SVM_param_dict = {'renew': False, 'w_negative': 2.0, 'gamma': 5.0, 'cost': 0.1,\
                           'hmmosvm_nu': 0.000316,\
                           'osvm_nu': 0.000359,\
                           'hmmsvm_diag_w_negative': 0.2, 'hmmsvm_diag_cost': 15.0, \
--- hrl_anomaly_detection/src/hrl_anomaly_detection/hmm/learning_hmm.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/hmm/learning_hmm.py
@@ -71,13 +71,19 @@ class learning_hmm(learning_base):
         self.F = ghmm.Float()  
 
 
-    def set_hmm_object(self, A, B, pi):
+    def set_hmm_object(self, A, B, pi, out_a_num=None, vec_num=None, mat_num=None, u_denom=None):
 
         self.ml = ghmm.HMMFromMatrices(self.F, ghmm.MultivariateGaussianDistribution(self.F), \
                                        A, B, pi)
         self.A = A
         self.B = B
         self.pi = pi
+
+        try:
+            self.ml.setBaumWelchParams(out_a_num, vec_num, mat_num, u_denom)
+        except:
+            print "Install new ghmm!!"
+            
         return self.ml
 
 
@@ -107,6 +113,14 @@ class learning_hmm(learning_base):
             self.pi = param_dict['pi']                       
             self.ml = ghmm.HMMFromMatrices(self.F, ghmm.MultivariateGaussianDistribution(self.F), \
                                            self.A, self.B, self.pi)
+
+            out_a_num = param_dict.get('out_a_num', None)
+            vec_num   = param_dict.get('vec_num', None)
+            mat_num   = param_dict.get('mat_num', None)
+            u_denom   = param_dict.get('u_denom', None)
+            if out_a_num is not None:
+                self.ml.setBaumWelchParams(out_a_num, vec_num, mat_num, u_denom)
+                                           
             return True
         else:
            
@@ -172,148 +186,43 @@ class learning_hmm(learning_base):
             param_dict['B'] = self.B
             param_dict['pi'] = self.pi
 
+            try:
+                [out_a_num, vec_num, mat_num, u_denom] = self.ml.getBaumWelchParams()            
+                param_dict['out_a_num'] = out_a_num
+                param_dict['vec_num']   = vec_num
+                param_dict['mat_num']   = mat_num
+                param_dict['u_denom']   = u_denom
+            except:
+                print "Install new ghmm!!"
+
             if ml_pkl is not None: ut.save_pickle(param_dict, ml_pkl)
             return ret
 
-    def partial_fit(self, xData, nTrain, scale, weight=8.0):
+
+    def partial_fit(self, xData, learningRate=0.2):
+        ''' Online update of HMM using online Baum-Welch algorithm
         '''
-        data: dimension x sample x length
+        
+        X = [np.array(data) for data in xData]
+
+        # print 'Creating Training Data'            
+        X_train = util.convert_sequence(X) # Training input
+        X_train = X_train.tolist()
+
+        if self.verbose: print 'Run Baum Welch method with (samples, length)', np.shape(X_train)
+
+        for i in xrange(len(X)):            
+            final_seq = ghmm.SequenceSet(self.F, X_train[i:i+1])
+            ret = self.ml.baumWelch(final_seq, nrSteps=1, learningRate=learningRate)
+            print 'Baum Welch return:', ret
+            if np.isnan(ret): return 'Failure'
+        return ret
+
+        
+    def predict(self, X):
         '''
-        A  = copy.copy(self.A)
-        B  = copy.copy(self.B)
-        pi = copy.copy(self.pi)
-
-        new_B = copy.copy(self.B)
-
-        t_features = []
-        mus        = []
-        covs       = []
-        for i in xrange(self.nState):
-            t_features.append( B[i][0] + [ float(i) / float(self.nState)*scale/2.0 ])
-            mus.append( B[i][0] )
-            covs.append( B[i][1] )
-        t_features = np.array(t_features)
-        mus        = np.array(mus)
-        covs       = np.array(covs)
-
-        # update b ------------------------------------------------------------
-        # mu
-        x_l = [[] for i in xrange(self.nState)]
-        X   = np.swapaxes(xData, 0, 1) # sample x dim x length
-        seq_len = len(X[0][0])
-        for i in xrange(len(X)):
-            sample = np.swapaxes(X[i], 0, 1) # length x dim
-
-            idx_l = []
-            for j in xrange(len(sample)):
-                feature = np.array( sample[j].tolist() + [float(j)/float(len(sample))*scale/2.0 ] )
-
-                min_dist = 10000
-                min_idx  = 0
-                for idx, t_feature in enumerate(t_features):
-                    dist = np.linalg.norm(t_feature-feature)
-                    if dist < min_dist:
-                        min_dist = dist
-                        min_idx  = idx
-
-                x_l[min_idx].append(feature[:-1].tolist())
-
-        for i in xrange(len(mus)):
-            if len(x_l[i]) > 0:
-                avg_x = np.mean(x_l[i], axis=0)
-                new_B[i][0] = list( ( float(nTrain-1)*mus[i] + avg_x*weight ) / float(nTrain+(weight-1) ) ) # specialized for single input
-
-
-        # Normalize the state prior and transition values.
-        A_sum = np.sum(A, axis=1)
-        for i in xrange(self.nState):
-            A[i,:] /= A_sum[i]
-        pi /= np.sum(pi)
-
-        # Daehyung: What is the shape and type of input data?
-        xData = [np.array(data) for data in xData]
-        X_ptrain = util.convert_sequence(xData) # Training input
-        X_ptrain = np.squeeze(X_ptrain)
-
-        final_ts_obj = ghmm.EmissionSequence(self.F, X_ptrain.tolist())        
-        (alpha, scale) = self.ml.forward(final_ts_obj)
-        beta = self.ml.backward(final_ts_obj, scale)
-
-        ## print np.shape(alpha), np.shape(beta), type(alpha), type(beta)
-
-        est_A = np.zeros((self.nState, self.nState))
-        new_A = np.zeros((self.nState, self.nState))
-        for i in xrange(self.nState):
-            for j in xrange(self.nState):
-
-                temp1 = 0.0
-                temp2 = 0.0
-                for t in xrange(seq_len):
-                    p = multivariate_normal.pdf( X[0][:,t], mean=mus[j], \
-                                                 cov=np.reshape(covs[j], \
-                                                                (self.nEmissionDim, self.nEmissionDim)))
-                    temp1 += alpha[t-1][i] * A[i,j] * p * beta[t][j]
-                    temp2 += alpha[t-1][i] * beta[t][j]
-
-                if temp1 == 0.0 or temp2 == 0.0: est_A[i,j] = 0
-                else: est_A[i,j] = temp1/temp2
-                    
-                new_A[i,j] = (float(nTrain-len(xData))*A[i,j] + est_A[i,j]*weight) / float(nTrain + (weight-1.0) )
-
-        # Normalize the state prior and transition values.
-        A_sum = np.sum(new_A, axis=1)
-        for i in xrange(self.nState):
-            new_A[i,:] /= A_sum[i]
-        pi /= np.sum(pi)
-            
-        self.set_hmm_object(new_A, new_B, pi)
-        return new_A, new_B, pi
-        
-
-    ## def predict(self, X):
-    ##     '''
-    ##     ???????????????????
-    ##     HMM is just a generative model. What will be prediction result?
-    ##     Which code is using this fuction?
-    ##     '''
-    ##     X = np.squeeze(X)
-    ##     X_test = X.tolist()
-
-    ##     mu_l = np.zeros(self.nEmissionDim)
-    ##     cov_l = np.zeros(self.nEmissionDim**2)
-
-    ##     if self.verbose: print self.F
-    ##     final_ts_obj = ghmm.EmissionSequence(self.F, X_test) # is it neccessary?
-
-    ##     try:
-    ##         # alpha: X_test length y # latent States at the moment t when state i is ended
-    ##         # test_profile_length x number_of_hidden_state
-    ##         (alpha, scale) = self.ml.forward(final_ts_obj)
-    ##         alpha = np.array(alpha)
-    ##     except:
-    ##         if self.verbose: print "No alpha is available !!"
-            
-    ##     f = lambda x: round(x, 12)
-    ##     for i in range(len(alpha)):
-    ##         alpha[i] = map(f, alpha[i])
-    ##     alpha[-1] = map(f, alpha[-1])
-        
-    ##     n = len(X_test)
-    ##     pred_numerator = 0.0
-
-    ##     for j in xrange(self.nState): # N+1
-    ##         total = np.sum(self.A[:,j]*alpha[n/self.nEmissionDim-1,:]) #* scaling_factor
-    ##         [mus, covars] = self.B[j]
-
-    ##         ## print mu1, mu2, cov11, cov12, cov21, cov22, total
-    ##         pred_numerator += total
-
-    ##         for i in xrange(mu_l.size):
-    ##             mu_l[i] += mus[i]*total
-    ##         for i in xrange(cov_l.size):
-    ##             cov_l[i] += covars[i] * (total**2)
-
-    ##     return mu_l, cov_l
+        '''
+        return
 
 
     def loglikelihood(self, X, bPosterior=False):
