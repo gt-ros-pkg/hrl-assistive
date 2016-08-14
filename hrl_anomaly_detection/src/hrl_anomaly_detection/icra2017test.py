a4038adaa4a756a3fd32f676e075368281ad6925
pidipidi <deric.park@gatech.edu>
2016-08-14 15:48:33 -0400

    param test

--- hrl_anomaly_detection/src/hrl_anomaly_detection/icra2017test.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/icra2017test.py
@@ -1094,7 +1094,7 @@ if __name__ == '__main__':
         ## subjects        = ['linda', 'jina', 'sai']        
         ## subjects        = ['ari', 'zack', 'hkim', 'park', 'jina', 'sai', 'linda']        
         param_dict['ROC']['methods'] = ['progress']
-        param_dict['ROC']['methods'] = ['kmean']
+        ## param_dict['ROC']['methods'] = ['kmean']
         param_dict['ROC']['nPoints'] = 16
 
         ## param_dict['HMM'] = {'renew': opt.bHMMRenew, 'nState': 25, 'cov': 2.0, 'scale': 13.0,\
