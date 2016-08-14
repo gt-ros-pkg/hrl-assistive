ee0c575051027c5aff4a9a137f979371968dcc52
pidipidi <deric.park@gatech.edu>
2016-08-14 12:08:56 -0400

    param test

--- hrl_anomaly_detection/src/hrl_anomaly_detection/params.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/params.py
@@ -359,7 +359,7 @@ def getPushingMicroWhite(task, data_renew, AE_renew, HMM_renew, rf_center,local_
                           'progress_state_param_range':np.logspace(-0.6, 3.5, nPoints)*-1.0+0.4, \
                           'fixed_param_range': np.linspace(-1.495, 0.171, nPoints),\
                           'change_param_range': np.logspace(0.2, 1.5, nPoints)*-1.0,\
-                          'osvm_param_range': np.logspace(-6, 0.0, nPoints),\                          
+                          'osvm_param_range': np.logspace(-6, 0.0, nPoints),\
                           'progress_diag_param_range':np.logspace(0, 1.6, nPoints)*-1.0, \
                           'svm_param_range': np.logspace(-1.236, 0.7, nPoints),\
                           'bpsvm_param_range': np.logspace(-3., 0.4, nPoints),\
