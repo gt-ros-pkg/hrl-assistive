771a864948a688abfd9f318c21a803ce1e35f7a2
pidipidi <deric.park@gatech.edu>
2016-08-14 12:06:12 -0400

    param test

--- hrl_anomaly_detection/scripts/batch_eval.sh
+++ hrl_anomaly_detection/scripts/batch_eval.sh
@@ -8,6 +8,6 @@ for ((I=2;I<=5;I++)); do
         fi 
     fi
 
-    python ../src/hrl_anomaly_detection/rss2016test.py --task $1 --dim $I --ea --np --hr \;
+    python ../src/hrl_anomaly_detection/rss2016test.py --task $1 --dim $I --ea --np --hr --dr \;
 
 done
