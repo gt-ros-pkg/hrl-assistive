20d049c3cbb640bdf69771d74dc473347f52dc0a
pidipidi <deric.park@gatech.edu>
2016-08-18 02:53:55 -0400

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/icra2017test.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/icra2017test.py
@@ -845,9 +845,9 @@ def run_online_classifier(idx, processed_data_path, task_name, nPtrainData,\
             ##     ret = ml.partial_fit( normalTrainData[:,(i-1)*nTrainOffset+j:(i-1)*nTrainOffset+j+1], learningRate=alpha,\
             ##                           nrSteps=3) #100(br) 10(c12) 5(c8)
 
-            alpha = np.exp(-4.0*float(i-1) )*0.2
+            alpha = np.exp(-4.0*float(i-1) )*0.1
             ret = ml.partial_fit( normalTrainData[:,(i-1)*nTrainOffset:i*nTrainOffset], learningRate=alpha,\
-                                  nrSteps=10)
+                                  nrSteps=5)
             if np.isnan(ret): sys.exit()
             # BAD: nrSteps=100
             # BAD: nrSteps=10
@@ -855,7 +855,7 @@ def run_online_classifier(idx, processed_data_path, task_name, nPtrainData,\
             # BAD: scale<=0.1
             # Good: progress update
             # step 10 4.0  0.2  c8 (5,2)
-            # step 10 0.1  0.1  c11 (5,2)
+            # step 5 4.0  0.1  c11 (5,2)
             # step 10 2.0, 0.05  c12 (5,2)
             # step 10 4.0  0.1  ep (5,2)
             # only hmm update br
