39dbd8b774dd5bb26c88d7b3a85d9a3d1ef55ebc
pidipidi <deric.park@gatech.edu>
2016-08-18 14:09:30 -0400

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/icra2017test.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/icra2017test.py
@@ -528,8 +528,8 @@ def evaluation_online(subject_names, task_name, raw_data_path, processed_data_pa
     method_list = ROC_dict['methods'] 
     nPoints     = ROC_dict['nPoints']
     nPtrainData = 30
-    nTrainOffset = 10
-    nTrainTimes  = 1
+    nTrainOffset = 1
+    nTrainTimes  = 5
     nNormalTrain = 30
 
     # leave-one-person-out
@@ -857,7 +857,7 @@ def run_online_classifier(idx, processed_data_path, task_name, nPtrainData,\
             ##     ret = ml.partial_fit( normalTrainData[:,(i-1)*nTrainOffset+j:(i-1)*nTrainOffset+j+1], learningRate=alpha,\
             ##                           nrSteps=3) #100(br) 10(c12) 5(c8)
 
-            alpha = np.exp(-4.0*float(i-1) )*0.05
+            alpha = np.exp(-1.0*float(i-1) )*0.05
             ret = ml.partial_fit( normalTrainData[:,(i-1)*nTrainOffset:i*nTrainOffset], learningRate=alpha,\
                                   nrSteps=10)
             if np.isnan(ret): sys.exit()
