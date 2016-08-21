32bed01951adf876db9d457b6e8eab834d55c590
pidipidi <deric.park@gatech.edu>
2016-08-21 18:09:54 -0400

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/hmm/run_hmm_cpu.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/hmm/run_hmm_cpu.py
@@ -121,7 +121,7 @@ def tune_hmm(parameters, cv_dict, param_dict, processed_data_path, verbose=False
             # Classifier train data
             #-----------------------------------------------------------------------------------------
             startIdx = 4
-            # Classifier training data
+            # Classifier training data (parallelization)
             ll_classifier_train_X, ll_classifier_train_Y, ll_classifier_train_idx =\
               hmm.getHMMinducedFeaturesFromRawFeatures(ml, normalTrainData, abnormalTrainData, \
                                                        startIdx, add_logp_d)
@@ -209,13 +209,37 @@ def tune_hmm(parameters, cv_dict, param_dict, processed_data_path, verbose=False
                 Y_test = ll_classifier_test_Y
             weights = ROC_dict[method+'_param_range']
 
+
+            if method == 'progress':
+                print "Classifier fitting", method
+                dtc = cf.classifier( method=method, nPosteriors=nState, nLength=nLength )
+                ret = dtc.fit(X_scaled, Y_train_org, idx_train_org, parallel=True)
+                cf_dict = {}
+                cf_dict['method']      = dtc.method
+                cf_dict['nPosteriors'] = dtc.nPosteriors
+                cf_dict['l_statePosterior'] = dtc.l_statePosterior
+                cf_dict['ths_mult']    = dtc.ths_mult
+                cf_dict['ll_mu']       = dtc.ll_mu
+                cf_dict['ll_std']      = dtc.ll_std
+                cf_dict['logp_offset'] = dtc.logp_offset
+            elif method == 'hmmgp':
+                print "Classifier fitting", method
+                dtc = cb.classifier( method=method, nPosteriors=nState, nLength=nLength )
+                ret = dtc.fit(X_scaled, Y_train_org, idx_train_org, parallel=True)
+                cf_dict = {}
+                cf_dict['method']      = dtc.method
+                cf_dict['nPosteriors'] = dtc.nPosteriors
+                cf_dict['ths_mult']    = dtc.ths_mult
+                dtc.save_model('./temp_hmmgp.pkl')
+            
+
             print "Start to run classifiers"
             r = Parallel(n_jobs=n_jobs, verbose=50)(delayed(run_classifiers)(iii, X_scaled, Y_train_org, \
                                                                              idx_train_org, \
                                                                              X_test, Y_test, \
                                                                              nEmissionDim, nLength, \
                                                                              SVM_dict, weight=weights[iii], \
-                                                                             method=method,\
+                                                                             method=method, cf_dict=cf_dict,\
                                                                              verbose=False)\
                                                                              for iii in xrange(len(weights)))
             idx_l, tp_ll, fn_ll, fp_ll, tn_ll = zip(*r)
@@ -282,22 +306,27 @@ def tune_hmm(parameters, cv_dict, param_dict, processed_data_path, verbose=False
 
 
 def run_classifiers(idx, X_scaled, Y_train_org, idx_train_org, X_test, Y_test, nEmissionDim, nLength, \
-                    SVM_dict, weight, method='svm',\
+                    SVM_dict, weight, method='svm', cf_dict=None,\
                     verbose=False):
 
     if verbose: print "Run a classifier"
     dtc = cb.classifier( method=method, nPosteriors=nEmissionDim, nLength=nLength )
     dtc.set_params( **SVM_dict )
+    if cf_dict is None:
+        ret = dtc.fit(X_scaled, Y_train_org, idx_train_org, parallel=False)        
+        if ret is False:
+            print "SVM fitting failure!!"
+            return idx, [np.nan], [np.nan], [np.nan], [np.nan]
+    else:
+        for k, v in cf_dict.iteritems():        
+            exec 'dtc.%s = v' % k        
+        if method == 'hmmgp':
+            dtc.load_model('./temp_hmmgp.pkl')
 
     if method.find('svm')>=0:
         dtc.set_params( class_weight=weight )
     else:
         dtc.set_params( ths_mult=weight )
-        
-    ret = dtc.fit(X_scaled, Y_train_org, idx_train_org, parallel=False)
-    if ret is False:
-        print "SVM fitting failure!!"
-        return idx, [np.nan], [np.nan], [np.nan], [np.nan]
 
     tp_l = []
     fn_l = []
@@ -320,278 +349,6 @@ def run_classifiers(idx, X_scaled, Y_train_org, idx_train_org, X_test, Y_test, n
             else: tn_l.append(1)
 
     return idx, tp_l, fn_l, fp_l, tn_l
-
-
-def tune_hmm_classifier(parameters, kFold_list, param_dict, verbose=True):
-
-    ## Parameters
-    # data
-    data_dict  = param_dict['data_param']
-    # AE
-    AE_dict     = param_dict['AE']
-    # HMM
-    HMM_dict = param_dict['HMM']
-    ## nState   = HMM_dict['nState']
-    ## cov      = HMM_dict['cov']
-    # SVM
-    SVM_dict = param_dict['SVM']
-    
-    #------------------------------------------
-
-    param_list = list(ParameterGrid(parameters))
-    startIdx   = 4
-    scores     = []
-    data       = {}
-
-    # get data
-    for idx, (normalTrainIdx, abnormalTrainIdx, normalTestIdx, abnormalTestIdx) \
-      in enumerate(kFold_list):
-        if AE_dict['switch']:
-            if verbose: print "Start "+str(idx)+"/"+str(len(kFold_list))+"th iteration"
-
-            AE_proc_data = os.path.join(processed_data_path, 'ae_processed_data_'+str(idx)+'.pkl')
-            d = ut.load_pickle(AE_proc_data)
-
-            if AE_dict['filter']:
-                # NOTE: pooling dimension should vary on each auto encoder.
-                # Filtering using variances
-                normalTrainData   = d['normTrainDataFiltered']
-                abnormalTrainData = d['abnormTrainDataFiltered']
-                normalTestData    = d['normTestDataFiltered']
-                abnormalTestData  = d['abnormTestDataFiltered']
-#                ## import data_viz as dv
-                ## dv.viz(normalTrainData)
-                ## continue                   
-            else:
-                normalTrainData   = d['normTrainData']
-                abnormalTrainData = d['abnormTrainData']
-                normalTestData    = d['normTestData']
-                abnormalTestData  = d['abnormTestData']
-
-        else:
-            # dim x sample x length
-            normalTrainData   = successData[:, normalTrainIdx, :] 
-            abnormalTrainData = failureData[:, abnormalTrainIdx, :] 
-            normalTestData    = successData[:, normalTestIdx, :] 
-            abnormalTestData  = failureData[:, abnormalTestIdx, :] 
-
-
-        if AE_dict['add_option'] is not None:
-            print "add feature!!"
-            newHandSuccTrData = handSuccTrData = d['handNormTrainData']
-            newHandFailTrData = handFailTrData = d['handAbnormTrainData']
-            handSuccTeData = d['handNormTestData']
-            handFailTeData = d['handAbnormTestData']
-
-            normalTrainData   = combineData( normalTrainData, newHandSuccTrData,\
-                                             AE_dict['add_option'], d['handFeatureNames'])
-            abnormalTrainData = combineData( abnormalTrainData, newHandFailTrData,\
-                                             AE_dict['add_option'], d['handFeatureNames'])
-            normalTestData   = combineData( normalTestData, handSuccTeData,\
-                                            AE_dict['add_option'], d['handFeatureNames'])
-            abnormalTestData  = combineData( abnormalTestData, handFailTeData,\
-                                             AE_dict['add_option'], d['handFeatureNames'])
-
-
-            ## pooling_param_dict  = {'dim': AE_dict['filterDim']} # only for AE
-            ## normalTrainData, abnormalTrainData,pooling_param_dict \
-            ##   = dm.errorPooling(d['normTrainData'], d['abnormTrainData'], pooling_param_dict)
-            ## normalTestData, abnormalTestData, _ \
-            ##   = dm.errorPooling(d['normTestData'], d['abnormTestData'], pooling_param_dict)
-
-            ## normalTrainData, pooling_param_dict = dm.variancePooling(normalTrainData, \
-            ##                                                          pooling_param_dict)
-            ## abnormalTrainData, _ = dm.variancePooling(abnormalTrainData, pooling_param_dict)
-            ## normalTestData, _    = dm.variancePooling(normalTestData, pooling_param_dict)
-            ## abnormalTestData, _  = dm.variancePooling(abnormalTestData, pooling_param_dict)
-
-        data[idx] = {'normalTrainData': normalTrainData, 'abnormalTrainData': abnormalTrainData, \
-                     'normalTestData': normalTestData, 'abnormalTestData': abnormalTestData }
-
-
-    # Training HMM, and getting classifier training and testing data
-    print "Start hmm - classifier"
-
-    idx_list = []
-    tp_list  = []
-    fp_list  = []
-    tn_list  = []
-    fn_list  = []
-    for param_idx, param in enumerate(param_list):
-        for idx in xrange(len(kFold_list)):
-            _, tp, fp, tn, fn = run_single_hmm_classifier(param_idx, data[idx], param, HMM_dict, SVM_dict, startIdx, n_jobs=-1)
-            idx_list.append(param_idx)
-            tp_list.append(tp)
-            fp_list.append(fp)
-            tn_list.append(tn)
-            fn_list.append(fn)
-            print "Finished ", param_idx*len(param_list)+idx, " / ", len(param_list)*len(kFold_list)
-    
-    ## r = Parallel(n_jobs=-1)(delayed(run_single_hmm_classifier)(param_idx, data[idx], param, HMM_dict, SVM_dict, startIdx, n_jobs=1) for idx in xrange(len(kFold_list)) for param_idx, param in enumerate(param_list) )
-    ## idx_list, tp_list, fp_list, tn_list, fn_list = zip(*r)
-
-    for i in xrange(len(param_list)):
-        tp_l = []
-        fn_l = []
-        for j, idx in enumerate(idx_list):
-            
-            if idx == -1:
-                print "Failed to fit HMM so ignore!!!"
-                break
-                
-            if i==idx:
-                tp_l += tp_list[j]
-                fn_l += fn_list[j]
-
-        if idx == -1:
-            score.append(-1.0*1e+10)
-        else:
-            tpr = float(np.sum(tp_l))/float(np.sum(tp_l)+np.sum(fn_l))*100.0
-            print "true positive rate : ", tpr
-            scores.append( tpr )        
-
-    for i, param in enumerate(param_list):
-        print("%0.3f for %r"
-              % (scores[i], param))
-
-
-
-
-def run_single_hmm_classifier(param_idx, data, param, HMM_dict, SVM_dict, startIdx, n_jobs=-1, verbose=True):
-
-    print "Start to run classifier with single data ", param_idx
-    normalTrainData   = data['normalTrainData'] * param['scale']
-    abnormalTrainData = data['abnormalTrainData'] * param['scale']
-    normalTestData    = data['normalTestData'] * param['scale']
-    abnormalTestData  = data['abnormalTestData'] * param['scale']
-
-    #
-    nEmissionDim = len(normalTrainData)
-    cov_mult     = [param['cov']]*(nEmissionDim**2)
-    nLength      = len(normalTrainData[0][0])
-
-    print "start fit hmm"
-    ml = hmm.learning_hmm( param['nState'], nEmissionDim )
-    ret = ml.fit( normalTrainData, cov_mult=cov_mult )
-    if ret == 'Failure' or np.isnan(ret):
-        print 'failure with ', param
-        return -1, [],[],[],[]
-        ## scores.append(-1.0 * 1e+10)
-        ## continue
-
-    #-----------------------------------------------------------------------------------------
-    # Classifier training data (dim x sample x length)
-    #-----------------------------------------------------------------------------------------
-    testDataX = np.vstack([ np.swapaxes(normalTrainData, 0, 1), \
-                            np.swapaxes(abnormalTrainData, 0, 1) ])
-    testDataX = np.swapaxes(testDataX, 0, 1)
-    testDataY = np.hstack([ -np.ones(len(normalTrainData[0])), \
-                            np.ones(len(abnormalTrainData[0])) ])
-
-    r = Parallel(n_jobs=n_jobs)(delayed(hmm.computeLikelihoods)(i, ml.A, ml.B, ml.pi, ml.F, \
-                                                            [ testDataX[j][i] for j in xrange(nEmissionDim) ], \
-                                                            ml.nEmissionDim, ml.nState,\
-                                                            startIdx=startIdx, \
-                                                            bPosterior=True)
-                                                            for i in xrange(len(testDataX[0])))
-    _, ll_classifier_train_idx, ll_logp, ll_post = zip(*r)
-
-    ll_classifier_train_X = []
-    ll_classifier_train_Y = []
-    for i in xrange(len(ll_logp)):
-        l_X = []
-        l_Y = []
-        for j in xrange(len(ll_logp[i])):        
-            l_X.append( [ll_logp[i][j]] + ll_post[i][j].tolist() )
-
-            if testDataY[i] > 0.0: l_Y.append(1)
-            else: l_Y.append(-1)
-
-        ll_classifier_train_X.append(l_X)
-        ll_classifier_train_Y.append(l_Y)
-
-    #-----------------------------------------------------------------------------------------
-    # Classifier test data (dim x sample x length)
-    #-----------------------------------------------------------------------------------------
-    testDataX = np.vstack([ np.swapaxes(normalTestData, 0, 1), \
-                            np.swapaxes(abnormalTestData, 0, 1) ])
-    testDataX = np.swapaxes(testDataX, 0, 1)
-    testDataY = np.hstack([ -np.ones(len(normalTestData[0])), \
-                            np.ones(len(abnormalTestData[0])) ])
-
-    r = Parallel(n_jobs=n_jobs)(delayed(hmm.computeLikelihoods)(i, ml.A, ml.B, ml.pi, ml.F, \
-                                                            [ testDataX[j][i] for j in xrange(nEmissionDim) ], \
-                                                            ml.nEmissionDim, ml.nState,\
-                                                            startIdx=startIdx, \
-                                                            bPosterior=True)
-                                                            for i in xrange(len(testDataX[0])))
-    _, ll_classifier_test_idx, ll_logp, ll_post = zip(*r)
-
-    # nSample x nLength
-    ll_classifier_test_X = []
-    ll_classifier_test_Y = []
-    for i in xrange(len(ll_logp)):
-        l_X = []
-        l_Y = []
-        for j in xrange(len(ll_logp[i])):        
-            l_X.append( [ll_logp[i][j]] + ll_post[i][j].tolist() )
-
-            if testDataY[i] > 0.0: l_Y.append(1)
-            else: l_Y.append(-1)
-
-        ll_classifier_test_X.append(l_X)
-        ll_classifier_test_Y.append(l_Y)
-
-    #-----------------------------------------------------------------------------------------
-    # 
-    #-----------------------------------------------------------------------------------------
-    # flatten the data
-    X_train = []
-    Y_train = []
-    idx_train = []
-    for i in xrange(len(ll_classifier_train_X)):
-        for j in xrange(len(ll_classifier_train_X[i])):
-            X_train.append(ll_classifier_train_X[i][j])
-            Y_train.append(ll_classifier_train_Y[i][j])
-            idx_train.append(ll_classifier_train_idx[i][j])
-
-
-    print "Start fit the classifier"
-    dtc = cb.classifier( method='svm', nPosteriors=ml.nState, nLength=nLength )        
-    dtc.set_params( **SVM_dict )
-    ret = dtc.fit(X_train, Y_train, idx_train)
-
-
-    tp_l = []
-    fp_l = []
-    tn_l = []
-    fn_l = []            
-
-    # We should maximize score,
-    #   if normal, error is close to 0
-    #   if abnormal, error is large
-    for i in xrange(len(ll_classifier_test_X)):
-        X     = ll_classifier_test_X[i]
-        try:
-            est_y = dtc.predict(X, y=ll_classifier_test_Y[i])
-        except:
-            continue
-
-        for j in xrange(len(est_y)):
-            if est_y[j] > 0.0:
-                break
-
-        if ll_classifier_test_Y[i][0] > 0.0:
-            if est_y[j] > 0.0: tp_l.append(1)
-            else: fn_l.append(1)
-        elif ll_classifier_test_Y[i][0] <= 0.0:
-            if est_y[j] > 0.0: fp_l.append(1)
-            else: tn_l.append(1)
-
-    print param_idx, param, len(tp_l), len(fn_l)
-    return param_idx, tp_l, fp_l, tn_l, fn_l
-
-
 
         
 
@@ -696,4 +453,3 @@ if __name__ == '__main__':
 
     tune_hmm(parameters, d, param_dict, save_data_path, verbose=True, n_jobs=opt.n_jobs, \
              bSave=opt.bSave, method=opt.method, max_check_fold=max_check_fold, no_cov=no_cov)
-    ## tune_hmm_classifier(parameters, kFold_list, param_dict, verbose=True)
