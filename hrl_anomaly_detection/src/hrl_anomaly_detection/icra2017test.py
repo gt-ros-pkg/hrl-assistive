3571d09afff5a5b9bc82945c60c890b34609dbcf
pidipidi <deric.park@gatech.edu>
2016-08-22 18:30:16 -0400

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/icra2017test.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/icra2017test.py
@@ -530,9 +530,9 @@ def evaluation_online(subject_names, task_name, raw_data_path, processed_data_pa
 
     # aws 7,4,  - 20, 2, 5, 30, 20
     # c11 8,8,  - 20, 2, 5, 30, 20 - good
-    # c11 7.5,7.5,  - 20, 2, 5, 30, 20 
+    # c11 6.6,6.6,  - 20, 2, 5, 30, 20 
 
-    #[9(9), , 9(4), ????]
+    #[9(9), , 7.5(7.5), ????]
     
     # leave-one-person-out
     kFold_list = []
@@ -1409,7 +1409,7 @@ if __name__ == '__main__':
         param_dict['ROC']['methods'] = ['hmmgp']
         param_dict['ROC']['nPoints'] = 16
 
-        param_dict['HMM'] = {'renew': opt.bHMMRenew, 'nState': 25, 'cov': 4.0, 'scale': 7.0,\
+        param_dict['HMM'] = {'renew': opt.bHMMRenew, 'nState': 25, 'cov': 6.0, 'scale': 6.0,\
                              'add_logp_d': False}
         ## param_dict['HMM'] = {'renew': opt.bHMMRenew, 'nState': 25, 'cov': 8., 'scale': 8.0,\
         ##                      'add_logp_d': False}
