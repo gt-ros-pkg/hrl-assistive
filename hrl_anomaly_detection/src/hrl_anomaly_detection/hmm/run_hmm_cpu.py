b8197a24bccf870bf267c7623ab6c624d0d996cc
pidipidi <deric.park@gatech.edu>
2016-08-22 13:29:34 -0400

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/hmm/run_hmm_cpu.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/hmm/run_hmm_cpu.py
@@ -126,12 +126,12 @@ def tune_hmm(parameters, cv_dict, param_dict, processed_data_path, verbose=False
               hmm.getHMMinducedFeaturesFromRawFeatures(ml, normalTrainData, abnormalTrainData, \
                                                        startIdx, add_logp_d)
 
-            if len(np.shape(ll_classifier_train_X))<3:
-                print "feature extractin failed", np.shape(ll_classifier_train_X)
-                scores.append(-1.0 * 1e+10)
-                ret = 'Failure'
-                sys.exit()
-                break
+            ## if len(np.shape(ll_classifier_train_X))<3:
+            ##     print "feature extractin failed", np.shape(ll_classifier_train_X)
+            ##     scores.append(-1.0 * 1e+10)
+            ##     ret = 'Failure'
+            ##     sys.exit()
+            ##     break
             if np.amax( np.array(ll_classifier_train_X)[:,:,0] ) < 0:
                 print "Negative likelihoods"
                 scores.append(-1.0 * 1e+10)
