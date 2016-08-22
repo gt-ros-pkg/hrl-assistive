d1c38af4663fafd5b5662c658b09367259ac3e58
pidipidi <deric.park@gatech.edu>
2016-08-22 15:14:45 -0400

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/icra2017test.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/icra2017test.py
@@ -522,7 +522,7 @@ def evaluation_online(subject_names, task_name, raw_data_path, processed_data_pa
     startIdx    = 4
     method_list = ROC_dict['methods'] 
     nPoints     = ROC_dict['nPoints']
-    nPtrainData  = 10
+    nPtrainData  = 20
     nTrainOffset = 2
     nTrainTimes  = 5 #10
     nNormalTrain = 30
