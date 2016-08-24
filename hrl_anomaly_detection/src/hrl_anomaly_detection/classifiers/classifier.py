d241b3e39d6f73c8d04d58105b3ecdf79da45296
pidipidi <deric.park@gatech.edu>
2016-08-24 16:56:35 -0400

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/classifiers/classifier.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/classifiers/classifier.py
@@ -1160,11 +1160,15 @@ def run_classifiers(idx, processed_data_path, task_name, method,\
             new_Y = []
             new_idx = []
             for i in xrange(len(ll_classifier_train_X)):
-                idx_list = range(len(ll_classifier_train_X[i]))
-                random.shuffle(idx_list)
-                new_X.append( np.array(ll_classifier_train_X)[i,idx_list[:nSubSample]].tolist() )
-                new_Y.append( np.array(ll_classifier_train_Y)[i,idx_list[:nSubSample]].tolist() )
-                new_idx.append( np.array(ll_classifier_train_idx)[i,idx_list[:nSubSample]].tolist() )
+                idx_list = np.linspace(startIdx, len(ll_classifier_train_X[i])-1, nSubSample).astype(int)
+                new_X.append( np.array(ll_classifier_train_X)[i,idx_list].tolist() )
+                new_Y.append( np.array(ll_classifier_train_Y)[i,idx_list].tolist() )
+                new_idx.append( np.array(ll_classifier_train_idx)[i,idx_list].tolist() )
+                ## idx_list = range(len(ll_classifier_train_X[i]))
+                ## random.shuffle(idx_list)
+                ## new_X.append( np.array(ll_classifier_train_X)[i,idx_list[:nSubSample]].tolist() )
+                ## new_Y.append( np.array(ll_classifier_train_Y)[i,idx_list[:nSubSample]].tolist() )
+                ## new_idx.append( np.array(ll_classifier_train_idx)[i,idx_list[:nSubSample]].tolist() )
 
             ll_classifier_train_X = new_X
             ll_classifier_train_Y = new_Y
