6e018155b22587657235169485897e007390c51f
pidipidi <deric.park@gatech.edu>
2016-09-01 02:29:00 -0400

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/rss2016test.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/rss2016test.py
@@ -2242,13 +2242,13 @@ if __name__ == '__main__':
         if False:
             step_mag =0.01*param_dict['HMM']['scale'] # need to varying it
             pkl_prefix = 'step_0.01'
-        elif True:
+        elif False:
             step_mag =0.05*param_dict['HMM']['scale'] # need to varying it
             pkl_prefix = 'step_0.05'
         elif False:
             step_mag = 0.1*param_dict['HMM']['scale'] # need to varying it
             pkl_prefix = 'step_0.1'
-        elif False:
+        elif True:
             step_mag =1.0*param_dict['HMM']['scale'] # need to varying it
             pkl_prefix = 'step_1.0'
         else:
