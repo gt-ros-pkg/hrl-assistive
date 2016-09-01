4bbc3270b4282b166a8515680081a561a2735507
Phillip Grice <phillip.grice@gatech.edu>
2016-09-01 11:40:47 -0400

    adding msg def for datalogging, updated file to module pattern

--- assistive_teleop/vci-www/js/utils/datalogging.js
+++ assistive_teleop/vci-www/js/utils/datalogging.js
@@ -1,42 +1,44 @@
-RFH.DataLogger = function (options) {
-    'use strict';
-    var ros = options.ros;
-    var logTopic = options.topic || 'interface_log';
-    var msgType = 'std_msgs/String';
+var RFH = (function (module) {
+    module.DataLogger = function (options) {
+        'use strict';
+        var ros = options.ros;
+        var logTopic = options.topic || 'interface_log';
+        var msgType = 'std_msgs/String';
 
+        ros.getMsgDetails(msgType);
+        var logPub = new ROSLIB.Topic({
+            ros: ros,
+            name: logTopic,
+            messageType: msgType
+        });
+        logPub.advertise();
 
-    ros.getMsgDetails(msgType);
-    var logPub = new ROSLIB.Topic({
-        ros: ros,
-        name: logTopic,
-        messageType: msgType
-    });
-    logPub.advertise();
-
-    var idClassString = function (element) {
-        var str = element.tagName + '#'+element.id;
-        for (var i=0; i < element.classList.length; i += 1) {
-            str += '.'+element.classList[i].toString();
+        var idClassString = function (element) {
+            var str = element.tagName + '#'+element.id;
+            for (var i=0; i < element.classList.length; i += 1) {
+                str += '.'+element.classList[i].toString();
+            }
+            return str;
         }
-        return str;
-    }
 
-    var getParentage = function (element) {
-        var str = idClassString(element);
-        while (element.parentElement !== null) {
-            element = element.parentElement;
-            str = idClassString(element) + ' > ' + str;
+        var getParentage = function (element) {
+            var str = idClassString(element);
+            while (element.parentElement !== null) {
+                element = element.parentElement;
+                str = idClassString(element) + ' > ' + str;
+            };
+            return str;
         };
-        return str;
+
+        var logClick = function (event, ui) {
+            var msg = ros.composeMsg(msgType);
+    //        msg.timestamp = new Date().toISOString();
+            msg.data = getParentage(event.target)
+            logPub.publish(msg);
+        };
+
+        $('*').on('click.datalogging', logClick);
     };
+    return module;
 
-    var logClick = function (event, ui) {
-        var msg = ros.composeMsg(msgType);
-//        msg.timestamp = new Date().toISOString();
-        msg.data = getParentage(event.target)
-        logPub.publish(msg);
-    };
-
-    $('*').on('click.datalogging', logClick);
-};
-
+})(RFH||{});
