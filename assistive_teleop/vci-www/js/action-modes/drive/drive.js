adfe90d70e64586090ad9da95145a10c41b3de8e
Phillip Grice <phillip.grice@gatech.edu>
2016-09-22 00:52:38 -0700

    fixing drive missing current stop for pointing arrows...

--- assistive_teleop/vci-www/js/action-modes/drive/drive.js
+++ assistive_teleop/vci-www/js/action-modes/drive/drive.js
@@ -362,7 +362,7 @@ var RFH = (function (module) {
             self.currentStop = stopName;
             $('#drive-dir-icon path').css({'fill':'rgb(30,220,250)'});
             $('#drive-dir-icon path.'+stopName).css({'fill':'#ffffff'});
-            head.setPosition(angs[0], angs[1]);
+            trackHeadPosition(stopName);
         };
 
         var trackHeadPosition = function (stopName) {
@@ -376,7 +376,7 @@ var RFH = (function (module) {
             Snap.load('./css/icons/drive-direction-icon.svg', function (icon_svg) {
                 driveDirIcon.append(icon_svg.select('g'));
                 var wedgeClickCB = function (e) {
-                    trackHeadPosition(e.target.classList[0]);
+                    moveToStop(e.target.classList[0]);
                 };
                 var wedges = driveDirIcon.selectAll('path');
                 for (var i=0; i<wedges.length; i+=1) {
@@ -532,7 +532,7 @@ var RFH = (function (module) {
             base.pubCmd(cmd.x, cmd.y, cmd.theta);
 //            console.log("Sent ", cmd);
             timer = setTimeout(function(){self.sendCmd(self.cmd);}, 50);
-            console.log("Set Timer: ", timer);
+//            console.log("Set Timer: ", timer);
         };
 
         $(document).on("mouseleave.rfh mouseout.rfh", self.setUnsafe);
@@ -548,7 +548,7 @@ var RFH = (function (module) {
             self.showGoal();
             $viewer.show();
             self.baseContactDisplay.show();
-            trackHeadPosition(getNearestStop());
+            moveToStop(getNearestStop());
             self.$div.on('resize.rfh', self.updateLineOffsets);
             $('#controls h3').text("Head Controls");
         };
