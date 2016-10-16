89fe23e299873c40eeb0bc2a600f055178a9b9b9
pidipidi <deric.park@gatech.edu>
2016-10-16 01:56:50 -0400

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/auro2016.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/auro2016.py
@@ -463,7 +463,7 @@ if __name__ == '__main__':
     elif opt.CLF_param_search:
         from hrl_anomaly_detection.classifiers import opt_classifier as clf_opt
         method = 'hmmgp'
-        clf_opt.tune_classifier(save_data_path, opt.task, method, param_dict, n_jobs=1, n_iter_search=2)
+        clf_opt.tune_classifier(save_data_path, opt.task, method, param_dict, n_jobs=8, n_iter_search=1000)
 
     elif opt.bEvaluationAll or opt.bDataGen:
         if opt.bHMMRenew: param_dict['ROC']['methods'] = ['fixed'] 
