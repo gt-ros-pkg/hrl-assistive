40e05b9cf07e3954b9427afb80d63f025fecca3c
pidipidi <deric.park@gatech.edu>
2016-10-26 15:49:37 -0400

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/classifiers/opt_classifier.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/classifiers/opt_classifier.py
@@ -219,9 +219,9 @@ def tune_classifier(save_data_path, task_name, method, param_dict, param_dist=No
                 #'weight': expon(scale=0.3),\
                 ## 'weight': uniform(np.exp(-2.15), np.exp(-0.1)),
         elif 'hmmgp' in method:            
-            param_dist = {'ths_mult': uniform(-12.0,7.0),\
-                          'nugget': uniform(50.0,100.0),\
-                          'theta0': uniform(1.0,0.5)}
+            param_dist = {'ths_mult': uniform(-15.0,7.0),\
+                          'nugget': uniform(40.0,80.0),\
+                          'theta0': uniform(0.8,0.5)}
                           ## 'nugget': [10, 50, 100, 200],\
                           ## 'theta0': [0.5, 1.0, 1.5]}
         elif 'progress' in method:            
