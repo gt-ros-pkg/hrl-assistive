086fce2b31f0346330d78283c7e6cf9342e46ff5
pidipidi <deric.park@gatech.edu>
2016-10-26 13:57:31 -0400

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/rss2016test.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/rss2016test.py
@@ -2255,11 +2255,11 @@ if __name__ == '__main__':
         elif opt.task == 'feeding':
             nPoints = 50
             ## param_dict['SVM']['hmmosvm_nu'] = 0.1
-            param_dict['ROC']['hmmgp_param_range']  = np.logspace(1.0, 3.0, nPoints)*-1.0
+            param_dict['ROC']['hmmgp_param_range']  = -np.logspace(0.0, 3.0, nPoints)+2.0
             param_dict['ROC']['kmean_param_range']  = np.logspace(0.16, 0.8, nPoints)*-1.0
-            param_dict['ROC']['progress_param_range'] = -np.logspace(0.0, 2.5, nPoints)            
+            param_dict['ROC']['progress_param_range'] = -np.logspace(0.0, 2.5, nPoints)+2.0            
             param_dict['ROC']['osvm_param_range']     = np.logspace(-5,1,nPoints)
-            param_dict['ROC']['hmmosvm_param_range']  = np.logspace(-5,0,nPoints)
+            param_dict['ROC']['hmmosvm_param_range']  = np.logspace(-4,1,nPoints)
             param_dict['ROC']['fixed_param_range']  = np.linspace(2.0, -2.5, nPoints)
             param_dict['ROC']['change_param_range'] = np.linspace(5.0, -55.0, nPoints)
 
@@ -2267,7 +2267,7 @@ if __name__ == '__main__':
         if False:
             step_mag =0.01*param_dict['HMM']['scale'] # need to varying it
             pkl_prefix = 'step_0.01'
-        elif 1:
+        elif 0:
             step_mag =0.05*param_dict['HMM']['scale'] # need to varying it
             pkl_prefix = 'step_0.05'
         elif 1:
