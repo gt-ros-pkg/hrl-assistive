1fd6dd56730d5cb444fdc241e4ffd3d8af6fbd71
pidipidi <deric.park@gatech.edu>
2016-10-31 11:55:49 -0400

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/auro2016.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/auro2016.py
@@ -489,7 +489,7 @@ if __name__ == '__main__':
         elif 0:
             step_mag =0.05*param_dict['HMM']['scale'] # need to varying it
             pkl_prefix = 'step_0.05'
-        elif 1:
+        elif 0:
             step_mag = 0.1*param_dict['HMM']['scale'] # need to varying it
             pkl_prefix = 'step_0.1'
         ## elif 0:
