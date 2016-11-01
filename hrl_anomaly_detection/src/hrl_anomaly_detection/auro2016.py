5b10a222c2c0e0a8a5cdd3d19f4e494adaa0a5b0
pidipidi <deric.park@gatech.edu>
2016-11-01 14:03:28 -0400

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/auro2016.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/auro2016.py
@@ -455,8 +455,8 @@ if __name__ == '__main__':
 
     elif opt.HMM_param_search:
         from hrl_anomaly_detection.hmm import run_hmm_cpu as hmm_opt
-        parameters = {'nState': [20, 25], 'scale': np.linspace(1.0,20.0,10), \
-                      'cov': np.linspace(1.0,4.0,3) }
+        parameters = {'nState': [20], 'scale': np.linspace(1.0,20.0,5), \
+                      'cov': np.linspace(1.0,4.0,2) }
         max_check_fold = len(subjects) #5 #None
         no_cov = False
         method = 'hmmgp'
