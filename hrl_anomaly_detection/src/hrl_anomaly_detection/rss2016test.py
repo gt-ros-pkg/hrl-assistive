2c12764e2ce9534df83763c229e9e47501aa83e8
pidipidi <deric.park@gatech.edu>
2016-11-04 12:21:05 -0400

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/rss2016test.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/rss2016test.py
@@ -1794,8 +1794,8 @@ if __name__ == '__main__':
 
     elif opt.bEvaluationAccParam or opt.bEvaluationWithNoise:
         ## param_dict['ROC']['methods']     = ['osvm', 'fixed', 'change', 'hmmosvm', 'progress', 'hmmgp']
-        param_dict['ROC']['methods']     = ['fixed']
-        param_dict['ROC']['update_list'] = ['fixed']
+        param_dict['ROC']['methods']     = ['fixed', 'hmmgp']
+        param_dict['ROC']['update_list'] = ['hmmgp']
         if opt.bNoUpdate: param_dict['ROC']['update_list'] = []        
         nPoints = param_dict['ROC']['nPoints']
 
@@ -1806,10 +1806,10 @@ if __name__ == '__main__':
         if opt.task == 'pushing_microblack':
             param_dict['ROC']['progress_param_range'] = np.logspace(0.01, 1.1, nPoints)*-1.0
             param_dict['ROC']['change_param_range'] = np.logspace(0.0, 0.9, nPoints)*-1.0
-            param_dict['ROC']['hmmgp_param_range']  = np.logspace(-1, 1.8, nPoints)*-1.0
+            param_dict['ROC']['hmmgp_param_range']  = np.logspace(-3, 2.8, nPoints)*-1.0
             param_dict['ROC']['osvm_param_range']  = np.logspace(-4., 0.0, nPoints)
             param_dict['ROC']['hmmosvm_param_range']  = np.logspace(-6.0, 1.0, nPoints)
-            param_dict['ROC']['fixed_param_range']  = np.linspace(-0.2, 0.4, nPoints )
+            param_dict['ROC']['fixed_param_range']  = np.linspace(-3.2, 1.4, nPoints )
         elif opt.task == 'pushing_microwhite':
             param_dict['ROC']['change_param_range'] = np.logspace(0.0, 0.9, nPoints)*-1.0
             param_dict['ROC']['hmmgp_param_range']  = np.logspace(-0.5, 2.0, nPoints)*-1.0
@@ -1833,7 +1833,7 @@ if __name__ == '__main__':
         elif 0:
             step_mag =0.05*param_dict['HMM']['scale'] # need to varying it
             pkl_prefix = 'step_0.05'
-        elif 0:
+        elif 1:
             step_mag = 0.1*param_dict['HMM']['scale'] # need to varying it
             pkl_prefix = 'step_0.1'
         ## elif 0:
