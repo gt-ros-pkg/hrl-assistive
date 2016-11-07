904b0f6b5a2d8f856e23d3320544862ea6ffdec0
pidipidi <deric.park@gatech.edu>
2016-11-07 15:46:00 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/auro2016.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/auro2016.py
@@ -497,7 +497,7 @@ if __name__ == '__main__':
     elif opt.bEvaluationAccParam or opt.bEvaluationWithNoise:
         ## param_dict['ROC']['methods'] = ['hmmgp'] #'fixed', 
         param_dict['ROC']['methods'] = ['osvm', 'fixed', 'change', 'hmmosvm', 'progress', 'hmmgp']
-        param_dict['ROC']['update_list'] = [ 'osvm', 'hmmosvm' ]
+        param_dict['ROC']['update_list'] = [ 'hmmgp' ]
         if opt.bNoUpdate: param_dict['ROC']['update_list'] = []        
         nPoints = 40
         param_dict['ROC']['nPoints'] = nPoints
