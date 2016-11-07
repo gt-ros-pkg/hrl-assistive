98ae17026ed9d5bdbebdf376761bd5de6709bb05
pidipidi <deric.park@gatech.edu>
2016-11-06 20:01:52 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/AURO2016_params.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/AURO2016_params.py
@@ -312,7 +312,7 @@ def getFeeding(task, data_renew, HMM_renew, CF_renew, rf_center='kinEEPos',local
         ## handFeatures = ['unimodal_ftForce', \
         ##                 'crossmodal_landmarkEEDist']
         handFeatures = ['unimodal_ftForceZ', 'crossmodal_landmarkEEDist']
-        HMM_param_dict = {'renew': HMM_renew, 'nState': 25, 'cov': 3.5, 'scale': 13.444,\
+        HMM_param_dict = {'renew': HMM_renew, 'nState': 25, 'cov': 1.9, 'scale': 2.55,\
                           'add_logp_d': False}
         ## HMM_param_dict = {'renew': HMM_renew, 'nState': 25, 'cov': 6.0, 'scale': 3.0}
         SVM_param_dict = {'renew': CF_renew, 'w_negative': 5.0, 'gamma': 2.049, 'cost': 1.75,\
