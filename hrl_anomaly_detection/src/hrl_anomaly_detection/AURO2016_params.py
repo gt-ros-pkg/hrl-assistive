d2c5f61b59a389b20036ee9266c8461871cb5e09
pidipidi <deric.park@gatech.edu>
2016-11-08 15:25:26 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/AURO2016_params.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/AURO2016_params.py
@@ -50,12 +50,12 @@ def getFeeding(task, data_renew, HMM_renew, CF_renew, rf_center='kinEEPos',local
 
     if dim == 5:
 
-        handFeatures = ['unimodal_audioWristRMS', 'unimodal_ftForceZ', 'crossmodal_landmarkEEAng', \
-                        'crossmodal_landmarkEEDist', 'unimodal_kinJntEff_1']
+        handFeatures = ['unimodal_audioWristRMS', 'unimodal_ftForceZ', \
+                        'crossmodal_landmarkEEDist', 'unimodal_kinJntEff_1', 'unimodal_kinEEChange']
                         
                         ## 'unimodal_fabricForce' ,\, 'unimodal_ftForceY'
                         ## 'unimodal_kinEEChange']
-                        ## 
+                        ## 'crossmodal_landmarkEEAng'
 
             ## ['unimodal_audioWristRMS', 'unimodal_ftForceZ', \
             ##             'crossmodal_landmarkEEDist', 'crossmodal_landmarkEEAng']
