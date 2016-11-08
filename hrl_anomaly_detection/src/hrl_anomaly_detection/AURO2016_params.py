e9ec145fe07e3a97ed16ef07bb088a5efe535cca
pidipidi <deric.park@gatech.edu>
2016-11-08 00:44:14 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/AURO2016_params.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/AURO2016_params.py
@@ -36,7 +36,7 @@ def getParams(task, bDataRenew, bHMMRenew, bCFRenew, dim, rf_center='kinEEPos',\
         param_dict['ROC']['update_list'] = ['fixed' ]
     else:
         param_dict['ROC']['methods'] = [ 'fixed', 'change', 'progress', 'osvm', 'hmmosvm', 'hmmgp']
-        param_dict['ROC']['update_list'] = [ 'hmmgp', 'progress']
+        param_dict['ROC']['update_list'] = [ 'hmmgp', 'progress', 'fixed', 'hmmosvm']
 
     param_dict['SVM']['raw_window_size'] = 5
 
@@ -169,12 +169,12 @@ def getFeeding(task, data_renew, HMM_renew, CF_renew, rf_center='kinEEPos',local
                           }
                           
         ROC_param_dict = {'nPoints': nPoints,\
-                          'hmmgp_param_range': -np.logspace(-1.0, 2.0, nPoints), \
-                          'progress_param_range': -np.logspace(-0.5, 1.2, nPoints)+1.0,\
+                          'hmmgp_param_range': -np.logspace(-0.2, 2.0, nPoints), \
+                          'progress_param_range': -np.logspace(-0.5, 1.4, nPoints)+1.0,\
                           'svm_param_range': np.logspace(-0.8, 2.5, nPoints),\
                           'bpsvm_param_range': np.logspace(-2, 0, nPoints),\
-                          'hmmosvm_param_range': np.logspace(-4.0, 0.5, nPoints),\
-                          'fixed_param_range': np.linspace(0.3, -0.7, nPoints),\
+                          'hmmosvm_param_range': np.logspace(-5.0, 0.6, nPoints),\
+                          'fixed_param_range': np.linspace(0.3, -0.3, nPoints),\
                           'change_param_range': np.logspace(-1.5, 3.0, nPoints)*-1.0+1.0,\
                           'osvm_param_range': np.logspace(-5., 0.5, nPoints),\
                           'cssvm_param_range': np.logspace(0.0, 2.0, nPoints) }
