d7342ea4b18291c279c82650d5050fad22b3260b
Phillip Grice <phillip.grice@gatech.edu>
2016-11-10 15:54:14 -0500

    adding roundtrip times from server to client and back to monitor connection, also bagging these data

--- assistive_teleop/src/assistive_teleop/ping_check.py
+++ assistive_teleop/src/assistive_teleop/ping_check.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 
 import rospy
+from std_msgs.msg import Duration
 from assistive_teleop.msg import Ping
 
 
 class CheckPingServer(object):
-    def __init__(self, outTopic, inTopic):
+    def __init__(self, outTopic, inTopic, rtTopic):
         self.ping_pub = rospy.Publisher(outTopic, Ping, queue_size=0)
         self.ping_sub = rospy.Subscriber(inTopic, Ping, self.ping_cb)
+        self.roundtrip_pub = rospy.Publisher(rtTopic, Duration, queue_size=0)
 
     def ping_cb(self, return_msg):
         print return_msg
@@ -16,8 +18,10 @@ class CheckPingServer(object):
         client_time = return_msg.recv_time
         s_to_c = (client_time - sent_time).to_sec()
         c_to_s = (now - client_time).to_sec()
-        rt = (now - sent_time).to_sec()
+        roundtrip_time = now - sent_time
+        rt = roundtrip_time.to_sec()
         rospy.loginfo("[%s] S->C: %f, C->S: %f, RT: %f", rospy.get_name(), s_to_c, c_to_s, rt)
+        self.roundtrip_pub.publish(roundtrip_time)
 
     def send_ping(self):
         msg = Ping()
@@ -27,7 +31,7 @@ class CheckPingServer(object):
 
 def main():
     rospy.init_node('web_ping_check')
-    cps = CheckPingServer('/ping_relay_out', '/ping_relay_return')
+    cps = CheckPingServer('/ping_relay_out', '/ping_relay_return', '/ping_relay_roundtrip')
     rate = rospy.Rate(0.5)
     while not rospy.is_shutdown():
         cps.send_ping()
