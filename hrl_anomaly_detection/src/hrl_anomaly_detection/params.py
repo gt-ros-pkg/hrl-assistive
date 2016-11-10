65fe832a2aae6faa4340fecf2778a45c56d3d365
pidipidi <deric.park@gatech.edu>
2016-11-10 01:22:02 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/params.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/params.py
@@ -402,7 +402,7 @@ def getPushingMicroWhite(task, data_renew, HMM_renew, CF_renew, rf_center,local_
                           'bpsvm_w_negative': 1.5,\
                           'progress_svm_w_negative': 1.5, 'progress_svm_cost': 15.0, \
                           'progress_svm_gamma': 0.01,\
-                          'nugget': 50.0, 'theta0': 1.0 }
+                          'nugget': 10.0, 'theta0': 1.0 }
                           ## 'nugget': 108.7, 'theta0': 1.174 }
 
         ROC_param_dict = {'methods': [ ],\
