f6c83468cfb7092a73af726cbd4cb0605281859b
pidipidi <deric.park@gatech.edu>
2016-11-18 23:56:44 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/evaluation.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/evaluation.py
@@ -119,14 +119,15 @@ def evaluation_step_noise(subject_names, task_name, raw_data_path, processed_dat
     noise_idx_pkl = os.path.join(processed_data_path, 'noise_idx.pkl')
     noise_idx_l   = []
     if os.path.isfile(noise_idx_pkl) and HMM_dict['renew'] is False:
-        d = ut.load_pickle(noise_idx_pkl)
-        noise_idx_l = d['noise_idx_l']
+        ddd = ut.load_pickle(noise_idx_pkl)
+        noise_idx_l = ddd['noise_idx_l']
     else:
         nLength = len(successData[0][0]) - startIdx    
         for i in xrange(len(successData[0])):
             start_idx = np.random.randint(startIdx, nLength*2/3, 1)[0]        
             noise_idx_l.append(start_idx)
-        ut.save_pickle(d, noise_idx_pkl)
+        ddd['noise_idxl_l'] = noise_idx_l
+        ut.save_pickle(ddd, noise_idx_pkl)
     
 
     #-----------------------------------------------------------------------------------------
