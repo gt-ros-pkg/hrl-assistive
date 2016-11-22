c7b905c8021728b5c45e88ab82668ccf8f40150a
pidipidi <deric.park@gatech.edu>
2016-11-22 01:30:22 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/auro2016.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/auro2016.py
@@ -806,7 +806,7 @@ if __name__ == '__main__':
         ## param_dict['ROC']['methods'] = ['hmmosvm']
         param_dict['ROC']['update_list'] = ['hmmosvm', 'osvm']
         if opt.bNoUpdate: param_dict['ROC']['update_list'] = []        
-        param_dict['ROC']['nPoints'] = nPoints = 2 #100
+        param_dict['ROC']['nPoints'] = nPoints = 4 #100
 
         save_data_path = os.path.expanduser('~')+\
           '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data/'+\
@@ -817,8 +817,8 @@ if __name__ == '__main__':
         param_dict['ROC']['change_param_range'] = np.linspace(-30.0, 10.0, nPoints)
         ## param_dict['ROC']['osvm_param_range']    = np.logspace(-7., -6, nPoints) #np.logspace(-3.5, 0.0, nPoints)
         ## param_dict['ROC']['hmmosvm_param_range'] = np.logspace(-5.5, -4.5, nPoints)
-        param_dict['ROC']['osvm_param_range']    = np.logspace(-6., -5, nPoints) #np.logspace(-3.5, 0.0, nPoints)
-        param_dict['ROC']['hmmosvm_param_range'] = np.logspace(-4., -3.0, nPoints)
+        param_dict['ROC']['osvm_param_range']    = np.logspace(-5., -4, nPoints) #np.logspace(-3.5, 0.0, nPoints)
+        param_dict['ROC']['hmmosvm_param_range'] = np.logspace(-3., -2.0, nPoints)
 
         ## step_mag_list = np.logspace(-2,np.log10(0.5),10)
         step_mag_list = np.linspace(0.0001,0.5,10)
