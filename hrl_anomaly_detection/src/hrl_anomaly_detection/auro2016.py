753989824bc15f74255499162597fc5d86e3d88a
pidipidi <deric.park@gatech.edu>
2016-11-22 11:50:18 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/auro2016.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/auro2016.py
@@ -802,9 +802,8 @@ if __name__ == '__main__':
 
     elif opt.bEvaluationAccParam or opt.bEvaluationWithNoise:
         param_dict['ROC']['methods'] = ['fixed', 'hmmgp', 'osvm', 'hmmosvm', 'progress', 'change']
-        #param_dict['ROC']['methods'] = ['hmmosvm']
+        param_dict['ROC']['methods'] = ['hmmosvm']
         #param_dict['ROC']['methods'] = ['hmmgp']
-        param_dict['ROC']['update_list'] = ['hmm']
         param_dict['ROC']['update_list'] = ['hmmosvm']
         if opt.bNoUpdate: param_dict['ROC']['update_list'] = []        
         ## param_dict['ROC']['nPoints'] = nPoints = 100
@@ -820,7 +819,7 @@ if __name__ == '__main__':
         ## param_dict['ROC']['osvm_param_range']    = np.logspace(0, 1, nPoints) 
         ## param_dict['ROC']['hmmosvm_param_range'] = np.logspace(-1, 0, nPoints)
         param_dict['ROC']['osvm_param_range']    = np.logspace(-4., -2, nPoints) #np.logspace(-3.5, 0.0, nPoints)
-        param_dict['ROC']['hmmosvm_param_range'] = np.logspace(-7.0, -5.5, nPoints)
+        param_dict['ROC']['hmmosvm_param_range'] = np.logspace(-5.8, -5.2, nPoints)
         
         param_dict['SVM']['hmmosvm_nu'] = 0.005
         param_dict['SVM']['osvm_nu'] = 0.001
@@ -836,7 +835,7 @@ if __name__ == '__main__':
                                      no_plot=opt.bNoPlot, delay_plot=True)
         else:
             for i, step_mag in enumerate(step_mag_list):
-                ## if not(i==5): continue
+                if not(i==9): continue
                 ## if not(step_mag > 0.13 and step_mag < 0.14): continue
                 ev.evaluation_step_noise(subjects, opt.task, raw_data_path, save_data_path, param_dict,\
                                          step_mag,\
