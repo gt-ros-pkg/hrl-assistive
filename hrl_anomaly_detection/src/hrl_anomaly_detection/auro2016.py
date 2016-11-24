8d277d72702fd16b475c39b920db9c2b5828a40b
pidipidi <deric.park@gatech.edu>
2016-11-24 11:07:24 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/auro2016.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/auro2016.py
@@ -807,8 +807,9 @@ if __name__ == '__main__':
     elif opt.bEvaluationAccParam or opt.bEvaluationWithNoise:
         ## param_dict['ROC']['methods'] = ['fixed', 'hmmgp', 'osvm', 'hmmosvm', 'progress', 'change']
         param_dict['ROC']['methods'] = ['fixed', 'hmmgp', 'progress']
-        ## param_dict['ROC']['methods'] = ['hmmgp']
-        ## param_dict['ROC']['update_list'] = ['fixed']
+        param_dict['ROC']['methods'] = ['hmmgp']
+        param_dict['ROC']['methods'] = ['fixed']
+        param_dict['ROC']['update_list'] = ['fixed']
         if opt.bNoUpdate: param_dict['ROC']['update_list'] = []        
         param_dict['ROC']['nPoints'] = nPoints = 20 
 
@@ -816,7 +817,7 @@ if __name__ == '__main__':
           '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data/'+\
           str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)+'_acc_param2'
         param_dict['ROC']['hmmgp_param_range']  = -np.logspace(3.0, 3.15, nPoints)
-        param_dict['ROC']['fixed_param_range']  = np.linspace(-0.3, 0.0, nPoints)
+        param_dict['ROC']['fixed_param_range']  = np.linspace(-3.3, -0.3, nPoints)
         param_dict['ROC']['progress_param_range'] = -np.logspace(0.4, 1.5, nPoints)+2.0            
         param_dict['ROC']['change_param_range'] = np.linspace(-30.0, 10.0, nPoints)
         ## param_dict['ROC']['osvm_param_range']    = np.logspace(0, 1, nPoints) 
