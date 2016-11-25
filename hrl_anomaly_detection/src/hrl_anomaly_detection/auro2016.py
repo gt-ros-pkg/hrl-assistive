b5ddb66d6464c5833aac631f16ff4de06f27b723
pidipidi <deric.park@gatech.edu>
2016-11-25 13:02:17 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/auro2016.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/auro2016.py
@@ -841,7 +841,7 @@ if __name__ == '__main__':
 
         ## step_mag_list = np.logspace(-1,np.log10(2.0),5)
         ## step_mag_list = np.logspace(-2,np.log10(1.0),10)
-        ## step_mag_list = np.linspace(0.01,1.5,10)
+        step_mag_list = np.linspace(0.01,1.5,10)
         ## step_mag_list = np.logspace(-2,np.log10(0.5),2)
         ## step_mag_list = np.linspace(0.0001,0.5,10)
 
@@ -853,7 +853,7 @@ if __name__ == '__main__':
                                      no_plot=opt.bNoPlot, delay_plot=True)
         else:
             for i, step_mag in enumerate(step_mag_list):
-                if not(i==6): continue
+                ## if not(i==6): continue
                 ev.evaluation_step_noise(subjects, opt.task, raw_data_path, save_data_path, param_dict,\
                                          step_mag,\
                                          save_pdf=opt.bSavePdf, verbose=opt.bVerbose, debug=opt.bDebug, \
