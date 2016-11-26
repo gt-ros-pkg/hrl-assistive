50a50b28288243269d5dc986afd59a190f1b9d3e
pidipidi <deric.park@gatech.edu>
2016-11-25 21:48:09 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/auro2016.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/auro2016.py
@@ -834,6 +834,17 @@ if __name__ == '__main__':
         param_dict['ROC']['change_param_range'] = np.linspace(-30.0, 10.0, nPoints)
         step_mag_list = np.logspace(-2,np.log10(2.0),20)
 
+        # all dim, temp fp
+        save_data_path = os.path.expanduser('~')+\
+          '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data/'+\
+          str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)+'_acc_param5'
+        param_dict['ROC']['hmmgp_param_range']  = -np.logspace(3.3, 3.13, nPoints)
+        param_dict['ROC']['fixed_param_range']  = np.linspace(-0.1, 0.1, nPoints)
+        param_dict['ROC']['progress_param_range'] = -np.logspace(2.0, 2.3, nPoints)+2.0            
+        param_dict['ROC']['change_param_range'] = np.linspace(-30.0, 10.0, nPoints)
+        step_mag_list = np.logspace(-2,np.log10(2.0),20)
+
+
         # one dim, no temp fp
         ## save_data_path = os.path.expanduser('~')+\
         ##   '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data/'+\
