8a005445b7304fb1a1a087d8e21be61b7231b45b
pidipidi <deric.park@gatech.edu>
2016-11-27 22:55:18 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/auro2016.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/auro2016.py
@@ -809,8 +809,8 @@ if __name__ == '__main__':
         param_dict['ROC']['methods'] = ['hmmgp', 'progress','fixed']
         param_dict['ROC']['methods'] = ['hmmgp']
         ## param_dict['ROC']['methods'] = ['fixed']
-        param_dict['ROC']['methods'] = ['progress']
-        param_dict['ROC']['update_list'] = ['hmmgp']
+        param_dict['ROC']['methods'] = ['progress', 'hmmgp']
+        param_dict['ROC']['update_list'] = ['hmmgp', 'progress']
         if opt.bNoUpdate: param_dict['ROC']['update_list'] = []        
 
         # all one dim, no temp fp
