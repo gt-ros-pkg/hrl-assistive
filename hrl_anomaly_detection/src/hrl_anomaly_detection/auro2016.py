7e8d46d6d6404d5842fd933b4a93cc5b86fc7f1f
pidipidi <deric.park@gatech.edu>
2016-12-02 11:38:57 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/auro2016.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/auro2016.py
@@ -811,7 +811,7 @@ if __name__ == '__main__':
         param_dict['ROC']['methods'] = ['hmmgp']
         #param_dict['ROC']['methods'] = ['fixed']
         ## param_dict['ROC']['methods'] = ['hmmosvm']
-        param_dict['ROC']['methods'] = ['progress']
+        ## param_dict['ROC']['methods'] = ['progress']
         param_dict['ROC']['update_list'] = ['hmmgp']
         if opt.bNoUpdate: param_dict['ROC']['update_list'] = []        
 
