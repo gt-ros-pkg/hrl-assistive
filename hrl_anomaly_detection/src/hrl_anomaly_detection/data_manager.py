e9b5e79f13dff64fd92445d063ba2c8c0e722240
pidipidi <deric.park@gatech.edu>
2016-12-08 13:48:07 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/data_manager.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/data_manager.py
@@ -1638,10 +1638,14 @@ def extractHandFeature(d, feature_list, scale=1.0, cut_data=None, init_param_dic
             ## ftForce = ftForce[[0,1,]]
             
             # magnitude
-            ## unimodal_ftForce_mag = np.linalg.norm(ftForce, axis=0)
-            unimodal_ftForce_mag = np.sum(ftForce**2, axis=0)
+            unimodal_ftForce_mag = np.linalg.norm(ftForce, axis=0)
+            ## unimodal_ftForce_mag = np.sum(ftForce**2, axis=0)
             if offset_flag: #correct???????
                 unimodal_ftForce_mag -= np.mean(unimodal_ftForce_mag[:startOffsetSize])
+
+            # cumulation
+            for i in xrange(1,unimodal_ftForce_mag):
+                unimodal_ftForce_mag[i] += unimodal_ftForce_mag[i-1]
 
             ## unimodal_ftForce_mag -= np.array([unimodal_ftForce_mag[0]]+unimodal_ftForce_mag.tolist()[:-1])
 
