79b900abf591d8e92149ac8dfd4f7805d07bb196
pidipidi <deric.park@gatech.edu>
2016-12-08 12:59:54 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
@@ -658,6 +658,8 @@ if __name__ == '__main__':
 
         param_dict['data_param']['handFeatures'] = ['unimodal_ftForce_delta', 'unimodal_kinJntEff_5']
 
+        param_dict['data_param']['handFeatures'] = ['unimodal_ftForce_delta', 'unimodal_kinJntEff_5',\
+                                                    'crossmodal_landmarkEEAng']
         
 
 
