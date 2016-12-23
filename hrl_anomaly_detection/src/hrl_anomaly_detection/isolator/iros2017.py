d7ab1b935c8537a9d6f9a61700e74880cb2fa0ea
pidipidi <deric.park@gatech.edu>
2016-12-23 01:29:32 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
@@ -980,19 +980,19 @@ if __name__ == '__main__':
 
     elif opt.evaluation_isolation:
 
-        # c12 offset 0 weight -8 [-1], idx none, dict_size
+        # c12 offset 0 weight -8 [-1], idx none, dict_size 0.01
         save_data_path = os.path.expanduser('~')+\
           '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation10/'+\
           str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
         weight = -8.0
         param_dict['SVM']['hmmgp_logp_offset'] = 0.0 #30.0 
 
-        ## # c11 - 0, -5, [-1], dict_size 0.01, normalize, idx_non
-        ## save_data_path = os.path.expanduser('~')+\
-        ##   '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation11/'+\
-        ##   str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
-        ## weight = -5.0
-        ## param_dict['SVM']['hmmgp_logp_offset'] = 0.0 
+        # c11 - 0, -5, [-1], dict_size 0.01, normalize, idx_non
+        save_data_path = os.path.expanduser('~')+\
+          '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation11/'+\
+          str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
+        weight = -5.0
+        param_dict['SVM']['hmmgp_logp_offset'] = 0.0 
 
         ## # c12 - offset 0 weight -5
         ## save_data_path = os.path.expanduser('~')+\
