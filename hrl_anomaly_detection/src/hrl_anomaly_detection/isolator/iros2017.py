a90f5203a1602976926909b338696a3a6afa9882
pidipidi <deric.park@gatech.edu>
2016-12-23 02:02:50 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
@@ -988,11 +988,11 @@ if __name__ == '__main__':
         param_dict['SVM']['hmmgp_logp_offset'] = 0.0 #30.0 
 
         # c11 offset 0 weight -8 [-1], idx none, dict_size 0.05
-        ## save_data_path = os.path.expanduser('~')+\
-        ##   '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation11/'+\
-        ##   str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
-        ## weight = -5.0
-        ## param_dict['SVM']['hmmgp_logp_offset'] = 0.0 
+        save_data_path = os.path.expanduser('~')+\
+          '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation11/'+\
+          str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
+        weight = -5.0
+        param_dict['SVM']['hmmgp_logp_offset'] = 0.0 
 
         ## # c12 - offset 0 weight -5
         ## save_data_path = os.path.expanduser('~')+\
