8c61d5470bb0c83af35dec5e9a1fcfd6a08b39e9
pidipidi <deric.park@gatech.edu>
2016-12-24 22:38:06 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
@@ -726,6 +726,7 @@ def evaluation_isolation(subject_names, task_name, raw_data_path, processed_data
 
         (x_train, y_train, x_test, y_test) = data_dict[idx]
 
+        from sklearn import preprocessing
         scaler = preprocessing.StandardScaler()
         x_train = scaler.fit_transform(x_train)
         x_test  = scaler.transform(x_test)
