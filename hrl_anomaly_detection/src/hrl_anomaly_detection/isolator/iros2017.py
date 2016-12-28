4313c6944cfeceffb0cddad450f811cf1d936c93
pidipidi <deric.park@gatech.edu>
2016-12-28 12:31:54 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
@@ -924,6 +924,7 @@ if __name__ == '__main__':
         param_dict['data_param']['handFeatures'] = ['unimodal_audioWristRMS',  \
                                                     'unimodal_audioWristAzimuth',\
                                                     'unimodal_kinJntEff_1',\
+                                                    'unimodal_ftForce',\
                                                     'unimodal_ftForce_integ',\
                                                     'unimodal_kinEEChange',\
                                                     'crossmodal_landmarkEEDist', \
