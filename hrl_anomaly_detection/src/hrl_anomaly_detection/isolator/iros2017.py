750baf2d33e3fffb392e45a5f79d9d61102b2544
pidipidi <deric.park@gatech.edu>
2016-12-27 23:47:23 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
@@ -1054,6 +1054,14 @@ if __name__ == '__main__':
                                                     'crossmodal_landmarkEEDist', \
                                                     ]
 
+        # 78
+        ## param_dict['data_param']['handFeatures'] = ['unimodal_audioWristRMS',  \
+        ##                                             'unimodal_kinJntEff_1',\
+        ##                                             'unimodal_ftForce_integ',\
+        ##                                             'unimodal_fabricForce',\
+        ##                                             'unimodal_kinEEChange', \
+        ##                                             'crossmodal_landmarkEEDist', \
+        ##                                             ]
         # 80
         ## param_dict['data_param']['handFeatures'] = ['unimodal_audioWristRMS',  \
         ##                                             'unimodal_kinJntEff_1',\
@@ -1081,7 +1089,7 @@ if __name__ == '__main__':
         ##                                             'unimodal_kinEEChange', \
         ##                                             'crossmodal_landmarkEEDist', \
         ##                                             ]
-        param_dict['ROC']['hmmgp_param_range'] = np.logspace(-0.6, 2.3, nPoints)*-1.0 
+        param_dict['ROC']['hmmgp_param_range'] = np.logspace(-0.6, 2.5, nPoints)*-1.0 
                                                     ## 'unimodal_landmarkDist',\
 
 
