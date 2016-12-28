6c664dccb54fd46afecbf0e1fce41bdd09bb7b43
pidipidi <deric.park@gatech.edu>
2016-12-28 03:40:42 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
@@ -1051,7 +1051,7 @@ if __name__ == '__main__':
           '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation7/'+\
           str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
 
-
+        # 84
         param_dict['data_param']['handFeatures'] = ['unimodal_audioWristRMS',  \
                                                     'unimodal_audioWristAzimuth',\
                                                     'unimodal_kinJntEff_1',\
@@ -1062,13 +1062,13 @@ if __name__ == '__main__':
                                                     ]
 
         # 83
-        ## param_dict['data_param']['handFeatures'] = ['unimodal_audioWristRMS',  \
-        ##                                             'unimodal_audioWristAzimuth',\
-        ##                                             'unimodal_kinJntEff_1',\
-        ##                                             'unimodal_ftForce_integ',\
-        ##                                             'unimodal_kinEEChange', \
-        ##                                             'crossmodal_landmarkEEDist', \
-        ##                                             ]
+        param_dict['data_param']['handFeatures'] = ['unimodal_audioWristRMS',  \
+                                                    'unimodal_audioWristAzimuth',\
+                                                    'unimodal_kinJntEff_1',\
+                                                    'unimodal_ftForce_integ',\
+                                                    'unimodal_kinEEChange', \
+                                                    'crossmodal_landmarkEEDist', \
+                                                    ]
 
         # 78
         ## param_dict['data_param']['handFeatures'] = ['unimodal_audioWristRMS',  \
