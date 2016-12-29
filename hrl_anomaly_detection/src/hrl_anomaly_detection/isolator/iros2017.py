7c3a32b786c2dd88cbf56df2724ee42081e177e0
pidipidi <deric.park@gatech.edu>
2016-12-29 03:05:06 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
@@ -1135,28 +1135,28 @@ if __name__ == '__main__':
         
                                                    
 
-        ## # c12, window 10 
-        ## save_data_path = os.path.expanduser('~')+\
-        ##   '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation8/'+\
-        ##   str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
-        ## weight = -20.0
-        ## param_dict['HMM']['scale'] = 6.111 
-        ## param_dict['SVM']['hmmgp_logp_offset'] = 0.0 #30.0
-        ## param_dict['data_param']['handFeatures'] = ['unimodal_audioWristRMS',  \
-        ##                                             'unimodal_kinJntEff_1',\
-        ##                                             'unimodal_ftForce_integ',\
-        ##                                             'unimodal_kinEEChange',\
-        ##                                             ]
-        ## param_dict['data_param']['staticFeatures'] = ['unimodal_audioWristFrontRMS',\
-        ##                                               'unimodal_audioWristAzimuth',\
-        ##                                               'unimodal_ftForceX',\
-        ##                                               'unimodal_ftForceY',\
-        ##                                               'unimodal_ftForceZ',\
-        ##                                               'unimodal_fabricForce',  \
-        ##                                               'unimodal_landmarkDist',\
-        ##                                               'crossmodal_landmarkEEDist', \
-        ##                                               'crossmodal_landmarkEEAng',\
-        ##                                               ]                                                  
+        # c12, window 10 
+        save_data_path = os.path.expanduser('~')+\
+          '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation8/'+\
+          str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
+        weight = -20.0
+        param_dict['HMM']['scale'] = 7.111 
+        param_dict['SVM']['hmmgp_logp_offset'] = 0.0 #30.0
+        param_dict['data_param']['handFeatures'] = ['unimodal_audioWristRMS',  \
+                                                    'unimodal_kinJntEff_1',\
+                                                    'unimodal_ftForce_integ',\
+                                                    'unimodal_kinEEChange',\
+                                                    ]
+        param_dict['data_param']['staticFeatures'] = ['unimodal_audioWristFrontRMS',\
+                                                      'unimodal_audioWristAzimuth',\
+                                                      'unimodal_ftForceX',\
+                                                      'unimodal_ftForceY',\
+                                                      'unimodal_ftForceZ',\
+                                                      'unimodal_fabricForce',  \
+                                                      'unimodal_landmarkDist',\
+                                                      'crossmodal_landmarkEEDist', \
+                                                      'crossmodal_landmarkEEAng',\
+                                                      ]                                                  
         
                                                     
 
