8ceb192913ea0e0efde57be89feb0bc4ce59112f
pidipidi <deric.park@gatech.edu>
2016-12-30 11:35:01 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
@@ -1207,32 +1207,32 @@ if __name__ == '__main__':
         
                                                     
 
-        # c12, window 10 #73
-        save_data_path = os.path.expanduser('~')+\
-          '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation10/'+\
-          str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
-        weight = -32.0
-        param_dict['HMM']['scale'] = 6.111 
-        param_dict['SVM']['hmmgp_logp_offset'] = 0.0 #30.0
-        param_dict['data_param']['handFeatures'] = ['unimodal_audioWristRMS',  \
-                                                    'unimodal_kinJntEff_1',\
-                                                    'unimodal_ftForce_integ',\
-                                                    'unimodal_kinEEChange',\
-                                                    'crossmodal_landmarkEEDist', \
-                                                    ]
+        ## # c12, window 10 #73
+        ## save_data_path = os.path.expanduser('~')+\
+        ##   '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation10/'+\
+        ##   str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
+        ## weight = -32.0
+        ## param_dict['HMM']['scale'] = 6.111 
+        ## param_dict['SVM']['hmmgp_logp_offset'] = 0.0 #30.0
+        ## param_dict['data_param']['handFeatures'] = ['unimodal_audioWristRMS',  \
+        ##                                             'unimodal_kinJntEff_1',\
+        ##                                             'unimodal_ftForce_integ',\
+        ##                                             'unimodal_kinEEChange',\
+        ##                                             'crossmodal_landmarkEEDist', \
+        ##                                             ]
 
-        param_dict['data_param']['dynamicFeatures'] = ['unimodal_ftForce_zero',\
-                                                       'unimodal_ftForceZ',\
-                                                       'crossmodal_landmarkEEDist', \
-                                                       ]                                                    
-        param_dict['data_param']['staticFeatures'] = ['unimodal_audioWristFrontRMS',\
-                                                      'unimodal_audioWristAzimuth',\
-                                                      'unimodal_ftForceX',\
-                                                      'unimodal_ftForceY',\
-                                                      'unimodal_fabricForce',  \
-                                                      'unimodal_landmarkDist',\
-                                                      'crossmodal_landmarkEEAng',\
-                                                      ]                                                  
+        ## param_dict['data_param']['dynamicFeatures'] = ['unimodal_ftForce_zero',\
+        ##                                                'unimodal_ftForceZ',\
+        ##                                                'crossmodal_landmarkEEDist', \
+        ##                                                ]                                                    
+        ## param_dict['data_param']['staticFeatures'] = ['unimodal_audioWristFrontRMS',\
+        ##                                               'unimodal_audioWristAzimuth',\
+        ##                                               'unimodal_ftForceX',\
+        ##                                               'unimodal_ftForceY',\
+        ##                                               'unimodal_fabricForce',  \
+        ##                                               'unimodal_landmarkDist',\
+        ##                                               'crossmodal_landmarkEEAng',\
+        ##                                               ]                                                  
 
         
         # noise: 3,8
