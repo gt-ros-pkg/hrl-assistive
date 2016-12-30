e9b93b53a7052d9bfd183cac7bfe0cdd296ae271
pidipidi <deric.park@gatech.edu>
2016-12-30 00:23:26 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
@@ -945,6 +945,7 @@ if __name__ == '__main__':
           '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation3/'+\
           str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
 
+        # 87.95
         param_dict['data_param']['handFeatures'] = ['unimodal_audioWristRMS',  \
                                                     'unimodal_audioWristAzimuth',\
                                                     'unimodal_kinJntEff_1',\
