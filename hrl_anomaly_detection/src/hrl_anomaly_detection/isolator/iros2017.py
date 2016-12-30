4b2d17577b38dffbc445477959a1d29dfc308f64
pidipidi <deric.park@gatech.edu>
2016-12-30 01:11:37 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
@@ -1159,41 +1159,11 @@ if __name__ == '__main__':
         
                                                    
 
-        ## # c12, window 10 
-        ## save_data_path = os.path.expanduser('~')+\
-        ##   '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation8/'+\
-        ##   str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
-        ## weight = -20.0
-        ## param_dict['HMM']['scale'] = 7.111 
-        ## param_dict['SVM']['hmmgp_logp_offset'] = 0.0 #30.0
-        ## param_dict['data_param']['handFeatures'] = ['unimodal_audioWristRMS',  \
-        ##                                             'unimodal_kinJntEff_1',\
-        ##                                             'unimodal_ftForce_integ',\
-        ##                                             'unimodal_kinEEChange',\
-        ##                                             'crossmodal_landmarkEEDist', \
-        ##                                             ]
-
-        ## param_dict['data_param']['dynamicFeatures'] = ['unimodal_ftForce_zero',\
-        ##                                                'unimodal_ftForceZ',\
-        ##                                                'crossmodal_landmarkEEDist', \
-        ##                                                ]                                                    
-        ## param_dict['data_param']['staticFeatures'] = ['unimodal_audioWristFrontRMS',\
-        ##                                               'unimodal_audioWristAzimuth',\
-        ##                                               'unimodal_ftForceX',\
-        ##                                               'unimodal_ftForceY',\
-        ##                                               'unimodal_ftForceZ',\
-        ##                                               'unimodal_fabricForce',  \
-        ##                                               'unimodal_landmarkDist',\
-        ##                                               'crossmodal_landmarkEEAng',\
-        ##                                               ]                                                  
-        
-                                                    
-
-        # c11, window 10 #73
+        # c12, window 10 
         save_data_path = os.path.expanduser('~')+\
-          '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation10/'+\
+          '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation8/'+\
           str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
-        weight = -32.0
+        weight = -20.0
         param_dict['HMM']['scale'] = 7.111 
         param_dict['SVM']['hmmgp_logp_offset'] = 0.0 #30.0
         param_dict['data_param']['handFeatures'] = ['unimodal_audioWristRMS',  \
@@ -1211,12 +1181,42 @@ if __name__ == '__main__':
                                                       'unimodal_audioWristAzimuth',\
                                                       'unimodal_ftForceX',\
                                                       'unimodal_ftForceY',\
+                                                      'unimodal_ftForceZ',\
                                                       'unimodal_fabricForce',  \
                                                       'unimodal_landmarkDist',\
                                                       'crossmodal_landmarkEEAng',\
                                                       ]                                                  
-                                                      ## 'unimodal_ftForce',\
-                                                      ## 'unimodal_kinEEChange',\
+        
+                                                    
+
+        ## # c11, window 10 #73
+        ## save_data_path = os.path.expanduser('~')+\
+        ##   '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation10/'+\
+        ##   str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
+        ## weight = -32.0
+        ## param_dict['HMM']['scale'] = 7.111 
+        ## param_dict['SVM']['hmmgp_logp_offset'] = 0.0 #30.0
+        ## param_dict['data_param']['handFeatures'] = ['unimodal_audioWristRMS',  \
+        ##                                             'unimodal_kinJntEff_1',\
+        ##                                             'unimodal_ftForce_integ',\
+        ##                                             'unimodal_kinEEChange',\
+        ##                                             'crossmodal_landmarkEEDist', \
+        ##                                             ]
+
+        ## param_dict['data_param']['dynamicFeatures'] = ['unimodal_ftForce_zero',\
+        ##                                                'unimodal_ftForceZ',\
+        ##                                                'crossmodal_landmarkEEDist', \
+        ##                                                ]                                                    
+        ## param_dict['data_param']['staticFeatures'] = ['unimodal_audioWristFrontRMS',\
+        ##                                               'unimodal_audioWristAzimuth',\
+        ##                                               'unimodal_ftForceX',\
+        ##                                               'unimodal_ftForceY',\
+        ##                                               'unimodal_fabricForce',  \
+        ##                                               'unimodal_landmarkDist',\
+        ##                                               'crossmodal_landmarkEEAng',\
+        ##                                               ]                                                  
+        ##                                               ## 'unimodal_ftForce',\
+        ##                                               ## 'unimodal_kinEEChange',\
 
         
         # noise: 3,8
