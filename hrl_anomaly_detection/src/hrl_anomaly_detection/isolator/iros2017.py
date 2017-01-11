71115ea98ff32191c30d9cff16488f275bdc5721
pidipidi <deric.park@gatech.edu>
2017-01-11 02:01:02 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
@@ -912,8 +912,8 @@ def evaluation_isolation2(subject_names, task_name, raw_data_path, processed_dat
         HMM_dict_local['scale'] = param_dict['HMM']['scale'][i]
 
         #
-        #temp
-        if i==0: continue
+        ## #temp
+        ## if i==0: continue
         
 
         # Training HMM, and getting classifier training and testing data
