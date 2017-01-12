1f9a1dae7be30bd446f5349e78dac489c5acd2b0
pidipidi <deric.park@gatech.edu>
2017-01-12 00:13:03 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
@@ -1299,7 +1299,7 @@ if __name__ == '__main__':
 
         # -------------------------------------------------------------------------------------
         # 85 25 @ fz
-        # 86 37 @ fzero amin
+        # 86 37 @ fzero 
         # 85.5 31 @ fzero fz
         save_data_path = os.path.expanduser('~')+\
           '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation6/'+\
@@ -1315,7 +1315,7 @@ if __name__ == '__main__':
                                                      'crossmodal_landmarkEEDist', \
                                                     ]]
         ## param_dict['HMM']['scale']   = [7.0, 13.0]
-        param_dict['HMM']['scale']   = [7.0, 11.0]
+        param_dict['HMM']['scale']   = [7.0, 13.0]
         param_dict['SVM']['hmmgp_logp_offset'] = 0 #30.0 #50.0
         param_dict['ROC']['hmmgp1_param_range'] = np.logspace(0.2, 2.5, nPoints)*-1.0+1.0
         param_dict['ROC']['hmmgp2_param_range'] = np.logspace(-0.4, 2.5, nPoints)*-1.0+0.5 #2.
