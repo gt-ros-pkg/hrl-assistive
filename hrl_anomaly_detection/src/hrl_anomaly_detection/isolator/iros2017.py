555542453481c39412da49ad4b12879916a9fbc5
pidipidi <deric.park@gatech.edu>
2017-01-11 23:10:44 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
@@ -1338,18 +1338,18 @@ if __name__ == '__main__':
         param_dict['HMM']['scale'] = [7.0, 12.0]
 
         # ep 1,2,4 max, zero
-        save_data_path = os.path.expanduser('~')+\
-          '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation9/'+\
-          str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
-        weight = [-20.0, -10.0]
-        param_dict['HMM']['scale'] = [7.0, 9.0]
-
-        # c8, # 124 max
         ## save_data_path = os.path.expanduser('~')+\
-        ##   '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation10/'+\
+        ##   '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation9/'+\
         ##   str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
         ## weight = [-20.0, -10.0]
-        ## param_dict['HMM']['scale'] = [7.0, 7.0]
+        ## param_dict['HMM']['scale'] = [7.0, 9.0]
+
+        # c8, # 124 max
+        save_data_path = os.path.expanduser('~')+\
+          '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation10/'+\
+          str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
+        weight = [-20.0, -10.0]
+        param_dict['HMM']['scale'] = [7.0, 7.0]
 
         
         window_steps=5
@@ -1372,8 +1372,8 @@ if __name__ == '__main__':
         param_dict['data_param']['staticFeatures'] = ['unimodal_audioWristFrontRMS',\
                                                       'unimodal_audioWristAzimuth',\
                                                       'unimodal_ftForceX',\
-                                                      'unimodal_ftForceY',\
-                                                      'unimodal_ftForceZ',\
+                                                      ## 'unimodal_ftForceY',\
+                                                      ## 'unimodal_ftForceZ',\
                                                       'unimodal_fabricForce',  \
                                                       'unimodal_landmarkDist',\
                                                       'crossmodal_landmarkEEAng',\
