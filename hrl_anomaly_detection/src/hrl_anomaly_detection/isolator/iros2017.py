3dd021eb24e29b8f7301e347596ad0a1ba70aad9
pidipidi <deric.park@gatech.edu>
2017-01-12 20:50:21 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
@@ -1361,7 +1361,7 @@ if __name__ == '__main__':
         save_data_path = os.path.expanduser('~')+\
           '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation9/'+\
           str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
-        weight = [-20.0, -100.0]
+        weight = [-20.0, -50.0]
         param_dict['HMM']['scale'] = [7.0, 13.0]
         single_detector = False #True
 
