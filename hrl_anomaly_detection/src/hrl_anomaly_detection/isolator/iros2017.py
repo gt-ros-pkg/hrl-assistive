55dced7d356fb638a523eddbf58460a4689afb1d
pidipidi <deric.park@gatech.edu>
2017-01-15 19:15:54 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
@@ -1207,7 +1207,7 @@ if __name__ == '__main__':
         evaluation with selected feature set 5,6
         '''
 
-        # ep  13-78 9-76 11-77 15-78
+        # ep  13-78 9-76 11-77 15-78 17-79
         save_data_path = os.path.expanduser('~')+\
           '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation5/'+\
           str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
@@ -1216,7 +1216,7 @@ if __name__ == '__main__':
                                                      'unimodal_kinDesEEChange', \
                                                      'crossmodal_landmarkEEDist', \
                                                     ]        
-        param_dict['HMM']['scale'] = 17.0
+        param_dict['HMM']['scale'] = 20.0
         param_dict['ROC']['hmmgp_param_range'] = np.logspace(-1.0, 2.7, nPoints)*-1.0 +0.5
        
 
