0389bed3ca7b36efe7941d1e177bf7e4c8c60577
pidipidi <deric.park@gatech.edu>
2017-01-16 17:08:50 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
@@ -1314,13 +1314,13 @@ if __name__ == '__main__':
         param_dict['HMM']['scale'] = [7.0, 13.0]
         single_detector = False 
 
-        ## ## ep  min novel w/ des
-        ## save_data_path = os.path.expanduser('~')+\
-        ##   '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation9/'+\
-        ##   str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
-        ## weight = [-20.0, -50.0]
-        ## param_dict['HMM']['scale'] = [7.0, 13.0]
-        ## single_detector = False
+        ## ep  min novel w/ des 68
+        save_data_path = os.path.expanduser('~')+\
+          '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation9/'+\
+          str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
+        weight = [-20.0, -30.0]
+        param_dict['HMM']['scale'] = [7.0, 13.0]
+        single_detector = False
 
         ## ## c12, 66
         ## save_data_path = os.path.expanduser('~')+\
@@ -1330,13 +1330,13 @@ if __name__ == '__main__':
         ## param_dict['HMM']['scale'] = [7.0, 13.0]
         ## single_detector = False 
 
-        ## c11, #nodes 66
-        save_data_path = os.path.expanduser('~')+\
-          '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation11/'+\
-          str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
-        weight = [-23.0, -30.0]  #23
-        param_dict['HMM']['scale'] = [7.0, 13.0]
-        single_detector = False #True
+        ## ## c11, #nodes 66
+        ## save_data_path = os.path.expanduser('~')+\
+        ##   '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation11/'+\
+        ##   str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
+        ## weight = [-23.0, -30.0]  #23
+        ## param_dict['HMM']['scale'] = [7.0, 13.0]
+        ## single_detector = False #True
 
         # br, 148 min #nodes
         ## save_data_path = os.path.expanduser('~')+\
@@ -1359,9 +1359,9 @@ if __name__ == '__main__':
                                                     'unimodal_kinEEChange',\
                                                     'crossmodal_landmarkEEDist'
                                                     ],\
-                                                    ['unimodal_kinVel',\
+                                                    [#'unimodal_kinVel',\
                                                      'unimodal_ftForce_zero',\
-                                                     ## 'unimodal_kinDesEEChange',\
+                                                     'unimodal_kinDesEEChange',\
                                                      'crossmodal_landmarkEEDist'
                                                     ]]
 
