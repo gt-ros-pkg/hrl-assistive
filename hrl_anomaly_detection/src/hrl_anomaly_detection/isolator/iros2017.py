b17a5b49ab4a79bd179bbd9495e802dc14741265
pidipidi <deric.park@gatech.edu>
2017-01-18 02:02:24 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
@@ -1311,17 +1311,17 @@ if __name__ == '__main__':
         save_data_path = os.path.expanduser('~')+\
           '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation8/'+\
           str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
-        weight = [-25.0, -25.0] # -50]
-        param_dict['HMM']['scale'] = [7.0, 15.0]
+        weight = [-30.0, -30.0] # -50]
+        param_dict['HMM']['scale'] = [7.0, 13.0]
         single_detector = False 
 
-        ## ep 148 min #nodes 68
-        save_data_path = os.path.expanduser('~')+\
-          '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation9/'+\
-          str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
-        weight = [-19.0, -20.0]
-        param_dict['HMM']['scale'] = [7.0, 13.0]
-        single_detector = False
+        ## ## ep 148 min #nodes 68
+        ## save_data_path = os.path.expanduser('~')+\
+        ##   '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation9/'+\
+        ##   str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
+        ## weight = [-19.0, -20.0]
+        ## param_dict['HMM']['scale'] = [7.0, 13.0]
+        ## single_detector = False
 
         ## c12, 68
         ## save_data_path = os.path.expanduser('~')+\
