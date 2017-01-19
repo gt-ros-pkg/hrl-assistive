589373bd61a088fca2f3ad5b0117d065e58e52ae
pidipidi <deric.park@gatech.edu>
2017-01-19 15:15:19 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
@@ -1259,19 +1259,19 @@ if __name__ == '__main__':
         # c8 w/o eedist
 
         #c8 9
-        save_data_path = os.path.expanduser('~')+\
-          '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation5/'+\
-          str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
+        ## save_data_path = os.path.expanduser('~')+\
+        ##   '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation5/'+\
+        ##   str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
 
         ## #c12 9
         ## save_data_path = os.path.expanduser('~')+\
         ##   '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation6/'+\
         ##   str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
           
-        ## #c11 13  
-        ## save_data_path = os.path.expanduser('~')+\
-        ##   '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation7/'+\
-        ##   str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
+        #c8 13  
+        save_data_path = os.path.expanduser('~')+\
+          '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation7/'+\
+          str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
         param_dict['data_param']['handFeatures'] = [['unimodal_audioWristRMS',  \
                                                     'unimodal_kinJntEff_1',\
                                                     'unimodal_ftForce_integ',\
@@ -1283,7 +1283,7 @@ if __name__ == '__main__':
                                                      ## 'unimodal_kinDesEEChange', \
                                                      'crossmodal_landmarkEEDist', \
                                                     ]]
-        param_dict['HMM']['scale']   = [7.0, 13.0] #9 nest
+        param_dict['HMM']['scale']   = [7.0, 15.0] #9 nest
         param_dict['SVM']['hmmgp_logp_offset'] = 0 #30.0 #50.0
         ## param_dict['ROC']['hmmgp0_param_range'] = np.logspace(0.7, 2.0, nPoints)*-1.0+1.0
         ## param_dict['ROC']['hmmgp1_param_range'] = np.logspace(0.7, 2.0, nPoints)*-1.0+0.5 #2.
