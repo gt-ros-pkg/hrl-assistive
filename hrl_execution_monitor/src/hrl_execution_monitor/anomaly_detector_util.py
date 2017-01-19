8405a84a5acbead1025a9c7096f70c337a3f586a
pidipidi <deric.park@gatech.edu>
2017-01-19 18:58:46 -0500

    test param update

--- hrl_execution_monitor/src/hrl_execution_monitor/anomaly_detector_util.py
+++ hrl_execution_monitor/src/hrl_execution_monitor/anomaly_detector_util.py
@@ -248,8 +248,8 @@ if __name__ == '__main__':
     param_dict['HMM']['scale'] = [1.0, 13.0]
 
     #c11 85
-    ## save_data_path = '/home/dpark/hrl_file_server/dpark_data/anomaly/IROS2017/'+opt.task+'_demo2'
-    ## param_dict['HMM']['scale'] = [1.,8.] #[1.0, 11.0]
+    save_data_path = '/home/dpark/hrl_file_server/dpark_data/anomaly/IROS2017/'+opt.task+'_demo2'
+    param_dict['HMM']['scale'] = [1.,1.] #[1.0, 11.0]
 
     ## #c8 
     ## save_data_path = '/home/dpark/hrl_file_server/dpark_data/anomaly/IROS2017/'+opt.task+'_demo3'
