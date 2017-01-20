4b7553f3b806cf9cd1af89c087253eb8fb626825
pidipidi <deric.park@gatech.edu>
2017-01-20 13:10:43 -0500

    test param update

--- hrl_execution_monitor/src/hrl_execution_monitor/anomaly_detector_util.py
+++ hrl_execution_monitor/src/hrl_execution_monitor/anomaly_detector_util.py
@@ -255,7 +255,7 @@ if __name__ == '__main__':
 
     #c12 84. - 10
     save_data_path = '/home/dpark/hrl_file_server/dpark_data/anomaly/IROS2017/'+opt.task+'_demo1'
-    param_dict['ROC']['methods'] = ['progress0', 'progress1']
+    param_dict['ROC']['methods'] = ['progress0'] #, 'progress1']
     param_dict['HMM']['scale'] = [4.0, 4.0]
     param_dict['ROC']['progress0_param_range'] = -np.logspace(0.4, 1.3, nPoints)
     param_dict['ROC']['progress1_param_range'] = -np.logspace(0.4, 1.3, nPoints)
