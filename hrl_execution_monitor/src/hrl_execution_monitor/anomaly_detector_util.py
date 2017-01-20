f84551cfbff4afb9bed21fa671c2d049b8325e6b
pidipidi <deric.park@gatech.edu>
2017-01-20 13:15:57 -0500

    test param update

--- hrl_execution_monitor/src/hrl_execution_monitor/anomaly_detector_util.py
+++ hrl_execution_monitor/src/hrl_execution_monitor/anomaly_detector_util.py
@@ -254,18 +254,18 @@ if __name__ == '__main__':
     param_dict['ROC']['progress1_param_range'] = -np.logspace(-1, 0.6, nPoints),\
 
     #c12 84. - 10
-    ## save_data_path = '/home/dpark/hrl_file_server/dpark_data/anomaly/IROS2017/'+opt.task+'_demo1'
-    ## param_dict['ROC']['methods'] = ['progress0'] #, 'progress1']
-    ## param_dict['HMM']['scale'] = [4.0, 4.0]
-    ## param_dict['ROC']['progress0_param_range'] = -np.logspace(-1., 0.6, nPoints)
-    ## param_dict['ROC']['progress1_param_range'] = -np.logspace(-1, 0.6, nPoints)
+    save_data_path = '/home/dpark/hrl_file_server/dpark_data/anomaly/IROS2017/'+opt.task+'_demo1'
+    param_dict['ROC']['methods'] = ['progress0'] #, 'progress1']
+    param_dict['HMM']['scale'] = [4.0, 4.0]
+    param_dict['ROC']['progress0_param_range'] = -np.logspace(-1., 0.6, nPoints)
+    param_dict['ROC']['progress1_param_range'] = -np.logspace(-1, 0.6, nPoints)
 
     #c8 87.5 - 80
-    save_data_path = '/home/dpark/hrl_file_server/dpark_data/anomaly/IROS2017/'+opt.task+'_demo2'
-    param_dict['ROC']['methods'] = ['progress0'] #, 'progress1']
-    param_dict['HMM']['scale'] = [3.0, 3.0] #15.0]
-    param_dict['ROC']['progress0_param_range'] = -np.logspace(-1, 0.6, nPoints)
-    param_dict['ROC']['progress1_param_range'] = -np.logspace(-1, 0.6, nPoints)
+    ## save_data_path = '/home/dpark/hrl_file_server/dpark_data/anomaly/IROS2017/'+opt.task+'_demo2'
+    ## param_dict['ROC']['methods'] = ['progress0'] #, 'progress1']
+    ## param_dict['HMM']['scale'] = [3.0, 3.0] #15.0]
+    ## param_dict['ROC']['progress0_param_range'] = -np.logspace(-1, 0.6, nPoints)
+    ## param_dict['ROC']['progress1_param_range'] = -np.logspace(-1, 0.6, nPoints)
 
     
 
