a88a2168d7fb3f50cc538b8223743bd3edb9d50a
pidipidi <deric.park@gatech.edu>
2017-01-20 01:41:15 -0500

    test param update

--- hrl_execution_monitor/src/hrl_execution_monitor/anomaly_detector_util.py
+++ hrl_execution_monitor/src/hrl_execution_monitor/anomaly_detector_util.py
@@ -245,15 +245,15 @@ if __name__ == '__main__':
 
     #c8 85 - s80
     save_data_path = '/home/dpark/hrl_file_server/dpark_data/anomaly/IROS2017/'+opt.task+'_demo3'
-    param_dict['HMM']['scale'] = [4.0, 6.0]
+    param_dict['HMM']['scale'] = [1.0, 1.0]
 
     #c11 85.0 - 1
-    save_data_path = '/home/dpark/hrl_file_server/dpark_data/anomaly/IROS2017/'+opt.task+'_demo1'
-    param_dict['HMM']['scale'] = [4.0, 5.0]
+    ## save_data_path = '/home/dpark/hrl_file_server/dpark_data/anomaly/IROS2017/'+opt.task+'_demo1'
+    ## param_dict['HMM']['scale'] = [4.0, 5.0]
 
     #c12 85.27 - 4
-    save_data_path = '/home/dpark/hrl_file_server/dpark_data/anomaly/IROS2017/'+opt.task+'_demo2'
-    param_dict['HMM']['scale'] = [4.,4.] #[1.0, 11.0]
+    ## save_data_path = '/home/dpark/hrl_file_server/dpark_data/anomaly/IROS2017/'+opt.task+'_demo2'
+    ## param_dict['HMM']['scale'] = [4.,4.] #[1.0, 11.0]
 
     
 
