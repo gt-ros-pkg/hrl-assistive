022fddfa2f56def884a5c31b9f225236cff2f561
pidipidi <deric.park@gatech.edu>
2017-01-20 02:34:53 -0500

    test param update

--- hrl_execution_monitor/src/hrl_execution_monitor/anomaly_detector_util.py
+++ hrl_execution_monitor/src/hrl_execution_monitor/anomaly_detector_util.py
@@ -238,18 +238,17 @@ if __name__ == '__main__':
     subject_names = ['s2', 's3','s4','s5', 's6','s7','s8', 's9']
     raw_data_path, save_data_path, param_dict = getParams(opt.task, opt.bDataRenew, \
                                                           opt.bHMMRenew, opt.bCLFRenew)
-    save_data_path = '/home/dpark/hrl_file_server/dpark_data/anomaly/IROS2017/'+opt.task+'_demo'
-
     task_name = 'feeding'
-    param_dict['ROC']['methods'] = ['hmmgp0', 'hmmgp1']
 
     #c8 85 - s80 #best
     save_data_path = '/home/dpark/hrl_file_server/dpark_data/anomaly/IROS2017/'+opt.task+'_demo3'
+    param_dict['ROC']['methods'] = ['hmmgp0', 'hmmgp1']
     param_dict['HMM']['scale'] = [4.0, 4.0]
 
     #c11 84. - 10
-    ## save_data_path = '/home/dpark/hrl_file_server/dpark_data/anomaly/IROS2017/'+opt.task+'_demo1'
-    ## param_dict['HMM']['scale'] = [4.0, 3.0]
+    save_data_path = '/home/dpark/hrl_file_server/dpark_data/anomaly/IROS2017/'+opt.task+'_demo1'
+    param_dict['ROC']['methods'] = ['progress0', 'progress1']
+    param_dict['HMM']['scale'] = [4.0, 4.0]
 
     #c12 84.7 - 10
     ## save_data_path = '/home/dpark/hrl_file_server/dpark_data/anomaly/IROS2017/'+opt.task+'_demo2'
