73cb8cf3abf3f0c7ab34716594ff8b1c8d01503a
pidipidi <deric.park@gatech.edu>
2017-01-28 23:43:24 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
@@ -1226,22 +1226,22 @@ if __name__ == '__main__':
                                                     'crossmodal_landmarkEEDist', \
                                                     ]
         param_dict['HMM']['scale'] = 3.0
-        param_dict['ROC']['progress_param_range'] = -np.logspace(-0.5, 1.3, nPoints)
+        param_dict['ROC']['progress_param_range'] = -np.logspace(-0.1, 1.3, nPoints)
         param_dict['ROC']['methods'] = ['progress']
 
         ## c11 87
-        save_data_path = os.path.expanduser('~')+\
-          '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation5/'+\
-          str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
-        param_dict['data_param']['handFeatures'] = ['unimodal_audioWristRMS',  \
-                                                    'unimodal_kinJntEff_1',\
-                                                    'unimodal_ftForce_integ',\
-                                                    'unimodal_kinEEChange',\
-                                                    'crossmodal_landmarkEEDist', \
-                                                    ]
-        param_dict['HMM']['scale'] = 2.0
-        param_dict['ROC']['progress_param_range'] = -np.logspace(-0.5, 1.3, nPoints)
-        param_dict['ROC']['methods'] = ['progress']
+        ## save_data_path = os.path.expanduser('~')+\
+        ##   '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation5/'+\
+        ##   str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
+        ## param_dict['data_param']['handFeatures'] = ['unimodal_audioWristRMS',  \
+        ##                                             'unimodal_kinJntEff_1',\
+        ##                                             'unimodal_ftForce_integ',\
+        ##                                             'unimodal_kinEEChange',\
+        ##                                             'crossmodal_landmarkEEDist', \
+        ##                                             ]
+        ## param_dict['HMM']['scale'] = 2.0
+        ## param_dict['ROC']['progress_param_range'] = -np.logspace(-0.5, 1.3, nPoints)
+        ## param_dict['ROC']['methods'] = ['progress']
 
         ## # c12
         ## save_data_path = os.path.expanduser('~')+\
