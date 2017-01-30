49c85005be544a4c4b26230dbc82073e60911031
pidipidi <deric.park@gatech.edu>
2017-01-29 23:47:57 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
@@ -1243,7 +1243,7 @@ if __name__ == '__main__':
         param_dict['ROC']['progress_param_range'] = -np.logspace(-1.0, 1.5, nPoints)
         param_dict['ROC']['methods'] = ['progress']
 
-        # c12 14-83.54 15-84.55
+        # c12 12-82 14-83.54 15-84.55
         ## save_data_path = os.path.expanduser('~')+\
         ##   '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation6/'+\
         ##   str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
@@ -1286,10 +1286,10 @@ if __name__ == '__main__':
         # -------------------------------------------------------------------------------------
         ## c12 
         save_data_path = os.path.expanduser('~')+\
-          '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation8/'+\
+          '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation6/'+\
           str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
         param_dict['ROC']['methods'] = ['progress0', 'progress1']
-        param_dict['HMM']['scale']   = [12.0, 12.0]
+        param_dict['HMM']['scale']   = [10.0, 10.0]
         param_dict['HMM']['cov']     = 1.0
           
         
@@ -1304,8 +1304,8 @@ if __name__ == '__main__':
                                                      'crossmodal_landmarkEEDist', \
                                                     ]]
         param_dict['SVM']['hmmgp_logp_offset'] = 0 
-        param_dict['ROC']['progress0_param_range'] = -np.logspace(-0.5, 1.3, nPoints)
-        param_dict['ROC']['progress1_param_range'] = -np.logspace(-0.5, 1.3, nPoints)
+        param_dict['ROC']['progress0_param_range'] = -np.logspace(-0.3, 1.3, nPoints)
+        param_dict['ROC']['progress1_param_range'] = -np.logspace(-0.3, 1.3, nPoints)
         param_dict['ROC']['hmmgp0_param_range'] = np.logspace(0.2, 2.5, nPoints)*-1.0+1.0
         param_dict['ROC']['hmmgp1_param_range'] = np.logspace(0.2, 2.5, nPoints)*-1.0+0.5 #2.
         # -------------------------------------------------------------------------------------
