d4f8e97e66b4cb4838edc17aae653751e23c90e9
pidipidi <deric.park@gatech.edu>
2017-01-31 00:42:48 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
@@ -1131,13 +1131,13 @@ if __name__ == '__main__':
         param_dict['HMM']['scale']   = [7.0, 13.0]
         param_dict['HMM']['cov']     = 1.0
 
-        ## ## c12
-        ## save_data_path = os.path.expanduser('~')+\
-        ##   '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation6/'+\
-        ##   str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
-        ## param_dict['ROC']['methods'] = ['progress0', 'progress1']
-        ## param_dict['HMM']['scale']   = [9.0, 9.0]
-        ## param_dict['HMM']['cov']     = 1.0
+        ## c12
+        save_data_path = os.path.expanduser('~')+\
+          '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation6/'+\
+          str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
+        param_dict['ROC']['methods'] = ['progress0', 'progress1']
+        param_dict['HMM']['scale']   = [9.0, 9.0]
+        param_dict['HMM']['cov']     = 1.0
 
         ## ## ep
         ## save_data_path = os.path.expanduser('~')+\
@@ -1226,14 +1226,14 @@ if __name__ == '__main__':
 
 
         ## c12 1010-70
-        ## save_data_path = os.path.expanduser('~')+\
-        ##   '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation6/'+\
-        ##   str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
-        ## param_dict['ROC']['methods'] = ['progress0', 'progress1']
-        ## weight = [-4.9, -4.9]
-        ## param_dict['HMM']['scale'] = [4.0, 7.0]
-        ## param_dict['HMM']['cov']   = 1.0
-        ## single_detector = False
+        save_data_path = os.path.expanduser('~')+\
+          '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation6/'+\
+          str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
+        param_dict['ROC']['methods'] = ['progress0', 'progress1']
+        weight = [-4.9, -4.9]
+        param_dict['HMM']['scale'] = [9.0, 9.0]
+        param_dict['HMM']['cov']   = 1.0
+        single_detector = False
 
         ## ep
         ## save_data_path = os.path.expanduser('~')+\
