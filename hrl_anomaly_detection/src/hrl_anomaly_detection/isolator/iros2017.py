d49460e83cd54d7f44132e2b1e8da2c68887ff20
pidipidi <deric.park@gatech.edu>
2017-01-30 22:15:48 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
@@ -1105,12 +1105,12 @@ if __name__ == '__main__':
         param_dict['HMM']['cov']     = 1.0
 
         ## c12
-        ## save_data_path = os.path.expanduser('~')+\
-        ##   '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation6/'+\
-        ##   str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
-        ## param_dict['ROC']['methods'] = ['progress0', 'progress1']
-        ## param_dict['HMM']['scale']   = [4.0, 7.0]
-        ## param_dict['HMM']['cov']     = 1.0
+        save_data_path = os.path.expanduser('~')+\
+          '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation6/'+\
+          str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
+        param_dict['ROC']['methods'] = ['progress0', 'progress1']
+        param_dict['HMM']['scale']   = [9.0, 9.0]
+        param_dict['HMM']['cov']     = 1.0
 
         ## c8
         ## save_data_path = os.path.expanduser('~')+\
