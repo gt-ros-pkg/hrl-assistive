70567a051c84b595d68718421b7c3bcd04131ac2
pidipidi <deric.park@gatech.edu>
2017-02-01 02:12:42 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
@@ -1260,14 +1260,14 @@ if __name__ == '__main__':
         ## single_detector = False
 
         ## ep
-        ## save_data_path = os.path.expanduser('~')+\
-        ##   '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation4/'+\
-        ##   str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
-        ## param_dict['ROC']['methods'] = ['progress0', 'progress1']
-        ## weight = [-3., -3.]
-        ## param_dict['HMM']['scale'] = [4.0, 8.0]
-        ## param_dict['HMM']['cov']   = 1.0
-        ## single_detector = False
+        save_data_path = os.path.expanduser('~')+\
+          '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation4/'+\
+          str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
+        param_dict['ROC']['methods'] = ['progress0', 'progress1']
+        weight = [-3., -3.]
+        param_dict['HMM']['scale'] = [4.0, 8.0]
+        param_dict['HMM']['cov']   = 1.0
+        single_detector = False
 
 
 
