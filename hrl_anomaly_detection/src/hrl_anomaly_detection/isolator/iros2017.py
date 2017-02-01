0dbabf6a6767c52c67c58c93b95d490b6eaf8111
pidipidi <deric.park@gatech.edu>
2017-01-31 23:19:35 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
@@ -1205,6 +1205,7 @@ if __name__ == '__main__':
         #-----------------------------------------------------------------------------------
         # 0505-79-70
         # 0507-80-72.5
+        # 0508
         # 0509-82-71.7
         # 0511-86-71
         # 0606-81-66
@@ -1215,6 +1216,7 @@ if __name__ == '__main__':
         # 0613-89-64
         # 0705-77-72
         # 0707-  -73.5
+        # 0708-
         # 0709-82-70
         # 0713-89-63
         # 0909-83-67
@@ -1237,20 +1239,20 @@ if __name__ == '__main__':
           str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
         param_dict['ROC']['methods'] = ['progress0', 'progress1']
         weight = [-4., -4.]
-        param_dict['HMM']['scale'] = [7.0, 9.0]
+        param_dict['HMM']['scale'] = [5.0, 8.0]
         param_dict['HMM']['cov']   = 1.0
         single_detector = False
 
 
         ## c12 1010-70
-        save_data_path = os.path.expanduser('~')+\
-          '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation6/'+\
-          str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
-        param_dict['ROC']['methods'] = ['progress0', 'progress1']
-        weight = [-4.9, -4.9]
-        param_dict['HMM']['scale'] = [7.0, 8.0]
-        param_dict['HMM']['cov']   = 1.0
-        single_detector = False
+        ## save_data_path = os.path.expanduser('~')+\
+        ##   '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation6/'+\
+        ##   str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
+        ## param_dict['ROC']['methods'] = ['progress0', 'progress1']
+        ## weight = [-4.9, -4.9]
+        ## param_dict['HMM']['scale'] = [7.0, 8.0]
+        ## param_dict['HMM']['cov']   = 1.0
+        ## single_detector = False
 
         ## ep
         ## save_data_path = os.path.expanduser('~')+\
