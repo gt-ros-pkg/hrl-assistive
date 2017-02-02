7d1d5d20d52a5e1d843fe5d6fce83c305143dfdb
pidipidi <deric.park@gatech.edu>
2017-02-01 22:14:23 -0500

    modified:   ../src/hrl_execution_monitor/anomaly_detector_util.py

--- hrl_execution_monitor/src/hrl_execution_monitor/anomaly_detector_util.py
+++ hrl_execution_monitor/src/hrl_execution_monitor/anomaly_detector_util.py
@@ -261,6 +261,13 @@ if __name__ == '__main__':
     ## param_dict['ROC']['progress0_param_range'] = -np.logspace(-0.3, 1.2, nPoints)
     ## param_dict['ROC']['progress1_param_range'] = -np.logspace(-0.3, 1.2, nPoints)
 
+    ## #c12 92 (-3,-4.5) # Best!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    save_data_path = '/home/dpark/hrl_file_server/dpark_data/anomaly/IROS2017/'+opt.task+'_demo1'
+    param_dict['ROC']['methods'] = ['progress0', 'progress1']
+    param_dict['HMM']['scale'] = [2, 2.]
+    param_dict['ROC']['progress0_param_range'] = -np.logspace(-0.3, 1.2, nPoints)
+    param_dict['ROC']['progress1_param_range'] = -np.logspace(-0.3, 1.2, nPoints)
+
     ## #c8 87.5 - 80
     ## save_data_path = '/home/dpark/hrl_file_server/dpark_data/anomaly/IROS2017/'+opt.task+'_demo2'
     ## param_dict['ROC']['methods'] = ['progress0', 'progress1']
