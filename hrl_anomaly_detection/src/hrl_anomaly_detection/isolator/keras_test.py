b31290083614c6d789d07cf3b416c6fc3b6e85b7
pidipidi <deric.park@gatech.edu>
2017-02-02 14:42:39 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/keras_test.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/keras_test.py
@@ -64,8 +64,8 @@ def train_isolator_modules(save_data_path, n_labels, verbose=False):
     save_data_path = os.path.join(save_data_path, 'keras')
 
     # training with signals ----------------------------------
-    train_with_signal(save_data_path, n_labels, nFold, nb_epoch=800, patience=50)
-    train_with_signal(save_data_path, n_labels, nFold, nb_epoch=800, patience=50, load_weights=True)
+    train_with_signal(save_data_path, n_labels, nFold, nb_epoch=800, patience=10)
+    train_with_signal(save_data_path, n_labels, nFold, nb_epoch=800, patience=5, load_weights=True)
 
     # training_with images -----------------------------------
     remove_label = [1]
