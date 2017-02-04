809c2b7a5fb298844264b3220c79110877b9a0b8
pidipidi <deric.park@gatech.edu>
2017-02-04 03:16:33 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/train_isol_net.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/train_isol_net.py
@@ -65,15 +65,15 @@ def train_isolator_modules(save_data_path, n_labels, verbose=False):
     save_data_path = os.path.join(save_data_path, 'keras')
 
     # training with signals ----------------------------------
-    ## kt.train_with_signal(save_data_path, n_labels, nFold, nb_epoch=800, patience=10)
-    ## kt.train_with_signal(save_data_path, n_labels, nFold, nb_epoch=800, patience=5, load_weights=True)
+    kt.train_with_signal(save_data_path, n_labels, nFold, nb_epoch=800, patience=10)
+    kt.train_with_signal(save_data_path, n_labels, nFold, nb_epoch=800, patience=5, load_weights=True)
     ## kt.train_with_signal(save_data_path, n_labels, nFold, nb_epoch=800, patience=5, load_weights=True,
     ##                      test_only=True) #70
     
     # training_with images -----------------------------------
     remove_label = [1]
-    ## kt.get_bottleneck_image(save_data_path, n_labels, nFold, vgg=True, remove_label=remove_label)
-    ## kt.train_top_model_with_image(save_data_path, n_labels, nFold, vgg=True)
+    kt.get_bottleneck_image(save_data_path, n_labels, nFold, vgg=True, remove_label=remove_label)
+    kt.train_top_model_with_image(save_data_path, n_labels, nFold, vgg=True)
     ## kt.train_top_model_with_image(save_data_path, n_labels, nFold, vgg=True, nb_epoch=1000, load_weights=True)
     ## kt.train_top_model_with_image(save_data_path, n_labels, nFold, vgg=True, nb_epoch=1000, load_weights=True,
     ##                               test_only=True)
@@ -86,13 +86,13 @@ def train_isolator_modules(save_data_path, n_labels, verbose=False):
     ##                  load_weights=True)
 
     # training_with all --------------------------------------
-    ## kt.get_bottleneck_mutil(save_data_path, n_labels, nFold, vgg=True)
-    #kt.train_multi_top_model(save_data_path, n_labels, nFold, vgg=True)
+    kt.get_bottleneck_mutil(save_data_path, n_labels, nFold, vgg=True)
+    kt.train_multi_top_model(save_data_path, n_labels, nFold, vgg=True)
     ## kt.train_multi_top_model(save_data_path, n_labels, nFold, vgg=True, load_weights=True)
     ## kt.train_multi_top_model(save_data_path, n_labels, nFold, vgg=True, load_weights=True,
     ##                          test_only=True)
     
-    ## kt.train_with_all(save_data_path, n_labels, nFold, patience=1, nb_epoch=1, vgg=True)
+    kt.train_with_all(save_data_path, n_labels, nFold, patience=1, nb_epoch=1, vgg=True)
     kt.train_with_all(save_data_path, n_labels, nFold, load_weights=True, patience=5, vgg=True)
     ## kt.train_with_all(save_data_path, n_labels, nFold, load_weights=True, patience=5, vgg=True,
     ##                   test_only=True)
