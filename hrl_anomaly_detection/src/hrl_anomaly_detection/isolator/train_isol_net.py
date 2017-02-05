d86be2722fff67cb86683ea37f8caf09439a4eab
pidipidi <deric.park@gatech.edu>
2017-02-05 15:26:19 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/train_isol_net.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/train_isol_net.py
@@ -89,10 +89,10 @@ def train_isolator_modules(save_data_path, n_labels, verbose=False):
     ## kt.train_multi_top_model(save_data_path, n_labels, fold_list, vgg=True, load_weights=True,
     ##                          test_only=True) #74
     
-    ## kt.train_with_all(save_data_path, n_labels, fold_list, patience=1, nb_epoch=1, vgg=True)
+    kt.train_with_all(save_data_path, n_labels, fold_list, patience=1, nb_epoch=1, vgg=True)
     ## kt.train_with_all(save_data_path, n_labels, fold_list, load_weights=True, patience=5, vgg=True)
-    kt.train_with_all(save_data_path, n_labels, fold_list, load_weights=True, patience=5, vgg=True,
-                      test_only=True)
+    ## kt.train_with_all(save_data_path, n_labels, fold_list, load_weights=True, patience=5, vgg=True,
+    ##                   test_only=True)
     return
 
 
