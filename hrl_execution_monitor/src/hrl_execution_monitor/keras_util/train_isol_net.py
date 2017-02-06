4f9a1a9e8236fe7c768e81c65c29066e350601d4
pidipidi <deric.park@gatech.edu>
2017-02-06 10:52:17 -0500

    test param update

--- hrl_execution_monitor/src/hrl_execution_monitor/keras_util/train_isol_net.py
+++ hrl_execution_monitor/src/hrl_execution_monitor/keras_util/train_isol_net.py
@@ -224,7 +224,7 @@ def train_with_image(save_data_path, n_labels, fold_list, nb_epoch=1, load_weigh
         else:
             if vgg: model = km.vgg16_net(np.shape(x_train_img)[1:], n_labels, weights_path, with_img_top=True)
             else: model = km.cnn_net(np.shape(x_train_img)[1:], n_labels, weights_path)
-            optimizer = RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
+            optimizer = RMSprop(lr=0.0001, rho=0.9, epsilon=1e-08, decay=0.0)
             ## model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
         ## optimizer = RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.005)                        
         model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
