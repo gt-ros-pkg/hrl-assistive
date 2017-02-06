1ca2d73de5f17d7757b30a08c07f9a928aed8cfd
pidipidi <deric.park@gatech.edu>
2017-02-06 02:42:24 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/train_isol_net.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/train_isol_net.py
@@ -83,14 +83,14 @@ def train_isolator_modules(save_data_path, n_labels, verbose=False):
     ## kt.train_top_model_with_image(save_data_path, n_labels, fold_list, vgg=True, nb_epoch=1000, load_weights=True,
     ##                               test_only=True)
 
-    ## fold_list = [1]
+    fold_list = [7]
     
     # training_with all --------------------------------------
     ## kt.get_bottleneck_mutil(save_data_path, n_labels, fold_list, vgg=True)
     ## kt.train_multi_top_model(save_data_path, n_labels, fold_list, vgg=True)
-    ## kt.train_multi_top_model(save_data_path, n_labels, fold_list, vgg=True, load_weights=True)
-    kt.train_multi_top_model(save_data_path, n_labels, fold_list, vgg=True, load_weights=True,
-                             test_only=True) #74
+    kt.train_multi_top_model(save_data_path, n_labels, fold_list, vgg=True, load_weights=True)
+    ## kt.train_multi_top_model(save_data_path, n_labels, fold_list, vgg=True, load_weights=True,
+    ##                          test_only=True) #74
 
     # 0.55 0.70 0.77 0.79    0.78 0.78 0.78 0.77 
     ## kt.train_with_all(save_data_path, n_labels, fold_list, patience=1, nb_epoch=1, vgg=True)
