dd718210072ea31f4e9b666b767e653dc2a9d2a1
pidipidi <deric.park@gatech.edu>
2017-02-06 11:37:38 -0500

    test param update

--- hrl_execution_monitor/src/hrl_execution_monitor/keras_util/train_isol_net.py
+++ hrl_execution_monitor/src/hrl_execution_monitor/keras_util/train_isol_net.py
@@ -534,7 +534,7 @@ def train_top_model_with_image(save_data_path, n_labels, fold_list, nb_epoch=400
             else: sys.exit()
             optimizer = SGD(lr=0.001, decay=1e-7, momentum=0.9, nesterov=True)                
             model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
-            model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+            ## model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
 
         
         class_weight={}
@@ -542,9 +542,9 @@ def train_top_model_with_image(save_data_path, n_labels, fold_list, nb_epoch=400
             class_weight[i] = 1.0
         class_weight[1]  = 0.1 # noisy env
         class_weight[6]  = 0.1 # anomalous snd
-        ## class_weight[-3] = 0.5 # spoon miss by sys
-        ## class_weight[-2] = 0.5 # spoon collision by sys
-        ## class_weight[-1] = 0.5 # freeze
+        class_weight[-3] = 0.5 # spoon miss by sys
+        class_weight[-2] = 0.5 # spoon collision by sys
+        class_weight[-1] = 0.5 # freeze
 
         if test_only is False:
             hist = model.fit(x_train, y_train, nb_epoch=nb_epoch, batch_size=4096, shuffle=True,
