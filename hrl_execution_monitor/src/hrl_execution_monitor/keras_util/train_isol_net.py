b98d5b02a7d8a53200a4c947f4a3f1729e1591d4
pidipidi <deric.park@gatech.edu>
2017-02-06 17:58:26 -0500

    test param update

--- hrl_execution_monitor/src/hrl_execution_monitor/keras_util/train_isol_net.py
+++ hrl_execution_monitor/src/hrl_execution_monitor/keras_util/train_isol_net.py
@@ -345,6 +345,7 @@ def train_with_all(save_data_path, n_labels, fold_list, nb_epoch=100, load_weigh
             optimizer = SGD(lr=0.0001, decay=1e-8, momentum=0.9, nesterov=True)                
             optimizer = RMSprop(lr=0.01, rho=0.9, epsilon=1e-08, decay=0.00001)                        
             model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+            model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
 
 
         class_weight={}
