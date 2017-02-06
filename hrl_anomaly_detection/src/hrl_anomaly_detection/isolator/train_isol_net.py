f0bc7fa8738249ca8d17bd0f3a5d42cc90071de5
pidipidi <deric.park@gatech.edu>
2017-02-06 02:53:31 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/train_isol_net.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/train_isol_net.py
@@ -87,7 +87,7 @@ def train_isolator_modules(save_data_path, n_labels, verbose=False):
     
     # training_with all --------------------------------------
     ## kt.get_bottleneck_mutil(save_data_path, n_labels, fold_list, vgg=True)
-    ## kt.train_multi_top_model(save_data_path, n_labels, fold_list, vgg=True)
+    kt.train_multi_top_model(save_data_path, n_labels, fold_list, vgg=True)
     kt.train_multi_top_model(save_data_path, n_labels, fold_list, vgg=True, patience=200, load_weights=True)
     ## kt.train_multi_top_model(save_data_path, n_labels, fold_list, vgg=True, load_weights=True,
     ##                          test_only=True) #74
