0c17e40d4d4237641de9d4044fdc43cba03c4906
pidipidi <deric.park@gatech.edu>
2017-02-06 02:55:45 -0500

    test param update

--- hrl_execution_monitor/src/hrl_execution_monitor/keras_util/train_isol_net.py
+++ hrl_execution_monitor/src/hrl_execution_monitor/keras_util/train_isol_net.py
@@ -687,7 +687,7 @@ def train_multi_top_model(save_data_path, n_labels, fold_list, nb_epoch=3000, lo
             else: sys.exit()
             ## optimizer = SGD(lr=0.001, decay=1e-8, momentum=0.9, nesterov=True)                
             optimizer = RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.001)                        
-            Emodel.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+            model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
             model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
         else:
             if vgg: model = km.vgg_multi_top_net(np.shape(x_train)[1:], n_labels, weights_path)
