8113053e4093eac833f7d041f0e771bb7bbf172c
pidipidi <deric.park@gatech.edu>
2017-02-06 23:30:35 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/train_isol_net.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/train_isol_net.py
@@ -64,7 +64,7 @@ def train_isolator_modules(save_data_path, n_labels, verbose=False):
 
     ## nFold = 1
     fold_list = range(nFold)
-    fold_list = [0,3] #[3,4,7]
+    ## fold_list = [0,3] #[3,4,7]
     ## fold_list = [3,4,5,6,7]
     
 
