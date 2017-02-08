7faa2968216e8fadfd0ca1f349faf80c83dde541
pidipidi <deric.park@gatech.edu>
2017-02-08 14:02:42 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/train_isol_net.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/train_isol_net.py
@@ -332,7 +332,7 @@ if __name__ == '__main__':
     ##   '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation6/'+\
     ##   str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
 
-    # 5 11  4 7
+    # 5,11-4,7
     save_data_path = os.path.expanduser('~')+\
       '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation5/'+\
       str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
