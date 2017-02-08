51f2e72b412f75b5817b2179c2b6d13be144abb7
pidipidi <deric.park@gatech.edu>
2017-02-08 10:17:28 -0500

    test param update

--- hrl_execution_monitor/src/hrl_execution_monitor/anomaly_isolator_util.py
+++ hrl_execution_monitor/src/hrl_execution_monitor/anomaly_isolator_util.py
@@ -200,11 +200,11 @@ if __name__ == '__main__':
     # -----------------------------------------------------------------------
     # ep
     save_data_path = '/home/dpark/hrl_file_server/dpark_data/anomaly/IROS2017/'+opt.task+'_demo5'
-    weight    = [-9.0, -9.0]
+    weight    = [-3.0, -9.0]
     
     ## # c8
-    save_data_path = '/home/dpark/hrl_file_server/dpark_data/anomaly/IROS2017/'+opt.task+'_demo4'
-    weight    = [-4., -6.]
+    ## save_data_path = '/home/dpark/hrl_file_server/dpark_data/anomaly/IROS2017/'+opt.task+'_demo4'
+    ## weight    = [-4., -6.]
     
     # c12
     ## save_data_path = '/home/dpark/hrl_file_server/dpark_data/anomaly/IROS2017/'+opt.task+'_demo'
