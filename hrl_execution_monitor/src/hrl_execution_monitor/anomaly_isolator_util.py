36fb2cc082b3a739abb8bd804e0e7773b97368ea
pidipidi <deric.park@gatech.edu>
2017-02-08 10:15:24 -0500

    test param update

--- hrl_execution_monitor/src/hrl_execution_monitor/anomaly_isolator_util.py
+++ hrl_execution_monitor/src/hrl_execution_monitor/anomaly_isolator_util.py
@@ -212,10 +212,12 @@ if __name__ == '__main__':
 
     # c11 5.2,5.2-65  5.2,6.2-69 maybebest 
     save_data_path = '/home/dpark/hrl_file_server/dpark_data/anomaly/IROS2017/'+opt.task+'_demo3'
-    weight    = [-3.3, -3.3]
+    weight    = [-3.3, -6.3]
     
     param_dict['HMM']['scale'] = [5.0, 11.0]
 
+    #3.3,3.3-54
+    #3.3,6.3
     #5.0,5.0
     #5.2,5.2-65
     #5.2,6.2-69 ==========> Best demo
