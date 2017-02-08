f79e3bc1853615ed7f7b3b136f30ac33b53f3b84
pidipidi <deric.park@gatech.edu>
2017-02-08 01:00:10 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/train_isol_net.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/train_isol_net.py
@@ -192,7 +192,6 @@ def test_isolator(save_data_path, n_labels, vgg=True, save_pdf=False):
     else:
         print "No file"
         sys.exit()
-    print d.keys()
     nFold = len(d.keys())
     del d
 
@@ -219,23 +218,23 @@ def test_isolator(save_data_path, n_labels, vgg=True, save_pdf=False):
         x_train_sig = scaler.fit_transform(x_train_sig)
         x_test_sig  = scaler.transform(x_test_sig)
 
-        ## weights_path = os.path.join(save_data_path,'sig_weights_'+str(idx)+'.h5')
-        ## model = km.sig_net(np.shape(x_train_sig)[1:], n_labels, activ_type='relu')                    
-        ## model.load_weights(weights_path)
-        ## y_pred = model.predict(x_test_sig)
-        ## y_pred_list += np.argmax(y_pred, axis=1).tolist()
-        ## y_test_list += np.argmax(y_test, axis=1).tolist()
-
-
-        weights_path = os.path.join(save_data_path,prefix+'all_weights_'+str(idx)+'.h5')
-        model = km.vgg16_net(np.shape(x_train_img)[1:], n_labels, with_multi_top=True,
-                             input_shape2=np.shape(x_train_sig)[1:],
-                             weights_path=weights_path,
-                             fine_tune=True)
-
-        y_pred = model.predict([x_test_img/255., x_test_sig])
+        weights_path = os.path.join(save_data_path,'sig_weights_'+str(idx)+'.h5')
+        model = km.sig_net(np.shape(x_train_sig)[1:], n_labels, activ_type='relu')                    
+        model.load_weights(weights_path)
+        y_pred = model.predict(x_test_sig)
         y_pred_list += np.argmax(y_pred, axis=1).tolist()
         y_test_list += np.argmax(y_test, axis=1).tolist()
+
+
+        ## weights_path = os.path.join(save_data_path,prefix+'all_weights_'+str(idx)+'.h5')
+        ## model = km.vgg16_net(np.shape(x_train_img)[1:], n_labels, with_multi_top=True,
+        ##                      input_shape2=np.shape(x_train_sig)[1:],
+        ##                      weights_path=weights_path,
+        ##                      fine_tune=True)
+
+        ## y_pred = model.predict([x_test_img/255., x_test_sig])
+        ## y_pred_list += np.argmax(y_pred, axis=1).tolist()
+        ## y_test_list += np.argmax(y_test, axis=1).tolist()
 
     from sklearn.metrics import accuracy_score
     print "score : ", accuracy_score(y_test_list, y_pred_list)
@@ -245,12 +244,12 @@ def test_isolator(save_data_path, n_labels, vgg=True, save_pdf=False):
 
     new_y_pred = copy.copy(y_pred_list)
     new_y_test = copy.copy(y_test_list)
-    for i, y in enumerate(y_pred_list):
-        if y == 9: new_y_pred[i] = 2
-        elif y == 10: new_y_pred[i] = 3
-    for i, y in enumerate(y_test_list):
-        if y == 9: new_y_test[i] = 2
-        elif y == 10: new_y_test[i] = 3
+    ## for i, y in enumerate(y_pred_list):
+    ##     if y == 9: new_y_pred[i] = 2
+    ##     elif y == 10: new_y_pred[i] = 3
+    ## for i, y in enumerate(y_test_list):
+    ##     if y == 9: new_y_test[i] = 2
+    ##     elif y == 10: new_y_test[i] = 3
 
     print np.unique(new_y_pred), np.unique(new_y_test)
 
@@ -280,7 +279,7 @@ def plot_confusion_matrix(y_test_list, y_pred_list, save_pdf=False):
 
     print np.sum(cm,axis=1)
 
-    eviz.plot_confusion_matrix(cm, classes=classes, normalize=False,
+    eviz.plot_confusion_matrix(cm, classes=classes, normalize=True,
                                title='Anomaly Classification', save_pdf=save_pdf)
     
 
