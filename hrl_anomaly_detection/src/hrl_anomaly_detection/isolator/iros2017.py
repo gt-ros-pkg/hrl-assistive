839da6e9905de56c00fdad4136d1e2ee6c2d5642
pidipidi <deric.park@gatech.edu>
2017-02-09 15:43:31 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
@@ -1273,23 +1273,23 @@ if __name__ == '__main__':
 
 
         ## ## aws
-        ## save_data_path = os.path.expanduser('~')+\
-        ##   '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation/'+\
-        ##   str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
-        ## param_dict['ROC']['methods'] = ['progress0', 'progress1']
-        ## param_dict['HMM']['scale'] = [6.0, 13.0]
-        ## param_dict['HMM']['cov']   = 1.0
-        ## single_detector = False 
-        ## ## param_dict['ROC']['weight'] = [-4.2,-8.2]
-
-
         save_data_path = os.path.expanduser('~')+\
-          '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation1/'+\
+          '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation/'+\
           str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
-        param_dict['ROC']['methods'] = ['progress0', 'progress1']
-        param_dict['HMM']['scale'] = [7.0, 7.0]
+        param_dict['ROC']['methods'] = ['hmmgp0', 'hmmgp1']
+        param_dict['HMM']['scale'] = [6.0, 13.0]
         param_dict['HMM']['cov']   = 1.0
         single_detector = False 
+        ## param_dict['ROC']['weight'] = [-4.2,-8.2]
+
+
+        ## save_data_path = os.path.expanduser('~')+\
+        ##   '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation1/'+\
+        ##   str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
+        ## param_dict['ROC']['methods'] = ['progress0', 'progress1']
+        ## param_dict['HMM']['scale'] = [7.0, 7.0]
+        ## param_dict['HMM']['cov']   = 1.0
+        ## single_detector = False 
         ## ## param_dict['ROC']['weight'] = [-4.2,-6.2]
 
         ## save_data_path = os.path.expanduser('~')+\
