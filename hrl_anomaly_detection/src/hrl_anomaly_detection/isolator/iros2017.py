68e4e63a13199d69a5cb2d2e34904e02b6d16e68
pidipidi <deric.park@gatech.edu>
2017-02-08 19:19:01 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
@@ -851,7 +851,7 @@ def evaluation_isolation2(subject_names, task_name, raw_data_path, processed_dat
 
         y_pred = clf.predict(x_test)
         y_pred_list += y_pred.tolist()
-        y_test_list += np.argmax(y_test, axis=1).tolist()
+        y_test_list += y_test.tolist()
 
 
     # ---------------- ROC Visualization ----------------------
@@ -1262,14 +1262,14 @@ if __name__ == '__main__':
 
 
         ## aws
-        ## save_data_path = os.path.expanduser('~')+\
-        ##   '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation/'+\
-        ##   str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
-        ## param_dict['ROC']['methods'] = ['progress0', 'progress1']
-        ## param_dict['HMM']['scale'] = [7.0, 13.0]
-        ## param_dict['HMM']['cov']   = 1.0
-        ## single_detector = False 
-        ## param_dict['ROC']['weight'] = [-4.2,-4.2]
+        save_data_path = os.path.expanduser('~')+\
+          '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation/'+\
+          str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
+        param_dict['ROC']['methods'] = ['progress0', 'progress1']
+        param_dict['HMM']['scale'] = [7.0, 13.0]
+        param_dict['HMM']['cov']   = 1.0
+        single_detector = False 
+        param_dict['ROC']['weight'] = [-4.2,-4.2]
 
 
 
