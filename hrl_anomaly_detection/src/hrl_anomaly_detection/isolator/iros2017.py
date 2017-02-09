dc5dec46e44812f3042910bb611e8c85790f7cac
pidipidi <deric.park@gatech.edu>
2017-02-08 19:09:19 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
@@ -1262,13 +1262,13 @@ if __name__ == '__main__':
 
 
         ## aws
-        save_data_path = os.path.expanduser('~')+\
-          '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation/'+\
-          str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
-        param_dict['ROC']['methods'] = ['progress0', 'progress1']
-        param_dict['HMM']['scale'] = [7.0, 13.0]
-        param_dict['HMM']['cov']   = 1.0
-        single_detector = False 
+        ## save_data_path = os.path.expanduser('~')+\
+        ##   '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation/'+\
+        ##   str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
+        ## param_dict['ROC']['methods'] = ['progress0', 'progress1']
+        ## param_dict['HMM']['scale'] = [7.0, 13.0]
+        ## param_dict['HMM']['cov']   = 1.0
+        ## single_detector = False 
         ## param_dict['ROC']['weight'] = [-4.2,-4.2]
 
 
