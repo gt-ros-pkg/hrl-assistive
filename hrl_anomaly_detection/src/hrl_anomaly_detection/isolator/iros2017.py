bc0811bdefe11b8de81309244f5c15898c5530c3
pidipidi <deric.park@gatech.edu>
2017-02-09 13:03:41 -0500

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/isolator/iros2017.py
@@ -1242,13 +1242,13 @@ if __name__ == '__main__':
 
         
         ## c8 0408-80-75 46-78-74
-        ## save_data_path = os.path.expanduser('~')+\
-        ##   '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation8/'+\
-        ##   str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
-        ## param_dict['ROC']['methods'] = ['progress0', 'progress1']
-        ## param_dict['HMM']['scale'] = [7.0, 7.0]
-        ## param_dict['HMM']['cov']   = 1.0
-        ## single_detector = False 
+        save_data_path = os.path.expanduser('~')+\
+          '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation8/'+\
+          str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
+        param_dict['ROC']['methods'] = ['progress0', 'progress1']
+        param_dict['HMM']['scale'] = [7.0, 7.0]
+        param_dict['HMM']['cov']   = 1.0
+        single_detector = False 
         ## param_dict['ROC']['weight'] = [-4.,-8.]
 
         ## c11 - 0511-86-77 713-88
@@ -1262,13 +1262,13 @@ if __name__ == '__main__':
         ## param_dict['ROC']['weight'] = [-5.2,-5.2]
 
         # c12 5.2,5.2=80  s4,10=77
-        ## save_data_path = os.path.expanduser('~')+\
-        ##   '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation5/'+\
-        ##   str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
-        ## param_dict['ROC']['methods'] = ['progress0', 'progress1']
-        ## param_dict['HMM']['scale'] = [7.0, 11.0]
-        ## param_dict['HMM']['cov']   = 1.0
-        ## single_detector = False 
+        save_data_path = os.path.expanduser('~')+\
+          '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation5/'+\
+          str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
+        param_dict['ROC']['methods'] = ['progress0', 'progress1']
+        param_dict['HMM']['scale'] = [5.0, 3.0]
+        param_dict['HMM']['cov']   = 1.0
+        single_detector = False 
         ## param_dict['ROC']['weight'] = [-5.2,-7.2]
 
 
@@ -1283,13 +1283,13 @@ if __name__ == '__main__':
         ## ## param_dict['ROC']['weight'] = [-4.2,-8.2]
 
 
-        save_data_path = os.path.expanduser('~')+\
-          '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation1/'+\
-          str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
-        param_dict['ROC']['methods'] = ['progress0', 'progress1']
-        param_dict['HMM']['scale'] = [7.0, 11.0]
-        param_dict['HMM']['cov']   = 1.0
-        single_detector = False 
+        ## save_data_path = os.path.expanduser('~')+\
+        ##   '/hrl_file_server/dpark_data/anomaly/AURO2016/'+opt.task+'_data_isolation1/'+\
+        ##   str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
+        ## param_dict['ROC']['methods'] = ['progress0', 'progress1']
+        ## param_dict['HMM']['scale'] = [7.0, 11.0]
+        ## param_dict['HMM']['cov']   = 1.0
+        ## single_detector = False 
         ## ## param_dict['ROC']['weight'] = [-4.2,-6.2]
 
         ## save_data_path = os.path.expanduser('~')+\
@@ -1310,10 +1310,10 @@ if __name__ == '__main__':
                                                     ],\
                                                     [
                                                      ## 'unimodal_kinVel',\
-                                                     'unimodal_ftForce_zero',\
-                                                     'unimodal_ftForceZ',\
+                                                     ## 'unimodal_ftForce_zero',\
+                                                     ## 'unimodal_ftForceZ',\
                                                      'unimodal_kinDesEEChange',\
-                                                     ## 'crossmodal_landmarkEEDist'
+                                                     'crossmodal_landmarkEEDist'
                                                     ]]
 
         # ------------------------------------------------------------------------------
