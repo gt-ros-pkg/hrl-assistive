dd85a0bcad6ff92a26b0f18e0c857dbbc56d274e
pidipidi <deric.park@gatech.edu>
2017-02-12 06:25:57 -0500

    test param update

--- hrl_execution_monitor/src/hrl_execution_monitor/anomaly_isolator_util.py
+++ hrl_execution_monitor/src/hrl_execution_monitor/anomaly_isolator_util.py
@@ -230,7 +230,7 @@ if __name__ == '__main__':
     #9,9-66
     # -----------------------------------------------------------------------
 
-    ## br 5.22,6.22-64, 5.22,5.22-66
+    ## br 5.22,7.22-66, 5.22,6.22-64, 5.22,5.22-66, 6.22,6.22-65
     save_data_path = os.path.expanduser('~')+'/hrl_file_server/dpark_data/anomaly/IROS2017/'+opt.task+'_demo1'
     weight    = [-6.22,-6.22]
     param_dict['HMM']['scale'] = [5.0, 9.0]
@@ -243,10 +243,10 @@ if __name__ == '__main__':
     param_dict['data_param']['noise_mag'] = [[0.03,0.1,0.03,0.08],[0.1,0.05,0.05,0.08]]
     # -----------------------------------------------------------------------
     ## ep
-    ## save_data_path = os.path.expanduser('~')+'/hrl_file_server/dpark_data/anomaly/IROS2017/'+opt.task+'_demo3'
-    ## weight    = [-5.22,-7.22]
-    ## param_dict['HMM']['scale'] = [5.0, 9.0]
-    ## param_dict['data_param']['noise_mag'] = [[0.03,0.1,0.03,0.08],[0.1,0.05,0.05,0.08]]
+    save_data_path = os.path.expanduser('~')+'/hrl_file_server/dpark_data/anomaly/IROS2017/'+opt.task+'_demo3'
+    weight    = [-5.22,-5.22]
+    param_dict['HMM']['scale'] = [5.0, 9.0]
+    param_dict['data_param']['noise_mag'] = [[0.03,0.1,0.03,0.05],[0.1,0.05,0.05,0.05]]
     # -----------------------------------------------------------------------
 
     param_dict['data_param']['handFeatures'] = [['unimodal_audioWristRMS',  \
