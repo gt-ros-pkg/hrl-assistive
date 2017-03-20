292d229000d83362b2c556932d07a770098451dd
pidipidi <deric.park@gatech.edu>
2017-03-20 18:14:25 -0400

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/adaptation/tcds2017.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/adaptation/tcds2017.py
@@ -342,7 +342,7 @@ def evaluation_single_ad(subject_names, task_name, raw_data_path, processed_data
         ml = hmm.learning_hmm(nState, d['nEmissionDim'])
         ml.set_hmm_object(d['A'], d['B'], d['pi'], d['out_a_num'], d['vec_num'], \
                           d['mat_num'], d['u_denom'])
-        ## ret = ml.partial_fit(X_ptrain+noise_arr, learningRate=0.3)
+        ret = ml.partial_fit(X_ptrain+noise_arr, learningRate=0.6)
         ## ret = ml.fit(X_ptrain+noise_arr)
         ## print idx, ret
         ## if np.isnan(ret):
@@ -389,12 +389,12 @@ def evaluation_single_ad(subject_names, task_name, raw_data_path, processed_data
         d['F']            = ml.F
         d['nState']       = nState
         d['startIdx']     = startIdx
-        ## d['ll_classifier_train_X']  = ll_classifier_train_X
-        ## d['ll_classifier_train_Y']  = ll_classifier_train_Y            
-        ## d['ll_classifier_train_idx']= ll_classifier_train_idx
-        d['ll_classifier_train_X']  = ll_classifier_ptrain_X
-        d['ll_classifier_train_Y']  = ll_classifier_ptrain_Y            
-        d['ll_classifier_train_idx']= ll_classifier_ptrain_idx
+        d['ll_classifier_train_X']  = ll_classifier_train_X
+        d['ll_classifier_train_Y']  = ll_classifier_train_Y            
+        d['ll_classifier_train_idx']= ll_classifier_train_idx
+        ## d['ll_classifier_train_X']  = ll_classifier_ptrain_X
+        ## d['ll_classifier_train_Y']  = ll_classifier_ptrain_Y            
+        ## d['ll_classifier_train_idx']= ll_classifier_ptrain_idx
         d['ll_classifier_ptrain_X']  = ll_classifier_ptrain_X
         d['ll_classifier_ptrain_Y']  = ll_classifier_ptrain_Y            
         d['ll_classifier_ptrain_idx']= ll_classifier_ptrain_idx
@@ -429,7 +429,7 @@ def evaluation_single_ad(subject_names, task_name, raw_data_path, processed_data
                                                                          startIdx=startIdx, nState=nState,\
                                                                          n_jobs=n_jobs,\
                                                                          modeling_pkl_prefix=pkl_prefix,\
-                                                                         adaptation=False) \
+                                                                         adaptation=True) \
                                                                          for idx in xrange(len(td['successDataList'])) )
 
     print "finished to run run_classifiers"
@@ -805,16 +805,19 @@ if __name__ == '__main__':
         '''
         nPoints = param_dict['ROC']['nPoints']
                
-        ## b
+        ## c11
         save_data_path = os.path.expanduser('~')+\
           '/hrl_file_server/dpark_data/anomaly/TCDS2017/'+opt.task+'_data_adaptation/'+\
           str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
-        ## ## c11
+        ## ## 
         ## save_data_path = os.path.expanduser('~')+\
         ##   '/hrl_file_server/dpark_data/anomaly/TCDS2017/'+opt.task+'_data_adaptation2'
         ## ## c12
         ## save_data_path = os.path.expanduser('~')+\
         ##   '/hrl_file_server/dpark_data/anomaly/TCDS2017/'+opt.task+'_data_adaptation3'
+        ## c8
+        save_data_path = os.path.expanduser('~')+\
+          '/hrl_file_server/dpark_data/anomaly/TCDS2017/'+opt.task+'_data_adaptation4'
         param_dict['data_param']['handFeatures'] = ['unimodal_audioWristRMS',  \
                                                      'unimodal_kinJntEff_1',\
                                                      'unimodal_ftForce_integ',\
