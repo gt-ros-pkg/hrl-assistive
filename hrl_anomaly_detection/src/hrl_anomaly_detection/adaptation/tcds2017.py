1370d45f70b785e4f6c87156d2ce27519026e63c
pidipidi <deric.park@gatech.edu>
2017-03-21 13:52:52 -0400

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/adaptation/tcds2017.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/adaptation/tcds2017.py
@@ -342,7 +342,7 @@ def evaluation_single_ad(subject_names, task_name, raw_data_path, processed_data
         ml = hmm.learning_hmm(nState, d['nEmissionDim'])
         ml.set_hmm_object(d['A'], d['B'], d['pi'], d['out_a_num'], d['vec_num'], \
                           d['mat_num'], d['u_denom'])
-        ret = ml.partial_fit(X_ptrain+noise_arr, learningRate=0.005 )
+        ret = ml.partial_fit(X_ptrain+noise_arr, learningRate=0.001 )
         ## ret = ml.fit(X_ptrain+noise_arr)
         ## print idx, ret
         if np.isnan(ret):
@@ -819,8 +819,8 @@ if __name__ == '__main__':
         ## save_data_path = os.path.expanduser('~')+\
         ##   '/hrl_file_server/dpark_data/anomaly/TCDS2017/'+opt.task+'_data_adaptation4'
         ## c12
-        ## save_data_path = os.path.expanduser('~')+\
-        ##   '/hrl_file_server/dpark_data/anomaly/TCDS2017/'+opt.task+'_data_adaptation5'
+        save_data_path = os.path.expanduser('~')+\
+          '/hrl_file_server/dpark_data/anomaly/TCDS2017/'+opt.task+'_data_adaptation5'
         param_dict['data_param']['handFeatures'] = ['unimodal_audioWristRMS',  \
                                                      'unimodal_kinJntEff_1',\
                                                      'unimodal_ftForce_integ',\
