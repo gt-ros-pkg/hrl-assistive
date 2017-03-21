ffb5ce8855c3a96c9ed2fb20b6b058c9a0ba540c
pidipidi <deric.park@gatech.edu>
2017-03-21 11:16:23 -0400

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/adaptation/tcds2017.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/adaptation/tcds2017.py
@@ -342,7 +342,7 @@ def evaluation_single_ad(subject_names, task_name, raw_data_path, processed_data
         ml = hmm.learning_hmm(nState, d['nEmissionDim'])
         ml.set_hmm_object(d['A'], d['B'], d['pi'], d['out_a_num'], d['vec_num'], \
                           d['mat_num'], d['u_denom'])
-        ret = ml.partial_fit(X_ptrain+noise_arr, learningRate=0.5, nrSteps=500)
+        ret = ml.partial_fit(X_ptrain+noise_arr, learningRate=0.5, nrSteps=100)
         ## ret = ml.fit(X_ptrain+noise_arr)
         ## print idx, ret
         if np.isnan(ret):
@@ -810,11 +810,11 @@ if __name__ == '__main__':
           '/hrl_file_server/dpark_data/anomaly/TCDS2017/'+opt.task+'_data_adaptation/'+\
           str(param_dict['data_param']['downSampleSize'])+'_'+str(opt.dim)
         ## c11
-        ## save_data_path = os.path.expanduser('~')+\
-        ##   '/hrl_file_server/dpark_data/anomaly/TCDS2017/'+opt.task+'_data_adaptation2'
-        ## ep
         save_data_path = os.path.expanduser('~')+\
-          '/hrl_file_server/dpark_data/anomaly/TCDS2017/'+opt.task+'_data_adaptation3'
+          '/hrl_file_server/dpark_data/anomaly/TCDS2017/'+opt.task+'_data_adaptation2'
+        ## ep
+        ## save_data_path = os.path.expanduser('~')+\
+        ##   '/hrl_file_server/dpark_data/anomaly/TCDS2017/'+opt.task+'_data_adaptation3'
         ## c8
         ## save_data_path = os.path.expanduser('~')+\
         ##   '/hrl_file_server/dpark_data/anomaly/TCDS2017/'+opt.task+'_data_adaptation4'
