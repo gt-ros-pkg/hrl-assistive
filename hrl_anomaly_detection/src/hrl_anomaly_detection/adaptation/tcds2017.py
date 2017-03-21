cd0becdad167ea96d62255a70c3e5d29dc817439
pidipidi <deric.park@gatech.edu>
2017-03-21 15:29:44 -0400

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/adaptation/tcds2017.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/adaptation/tcds2017.py
@@ -343,8 +343,8 @@ def evaluation_single_ad(subject_names, task_name, raw_data_path, processed_data
         ml.set_hmm_object(d['A'], d['B'], d['pi'], d['out_a_num'], d['vec_num'], \
                           d['mat_num'], d['u_denom'])
                           
-        ret = ml.partial_fit(X_ptrain+noise_arr, learningRate=1.0) #0.001 )
-        ## ret = ml.fit(X_ptrain+noise_arr)
+        ## ret = ml.partial_fit(X_ptrain+noise_arr, learningRate=1.0) #0.001 )
+        ret = ml.fit(X_ptrain+noise_arr)
         ## print idx, ret
         if np.isnan(ret):
             print "kFold_list ........ partial fit error... "
