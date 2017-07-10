e779fcf41fe169cbcc331d33e41acd080a1923bf
pidipidi <deric.park@gatech.edu>
2017-07-09 23:03:29 -0400

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/vae/keras_models.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/vae/keras_models.py
@@ -240,7 +240,7 @@ def lstm_vae3(trainData, testData, weights_file=None, batch_size=1024, nb_epoch=
     decoded_h2 = RepeatVector(timesteps, name='h_2')
     decoded_L1 = LSTM(h1_dim, return_sequences=True, name='L_1')
     decoded_L21 = LSTM(input_dim, return_sequences=True, name='L_21')
-    decoded_L22 = LSTM(input_dim, return_sequences=True, activation='tanh', name='L_22')
+    decoded_L22 = LSTM(input_dim, return_sequences=True, activation='softplus', name='L_22')
 
     # Custom loss layer
     class CustomVariationalLayer(Layer):
@@ -249,8 +249,8 @@ def lstm_vae3(trainData, testData, weights_file=None, batch_size=1024, nb_epoch=
             super(CustomVariationalLayer, self).__init__(**kwargs)
 
         def vae_loss(self, x, x_d_mean, x_d_log_var):
-            log_p_x_z = -0.5 * ( K.sum(K.square((x-x_d_mean))/K.exp(x_d_log_var), axis=-1) \
-                                 + float(input_dim) * K.log(2.0*np.pi) + K.sum(x_d_log_var, axis=-1) )
+            log_p_x_z = -0.5 * ( K.sum(K.square((x-x_d_mean))/K.exp(x_d_log_var-1e-10), axis=-1) \
+                                 + float(input_dim) * K.log(2.0*np.pi) + K.sum(x_d_log_var-1e-10, axis=-1) )
             ## xent_loss = K.sum(-log_p_x_z, axis=-1)
             xent_loss = K.mean(-log_p_x_z, axis=-1)
             ## xent_loss = K.mean(K.sum(K.square(x_d_mean - x), axis=-1), axis=-1)
@@ -263,7 +263,7 @@ def lstm_vae3(trainData, testData, weights_file=None, batch_size=1024, nb_epoch=
         def call(self, args):
             x = args[0]
             x_d_mean    = args[1]
-            x_d_log_var = args[2] # relu outputs positives only. So multiply -1
+            x_d_log_var = -args[2] # relu outputs positives only. So multiply -1
             
             loss = self.vae_loss(x, x_d_mean, x_d_log_var)
             self.add_loss(loss, inputs=args)
@@ -309,8 +309,8 @@ def lstm_vae3(trainData, testData, weights_file=None, batch_size=1024, nb_epoch=
             lr = 0.001
         else:
             lr = 0.01
-        optimizer = RMSprop(lr=lr, rho=0.9, epsilon=1e-08, decay=0.0001)
-        #optimizer = Adam(lr=lr)                
+        #optimizer = RMSprop(lr=lr, rho=0.9, epsilon=1e-08, decay=0.0001)
+        optimizer = Adam(lr=lr)                
         vae_autoencoder.compile(optimizer=optimizer, loss=None)
 
         ## vae_autoencoder.load_weights(weights_file)
@@ -325,7 +325,7 @@ def lstm_vae3(trainData, testData, weights_file=None, batch_size=1024, nb_epoch=
             ##         ReduceLROnPlateau(monitor='val_loss', factor=0.2,
             ##                           patience=3, min_lr=0.0001)]
 
-        train_datagen = ku.sigGenerator(augmentation=False, noise_mag=0.03)
+        train_datagen = ku.sigGenerator(augmentation=True, noise_mag=0.03)
         train_generator = train_datagen.flow(x_train, x_train, batch_size=batch_size, seed=3334)
         ## test_datagen = ku.sigGenerator(augmentation=False)
         ## test_generator = test_datagen.flow(x_test, x_test, batch_size=len(x_test),
