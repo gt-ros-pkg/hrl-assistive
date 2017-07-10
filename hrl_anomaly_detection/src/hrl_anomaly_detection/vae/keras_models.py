74bf1d4097db3bc1675fe28b67d05a4234c93ead
pidipidi <deric.park@gatech.edu>
2017-07-10 18:32:45 +0000

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/vae/keras_models.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/vae/keras_models.py
@@ -252,7 +252,8 @@ def lstm_vae3(trainData, testData, weights_file=None, batch_size=1024, nb_epoch=
             log_p_x_z = -0.5 * ( K.sum(K.square((x-x_d_mean))/K.exp(x_d_log_var-1e-10), axis=-1) \
                                  + float(input_dim) * K.log(2.0*np.pi) + K.sum(x_d_log_var-1e-10, axis=-1) )
             ## xent_loss = K.sum(-log_p_x_z, axis=-1)
-            xent_loss = K.mean(-log_p_x_z, axis=-1)
+            ## xent_loss = K.mean(-log_p_x_z, axis=-1)
+            xent_loss = K.max(-log_p_x_z, axis=-1)
             ## xent_loss = K.mean(K.sum(K.square(x_d_mean - x), axis=-1), axis=-1)
             
             kl_loss = - 0.5 * K.sum(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
