71396bcbe7d2aadffa84122c803c03507eee7f45
pidipidi <deric.park@gatech.edu>
2017-08-02 22:02:00 +0000

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/vae/vae_data.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/vae/vae_data.py
@@ -141,7 +141,7 @@ def lstm_test(subject_names, task_name, raw_data_path, processed_data_path, para
     # HMM-induced vector with LOPO
     for idx, (normalTrainIdx, abnormalTrainIdx, normalTestIdx, abnormalTestIdx) \
       in enumerate(d['kFoldList']):
-        if idx != 4: continue
+        if idx != 7: continue
         np.random.shuffle(normalTrainIdx)  
 
         # dim x sample x length
@@ -202,7 +202,7 @@ def lstm_test(subject_names, task_name, raw_data_path, processed_data_path, para
         batch_size  = 32
         fixed_batch_size = True
         noise_mag   = 0.1
-        sam_epoch   = 15
+        sam_epoch   = 15        
 
         if method == 'lstm_vae' or method == 'lstm_vae2' or method == 'lstm_dvae':
             if method == 'lstm_vae':
