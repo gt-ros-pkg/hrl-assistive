5f54c99b38b067213577a666362fdd9f0742ca57
pidipidi <deric.park@gatech.edu>
2017-08-18 01:43:29 -0400

    test param update

--- hrl_anomaly_detection/src/hrl_anomaly_detection/vae/util.py
+++ hrl_anomaly_detection/src/hrl_anomaly_detection/vae/util.py
@@ -195,7 +195,8 @@ def graph_latent_space(normalTestData, abnormalTestData, enc_z, timesteps=1, bat
 
             z_mean=[]
             for j in xrange(len(x[0])-timesteps+1):
-                if method.find('lstm_vae_custom') >=0 or method.find('phase')>=0:
+                if method.find('lstm_vae_custom') >=0 or method.find('lstm_dvae_custom') >=0
+                    or method.find('phase')>=0:
                     x_in = np.concatenate((x[:,j:j+timesteps],
                                            np.zeros((len(x), timesteps,1))), axis=-1)
                 else:
@@ -217,7 +218,8 @@ def graph_latent_space(normalTestData, abnormalTestData, enc_z, timesteps=1, bat
 
             z_mean=[]
             for j in xrange(len(x[0])-timesteps+1):
-                if method.find('lstm_vae_custom')>=0 or method.find('phase')>=0:
+                if method.find('lstm_vae_custom')>=0 or method.find('lstm_vae_custom')>=0
+                    or method.find('phase')>=0:
                     x_in = np.concatenate((x[:,j:j+timesteps],
                                            np.zeros((len(x), timesteps,1))), axis=-1)
                 else:
