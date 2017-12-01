predictor finished, need to test pyaudio stream, must fix scaling

hrl_multimodal_prediction/src/hrl_multimodal_prediction/bag_to_dataset.py
@@ -33,7 +33,7 @@ import os, copy, sys
 import librosa
 ## from hrl_msgs.msg import FloatArray
 ## from std_msgs.msg import Float64
-from hrl_anomaly_detection.msg import audio
+from hrl_multimodal_prediction.msg import audio
 
 # util
 import numpy as np
@@ -41,11 +41,11 @@ import math
 import pyaudio
 import struct
 import array
-try:
-    from features import mfcc
-except:
-    from python_speech_features import mfcc
-from scipy import signal, fftpack, conj, stats
+# try:
+#     from features import mfcc
+# except:
+#     from python_speech_features import mfcc
+# from scipy import signal, fftpack, conj, stats
 
 import scipy.io.wavfile as wav
 import glob

hrl_multimodal_prediction/src/hrl_multimodal_prediction/predict_subscriber.py
@@ -60,8 +60,9 @@ class predict_subscriber():
 		# audio_store = np.hstack(audio_store)
 		# audio_store = np.array(audio_store, dtype=np.float64)
 		mfccs = librosa.feature.mfcc(y=audio, sr=cf.RATE, hop_length=cf.HOP_LENGTH, n_fft=cf.N_FFT, n_mfcc=cf.N_MFCC)
-		mfccs = np.array(mfccs, dtype=np.float64)
-		self.Audio_Buffer = mfccs
+		mfccs = np.array(mfccs, dtype=np.float64) # shape=(n_mfcc,t)
+		mfccs = np.swapaxes(mfccs,0,1) # shape=(t, n_mfcc)
+		self.Audio_Buffer = mfccs 
 		
 		msg = pub_mfcc()
 		msg.mfcc = self.Audio_Buffer.flatten() #Can read from subscribe and reconstruct into 2D

hrl_multimodal_prediction/src/hrl_multimodal_prediction/predictor.py
@@ -13,29 +13,40 @@ import tensorflow as tf
 import numpy.matlib
 import scipy as sp
 import scipy.interpolate
+from sklearn import preprocessing
 
 from keras.models import load_model
 from keras.models import Sequential
 from keras.layers import Dense, Activation, LSTM, Dropout, RepeatVector, TimeDistributed, Input
 from keras.models import Sequential, Model
 from keras import optimizers
+from keras import backend as K
 
 import config as cf
 import pyaudio
-from std_msgs.msg import String, Float64
+from std_msgs.msg import String, Float64, Float64MultiArray, MultiArrayLayout
+from hrl_multimodal_prediction.msg import audio, pub_relpos, pub_mfcc
+from visualization_msgs.msg import Marker
 
-from predict_subscriber import predict_subscriber
 # Predictor and visualizer go hand in hand
 # Predict 10 time steps and save in a variable
 # unlock mutex_viz
 # variable read from visualizer, plot and lock mutex_viz -- wait for predicted value
 # repeat
 class predictor():
-	g_pred_mfcc = None
-	g_pred_relpos = None
-	g_actual_mfcc = None
-	g_actual_relpos = None
+	posUpdate = False
+	mfccUpdate = False
+	relpos = None
+	mfcc = None
+	
+	model = None #LSTM Model
+	graph = None
+	stream = None
 
+	def __init__(self):
+		pya = pyaudio.PyAudio()
+		self.stream = pya.open(format=pyaudio.paFloat32, channels=1, rate=cf.RATE, output=True)
+		
 	def define_network(self, batch_size, time_in, time_out, input_dim, n_neurons):
 		model = Sequential()
 		model.add(LSTM(input_dim*time_out, batch_input_shape=(batch_size, time_in, input_dim),
@@ -43,12 +54,45 @@ class predictor():
 		model.add(LSTM(input_dim*time_out, stateful=True, return_sequences=True, activation='tanh'))
 		model.add(TimeDistributed(Dense(input_dim*time_out, activation='linear')))
 
-		model.load_weights(WEIGHT_FILE)
+		model.load_weights(cf.WEIGHT_FILE)
 		model.compile(loss='mse', optimizer='RMSprop')
 		print model.summary()
 		print "Inputs: {}".format(model.input_shape)
 		print "Outputs: {}".format(model.output_shape)
 		return model
+
+	def reconstruct_mfcc(self, mfccs):
+		#build reconstruction mappings
+		n_mfcc = mfccs.shape[0]
+		n_mel = cf.N_MEL
+		dctm = librosa.filters.dct(n_mfcc, n_mel)
+		n_fft = cf.N_FFT
+		mel_basis = librosa.filters.mel(cf.RATE, n_fft, n_mels=n_mel)
+
+		#Empirical scaling of channels to get ~flat amplitude mapping.
+		bin_scaling = 1.0/np.maximum(0.0005, np.sum(np.dot(mel_basis.T, mel_basis), axis=0))
+		#Reconstruct the approximate STFT squared-magnitude from the MFCCs.
+		recon_stft = bin_scaling[:, np.newaxis] * np.dot(mel_basis.T, self.invlogamplitude(np.dot(dctm.T, mfccs)))
+		#Impose reconstructed magnitude on white noise STFT.
+		excitation = np.random.randn(cf.HOP_LENGTH*cf.TIMESTEP_OUT-1) # this will be constant--based on one msgsize
+		E = librosa.stft(excitation, n_fft=cf.N_FFT)
+		recon = librosa.istft(E/np.abs(E)*np.sqrt(recon_stft))
+		#print recon
+		#print recon.shape
+		# wav.write('reconsturct.wav', cf.RATE, recon)
+		return recon
+
+	def invlogamplitude(self, S):
+	#"""librosa.logamplitude is actually 10_log10, so invert that."""
+		return 10.0**(S/10.0)
+
+	def play_sound_realtime(self, mfcc):
+		mfcc = mfcc.reshape(cf.MFCC_DIM, cf.TIMESTEP_OUT)
+		recon = self.reconstruct_mfcc(mfcc)
+		self.stream.write(recon)
+		# stream.stop_stream()
+		# stream.close()
+		# pya.terminate()
 
 	def rescale(self, dataset):
 		# rescale values to -1, 1 for tanh
@@ -57,96 +101,85 @@ class predictor():
 		dim2 = dataset.shape[1]
 		dim3 = dataset.shape[2]
 		dataset = dataset.reshape(dim1*dim2, dim3)
-		dataset[:,0:3] = scaler.fit_transform(dataset[:,0:3])
+		dataset[:,:] = scaler.fit_transform(dataset[:,:])
 		dataset = dataset.reshape(dim1, dim2, dim3)
 		return dataset, scaler
 
-	def scale_back(self, pred, mfcc_scaler, relpos_scaler):
-		g_pred_mfcc = mfcc_scaler.inverse_transform(pred[0:3])
-		g_pred_relpos = relpos_scaler.inverse_transform(pred[3:6])
-		print 'saving values scaled back'
-
-	def preprocess(self, mfcc, relpos):
-		#interpolate so that mfcc and relpos have the same length?
-		mfcc, mfcc_scaler = rescale(mfcc)
-		relpos, relpos_scaler = rescale(relpos)
-		comb_data = np.concatenate((mfccs, relpos), axis=2)
-		return comb_data, mfcc_scaler, relpos_scaler
-
-	def predict(self, model, comb_data):
-		#some reshaping before prediction?
-		for i in range(0, comb_data.shape[0]):
-			p_tmp = model.predict_on_batch(comb_data[i]) 
-			rst.append(p_tmp)
-		rst = np.array(rst)
-		return rst
+	def scale_back(self, pred, mfcc_scaler, relpos_scaler):		
+		dim1 = pred.shape[0]
+		dim2 = pred.shape[1]
+		dim3 = pred.shape[2]
+		pred = pred.reshape(dim1*dim2, dim3)
+		mfcc = mfcc_scaler.inverse_transform(pred[:,0:3])
+		mfcc = mfcc.reshape(1, cf.TIMESTEP_OUT, cf.MFCC_DIM)
+		relpos = relpos_scaler.inverse_transform(pred[:,3:6])
+		relpos = relpos.reshape(1, cf.TIMESTEP_OUT, cf.IMAGE_DIM)
+		return mfcc, relpos
 	
-	def reconstruct_mfcc(self, mfccs):
-		#build reconstruction mappings
-		n_mfcc = mfccs.shape[0]
-		n_mel = cf.N_MEL
-		dctm = librosa.filters.dct(n_mfcc, n_mel)
-		n_fft = cf.N_FFT
-		mel_basis = librosa.filters.mel(self.RATE, n_fft, n_mels=n_mel)
+	def callback(self, data):
+		if data._type == 'hrl_multimodal_prediction/pub_mfcc':
+			self.mfcc = data.mfcc
+			self.mfccUpdate = True
+		elif data._type == 'hrl_multimodal_prediction/pub_relpos':
+			self.relpos = data.relpos
+			self.posUpdate = True
 
-		#Empirical scaling of channels to get ~flat amplitude mapping.
-		bin_scaling = 1.0/np.maximum(0.0005, np.sum(np.dot(mel_basis.T, mel_basis), axis=0))
-		#Reconstruct the approximate STFT squared-magnitude from the MFCCs.
-		recon_stft = bin_scaling[:, np.newaxis] * np.dot(mel_basis.T, self.invlogamplitude(np.dot(dctm.T, mfccs)))
-		#Impose reconstructed magnitude on white noise STFT.
-		excitation = np.random.randn(y.shape[0]) # this will be constant--based on one msgsize
-		E = librosa.stft(excitation, n_fft=cf.N_FFT)
-		recon = librosa.istft(E/np.abs(E)*np.sqrt(recon_stft))
-		#print recon
-		#print recon.shape
-		wav.write('reconsturct.wav', cf.RATE, recon)
-		return recon
+		if self.mfccUpdate and self.posUpdate:
+			self.posUpdate = False
+			self.mfccUpdate = False
 
-	def invlogamplitude(self, S):
-	#"""librosa.logamplitude is actually 10_log10, so invert that."""
-		return 10.0**(S/10.0)
+			# Convert shape to fit LSTM
+			self.mfcc = np.array(self.mfcc).reshape(1, cf.P_MFCC_TIMESTEP, cf.N_MFCC) # shape=(t, n_mfcc)
+			self.relpos = np.array(self.relpos).reshape(1, 1, cf.IMAGE_DIM) 
+			print self.mfcc.shape, self.relpos.shape
+			tmp = self.relpos
+			for i in range(cf.P_MFCC_TIMESTEP-1): #make position data match mfcc timestep
+				self.relpos = np.concatenate((self.relpos,tmp), axis=1)
+			print self.mfcc.shape, self.relpos.shape
 
-	def play_sound_realtime(self, mfcc):
-		recon = reconstruct_mfcc(mfcc)
-		pya = pyaudio.PyAudio()
-		stream = pya.open(format=pyaudio.paFloat32, channels=1, rate=cf.RATE, output=True)
-		stream.write(recon)
-		stream.stop_stream()
-		stream.close()
-		pya.terminate()
+			# Rescale perFeature, save Scaler
+			norm_mfcc, mfcc_scaler = self.rescale(self.mfcc)
+			norm_relpos, relpos_scaler = self.rescale(self.relpos)
 
-	def m_callback(self, data):
-		print data
+			# Combine two modes 
+			comb_data = np.concatenate((norm_mfcc, norm_relpos), axis=2)
+			print comb_data.shape
+			comb_data = np.array(comb_data)
 
-	def a_callback(self, data):
-		print data
+			# Predic -- graph default must to fix multithread bug in keras-tensorflow
+			with self.graph.as_default():
+				for i in range(0, cf.P_MFCC_TIMESTEP):
+					pred = self.model.predict_on_batch(comb_data[:,i:i+1,:])
+			#Now pred has the last value
+			pred = pred.reshape(1, cf.TIMESTEP_OUT, cf.INPUT_DIM)
+			print pred.shape
+
+			# # Scaleback --only the last one to play in realtime
+			#**********************************************************************
+			# SCALE - MUST BE SAVED FROM THE TRAINING DATA, SCALED BACK -- SCALE of TOTAL DATA
+			#**********************************************************************
+			sb_mfcc, sb_relpos = self.scale_back(pred, mfcc_scaler, relpos_scaler) 
+			print sb_mfcc.shape, sb_relpos.shape
+
+			# Play 
+			self.play_sound_realtime(sb_mfcc)
+
+			# Publish for plot
+
 
 	def run(self):
-		p = predict_subscriber()
-		while True:
-			print p.Audio_Buffer
-			print p.RelPos_Buffer
-
-		# rospy.init_node('predictor', anonymous=True)
-		# rospy.Subscriber('processed_audio', Float64, self.a_callback)
-		# rospy.Subscriber('processed_relpos', Float64, self.m_callback)
-		# rospy.spin()
-
-		# r = rospy.Rate(8) # 8hz		
+		rospy.init_node('predictor', anonymous=True)
 		# while not rospy.is_shutdown():
-		# 	mfcc = psub.Audio_buffer().pop() # takes the last element
-		# 	relpos = psub.RelPos_buffer().pop()
-		# 	g_actual_mfcc = mfcc_scaler
-		# 	g_actual_relpos = relpos
-		# 	comb_data, mfcc_scaler, relpos_scaler = preprocess(mfcc, relpos)
-		# 	model = define_network(cf.PRED_BATCH_SIZE, cf.TIMESTEP_IN, cf.TIMESTEP_OUT, cf.INPUT_DIM, cf.N_NEURONS)
-		# 	pred = predict(model, comb_data)
-		# 	scale_back(pred, mfcc_scaler, relpos_scaler)
-		# 	r.sleep()
-
+		rospy.Subscriber('preprocessed_audio', pub_mfcc, self.callback)
+		rospy.Subscriber('preprocessed_relpos', pub_relpos, self.callback)
+		rospy.spin()
+		
 def main():
-	pred = predictor()
-	pred.run()
+	p = predictor()
+	p.graph = tf.get_default_graph()
+	p.model = p.define_network(cf.PRED_BATCH_SIZE, cf.TIMESTEP_IN, cf.TIMESTEP_OUT, cf.INPUT_DIM, cf.N_NEURONS)
+	p.run()
+
 
 if __name__ == '__main__':
 	main()    

hrl_multimodal_prediction/src/hrl_multimodal_prediction/train.py
@@ -44,7 +44,6 @@ import gc
 import librosa
 import os, copy, sys
 import tensorflow as tf
-from tqdm import tqdm
 import numpy.matlib
 import scipy.io.wavfile as wav
 import scipy as sp
@@ -233,7 +232,7 @@ def main():
     print(y_train.shape)
     print(X_test.shape)
     print(y_test.shape)   
-
+    
     np.random.seed(3334)
     print('creating model...')
     lstm_model = define_network(BATCH_SIZE, TIMESTEP_IN, TIMESTEP_OUT, INPUT_DIM, N_NEURONS, False)