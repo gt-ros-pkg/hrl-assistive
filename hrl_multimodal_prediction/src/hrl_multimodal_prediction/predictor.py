all tools ready, need to tune model. Potentially might have to look at scaling bug across different phases, interpolating position during prediction, visualizer multi timestep plot

hrl_multimodal_prediction/src/hrl_multimodal_prediction/predictor.py
@@ -38,18 +38,26 @@ class predictor():
 	mfccUpdate = False
 	relpos = None
 	mfcc = None
-	
+	timestamp_mfcc = None
+	timestamp_relpos = None
+
 	model = None #LSTM Model
 	graph = None
 	stream = None
-	plot_pub = None
+	plot_publisher = None
 
 	def __init__(self):
+		print 'initiating...'
 		# pya = pyaudio.PyAudio()
 		# self.stream = pya.open(format=pyaudio.paFloat32, channels=1, rate=cf.RATE, output=True)		
-		
+
+		# audio and image minmax of training data
+		mm = np.load(cf.PROCESSED_DATA_PATH + 'combined_train_minmax.npy')		
+		self.a_min, self.a_max, self.i_min, self.i_max = mm[0], mm[1], mm[2], mm[3]
+		# print self.a_min, self.a_max, self.i_min, self.i_max
+
 		# must publish original and predicted for visualizer
-		# self.plot_pub = rospy.Publisher('plot_pub', plot_pub, queue_size=10)
+		self.plot_publisher = rospy.Publisher('orig_pred_plotData', plot_pub, queue_size=10)
 
 	def define_network(self, batch_size, time_in, time_out, input_dim, n_neurons):
 		model = Sequential()
@@ -97,60 +105,54 @@ class predictor():
 		# stream.stop_stream()
 		# stream.close()
 		# pya.terminate()
-
-	def rescale(self, dataset):
-		# rescale values to -1, 1 for tanh
-		scaler = preprocessing.MinMaxScaler(feature_range=(-1,1))
-		dim1 = dataset.shape[0]
-		dim2 = dataset.shape[1]
-		dim3 = dataset.shape[2]
-		dataset = dataset.reshape(dim1*dim2, dim3)
-		dataset[:,:] = scaler.fit_transform(dataset[:,:])
-		dataset = dataset.reshape(dim1, dim2, dim3)
-		return dataset, scaler
-
-	def scale_back(self, pred, mfcc_scaler, relpos_scaler):		
-		dim1 = pred.shape[0]
-		dim2 = pred.shape[1]
-		dim3 = pred.shape[2]
-		pred = pred.reshape(dim1*dim2, dim3)
-		mfcc = mfcc_scaler.inverse_transform(pred[:,0:3])
-		mfcc = mfcc.reshape(1, cf.TIMESTEP_OUT, cf.MFCC_DIM)
-		relpos = relpos_scaler.inverse_transform(pred[:,3:6])
-		relpos = relpos.reshape(1, cf.TIMESTEP_OUT, cf.IMAGE_DIM)
-		return mfcc, relpos
 	
+	def normalize(self, y, min_y, max_y):
+	    y = (y - min_y) / (max_y - min_y)
+	    return y
+
+	def scale_back(self, seq, min_y, max_y):
+	    # scale back 
+	    seq = seq * (max_y - min_y) + min_y
+	    return seq
+
 	def callback(self, data):
 		if data._type == 'hrl_multimodal_prediction/pub_mfcc':
 			self.mfcc = data.mfcc
+			self.timestamp_mfcc = data.header.stamp
 			self.mfccUpdate = True
 		elif data._type == 'hrl_multimodal_prediction/pub_relpos':
 			self.relpos = data.relpos
+			self.timestamp_relpos = data.header.stamp
 			self.posUpdate = True
 
 		if self.mfccUpdate and self.posUpdate:
 			self.posUpdate = False
 			self.mfccUpdate = False
 
+			# Timestamp for combined msg to be published
+			loc_timestamp_mfcc = self.timestamp_mfcc 
+			loc_timestamp_relpos = self.timestamp_relpos
+
 			# Convert shape to fit LSTM
-			self.mfcc = np.array(self.mfcc).reshape(1, cf.P_MFCC_TIMESTEP, cf.N_MFCC) # shape=(t, n_mfcc)
-			self.relpos = np.array(self.relpos).reshape(1, 1, cf.IMAGE_DIM) 
+			orig_mfcc = np.array(self.mfcc).reshape(1, cf.P_MFCC_TIMESTEP, cf.N_MFCC) # shape=(t, n_mfcc)
+			orig_relpos = np.array(self.relpos).reshape(1, 1, cf.IMAGE_DIM) 			
+
 			# print self.mfcc.shape, self.relpos.shape
-			tmp = self.relpos
-			for i in range(cf.P_MFCC_TIMESTEP-1): #make position data match mfcc timestep
-				self.relpos = np.concatenate((self.relpos,tmp), axis=1)
+			tmp = orig_relpos
+			for i in range(cf.P_MFCC_TIMESTEP-1): #make position data match mfcc timestep aka interpolating
+				orig_relpos = np.concatenate((orig_relpos,tmp), axis=1)
 			# print self.mfcc.shape, self.relpos.shape
 
 			# Rescale perFeature, save Scaler
-			norm_mfcc, mfcc_scaler = self.rescale(self.mfcc)
-			norm_relpos, relpos_scaler = self.rescale(self.relpos)
+			norm_mfcc = self.normalize(orig_mfcc, self.a_min, self.a_max)
+			norm_relpos = self.normalize(orig_relpos, self.i_min, self.a_max)
 
 			# Combine two modes 
 			comb_data = np.concatenate((norm_mfcc, norm_relpos), axis=2)
 			# print comb_data.shape
 			comb_data = np.array(comb_data)
 
-			# Predic -- graph default must to fix multithread bug in keras-tensorflow
+			# Predict -- graph default must to fix multithread bug in keras-tensorflow
 			with self.graph.as_default():
 				for i in range(0, cf.P_MFCC_TIMESTEP):
 					pred = self.model.predict_on_batch(comb_data[:,i:i+1,:])
@@ -162,22 +164,30 @@ class predictor():
 			#**********************************************************************
 			# SCALE - MUST BE SAVED FROM THE TRAINING DATA, SCALED BACK -- SCALE of TOTAL DATA
 			#**********************************************************************
-			sb_mfcc, sb_relpos = self.scale_back(pred, mfcc_scaler, relpos_scaler) 
+			sb_mfcc = self.scale_back(pred[:,:,0:3], self.a_min, self.a_max)
+			sb_relpos = self.scale_back(pred[:,:,3:6], self.i_min, self.a_max) 
 			# print sb_mfcc.shape, sb_relpos.shape
 
 			# Play -- Not detecting sound device, Use a Latop for this
 			# self.play_sound_realtime(sb_mfcc)
 
 			# Publish for plot
-			# processed data time stamp has delay for processing time
-			# when plotting time for prediction will be added to the processing time as plotting latency
-			print self.mfcc.shape, self.relpos.shape, sb_mfcc.shape, sb_relpos.shape
-			# msg = plot_pub()
-			# msg.orig_mfcc =
-			# msg.pred_mfcc
-			# msg.orig_relpos=
-			# msg.pred_relpos = 
-
+			# Getting the last time step of orig and pred and flattening for msg
+			sb_mfcc = np.array(sb_mfcc).reshape(cf.TIMESTEP_OUT*cf.MFCC_DIM)
+			sb_relpos = np.array(sb_relpos).reshape(cf.TIMESTEP_OUT*cf.IMAGE_DIM)
+			orig_mfcc = np.array(orig_mfcc[:,cf.P_MFCC_TIMESTEP-1,:]).reshape(cf.MFCC_DIM)
+			orig_relpos = np.array(orig_relpos[:,cf.P_MFCC_TIMESTEP-1,:]).reshape(cf.IMAGE_DIM)
+			# print orig_mfcc.shape, orig_relpos.shape, sb_mfcc.shape, sb_relpos.shape
+			
+			#received messages need to be reshaped in visualizer
+			msg = plot_pub()
+			msg.header.stamp.secs = (loc_timestamp_relpos.secs + loc_timestamp_mfcc.secs)/2
+			msg.header.stamp.nsecs = (loc_timestamp_relpos.nsecs + loc_timestamp_mfcc.nsecs)/2
+			msg.orig_mfcc = orig_mfcc
+			msg.orig_relpos = orig_relpos
+			msg.pred_mfcc = sb_mfcc
+			msg.pred_relpos = sb_relpos
+			self.plot_publisher.publish(msg)
 
 	def run(self):
 		rospy.init_node('predictor', anonymous=True)

hrl_multimodal_prediction/src/hrl_multimodal_prediction/preprocess.py
@@ -50,62 +50,27 @@ from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 from sklearn.utils import shuffle
 import gc
+import config as cf
 
-################################################
-# Trick from 1D to 2D is flattening!!!!!
-# First Check the base case 2-10 which worked well before
-# Then Convert it into flattened version
-################################################
 
-#Use these config only in main
-IMAGE_DIM = 3 #image dimension
-MFCC_DIM = 3 #audio dimension
-INPUT_DIM = MFCC_DIM + IMAGE_DIM #total dimension in LSTM
-TIMESTEP_IN = 1
-TIMESTEP_OUT = 10
-N_NEURONS = TIMESTEP_OUT
-
-BATCH_SIZE = 256
-# NUM_BATCH = 300 #Total #samples = Num_batch x Batch_size
-NB_EPOCH = 500
-PRED_BATCH_SIZE = 1
-
-WEIGHT_FILE = './weights/stateful-tanh-denoise-2stack-Tdense-2D-lab.h5'
-PLOT = True
-DENSE = True #True if TimeDistributedDense layer is used
-
-PROCESSED_DATA_PATH = './processed_data/'
-
-# def add_noise(X):
-# 	print 'add_noise to X'
-# 	print X.shape
-# 	batch_size = BATCH_SIZE #32,64,128,256,512,1024,2048
-# 	n = batch_size/16 
-# 	for j in range(NUM_BATCH): 
-# 		for i in xrange(batch_size):
-# 			X[i,:,:,:] = X[i,:,:,:] + np.random.normal(0.0, 0.0025, (X.shape[1], X.shape[2], X.shape[3]) ) 
-
-# 	# for j in range(NUM_BATCH): 
-# 	# 	for i in range(batch_size):
-# 	# 	  pyplot.plot(X[i,:,:,0])
-# 	# pyplot.show()
-# 	return X
-
-def rescale(dataset):
-	# rescale values to -1, 1 for tanh
-	scaler = preprocessing.MinMaxScaler(feature_range=(-1,1))
-	dim1 = dataset.shape[0]
-	dim2 = dataset.shape[1]
-	dim3 = dataset.shape[2]
-	dataset = dataset.reshape(dim1*dim2, dim3)
-	dataset[:,0:3] = scaler.fit_transform(dataset[:,0:3])
-	dataset[:,3:6] = scaler.fit_transform(dataset[:,3:6])
-	dataset = dataset.reshape(dim1, dim2, dim3)
-	return dataset
+def normalize(y, min_y, max_y):
+    y = (y - min_y) / (max_y - min_y)
+    return y
 
 def data_generator(dataset):
 	dataset = augment_data(dataset)
-	dataset = rescale(dataset)
+	# print dataset.shape
+
+	#normalize
+	mm = np.load(cf.PROCESSED_DATA_PATH + 'combined_train_minmax.npy')		
+	a_min, a_max, i_min, i_max = mm[0], mm[1], mm[2], mm[3]
+	a_data = dataset[:,:,0:3]
+	i_data = dataset[:,:,3:6]
+	a_data = normalize(a_data, a_min, a_max)
+	i_data = normalize(i_data, i_min, i_max)
+	dataset = np.concatenate((a_data, i_data), axis=2)
+	# print dataset.shape
+
 	X, y = format_data(dataset) # X.shape=(Batch=256,10,2), y.shape=(256,10,2)
 	return X,y #(num_window, batch, window_size, dim)
 
@@ -132,11 +97,11 @@ def augment_data(dataset):
 	# print more_data.shape
 
 	more_data = dataset
-	while more_data.shape[0] < BATCH_SIZE - dataset.shape[0]:
+	while more_data.shape[0] < cf.BATCH_SIZE - dataset.shape[0]:
 		more_data = np.concatenate((more_data, dataset), axis=0)
 	print more_data.shape
 
-	diff = BATCH_SIZE - more_data.shape[0]
+	diff = cf.BATCH_SIZE - more_data.shape[0]
 	more_data = np.concatenate((more_data, dataset[0:diff,:,:]), axis=0)
 	print more_data.shape
 
@@ -145,9 +110,9 @@ def augment_data(dataset):
 
 def format_data(dataset): #dataset.shape=(batchsize=256, datapoints=100, dim=2)
 	X, y = [], []
-	for i in range(dataset.shape[1] - TIMESTEP_IN - TIMESTEP_OUT + 1):
-		x_f = dataset[:, i:i+TIMESTEP_IN, :] 
-		y_f = dataset[:, i+TIMESTEP_IN:i+TIMESTEP_IN+TIMESTEP_OUT, :]
+	for i in range(dataset.shape[1] - cf.TIMESTEP_IN - cf.TIMESTEP_OUT + 1):
+		x_f = dataset[:, i:i+cf.TIMESTEP_IN, :] 
+		y_f = dataset[:, i+cf.TIMESTEP_IN : i+cf.TIMESTEP_IN+cf.TIMESTEP_OUT, :]
 		X.append(x_f)
 		y.append(y_f)
 	X = np.array(X)
@@ -161,7 +126,7 @@ def main():
 	dataset.shape:: (num_window, batch x N, window_size, dim)
 	'''
 	# Read and plot and check for all 43 data
-	inputFile = PROCESSED_DATA_PATH + 'combined_train.npy'
+	inputFile = cf.PROCESSED_DATA_PATH + 'combined_train.npy'
 	#Load up the training data
 	print ('Loading raw data read from rosbag')
 	dataset = np.load(inputFile)
@@ -173,8 +138,8 @@ def main():
 	# If the data overfits and doesn't work than comeback and either collect more data or augemnt well
 	# Skip for now
 	X, y = data_generator(dataset)
-	X = X.reshape(X.shape[0],X.shape[1],X.shape[2],INPUT_DIM)
-	y = y.reshape(y.shape[0],y.shape[1],y.shape[2],INPUT_DIM) 
+	X = X.reshape(X.shape[0],X.shape[1],X.shape[2],cf.INPUT_DIM)
+	y = y.reshape(y.shape[0],y.shape[1],y.shape[2],cf.INPUT_DIM) 
 	X = np.swapaxes(X, 0, 1)
 	y = np.swapaxes(y, 0, 1)
 	print 'in main'
@@ -185,7 +150,7 @@ def main():
 	# X = add_noise(X)
 
 	# flatten data to shape into lstm
-	if DENSE:
+	if cf.DENSE:
 		y = y.reshape(y.shape[0], y.shape[1], 1, y.shape[2]*y.shape[3])
 	else:
 		y = y.reshape(y.shape[0], y.shape[1], y.shape[2]*y.shape[3])
@@ -217,10 +182,10 @@ def main():
 	pyplot.plot(X_test[0,:,:,5])
 	pyplot.show()
 
-	np.save(PROCESSED_DATA_PATH + 'X_train', X_train)
-	np.save(PROCESSED_DATA_PATH + 'X_test', X_test)
-	np.save(PROCESSED_DATA_PATH + 'y_train', y_train)
-	np.save(PROCESSED_DATA_PATH + 'y_test', y_test)
+	np.save(cf.PROCESSED_DATA_PATH + 'X_train', X_train)
+	np.save(cf.PROCESSED_DATA_PATH + 'X_test', X_test)
+	np.save(cf.PROCESSED_DATA_PATH + 'y_train', y_train)
+	np.save(cf.PROCESSED_DATA_PATH + 'y_test', y_test)
 
 if __name__ == "__main__":
 	main()

hrl_multimodal_prediction/src/hrl_multimodal_prediction/train.py
@@ -58,24 +58,7 @@ from keras import optimizers
 from keras.callbacks import EarlyStopping, CSVLogger, ModelCheckpoint
 from keras import backend as K
 
-#Use these config only in main
-IMAGE_DIM = 3 #image dimension
-MFCC_DIM = 3 #audio dimension
-INPUT_DIM = MFCC_DIM + IMAGE_DIM #total dimension in LSTM
-TIMESTEP_IN = 1
-TIMESTEP_OUT = 10
-N_NEURONS = TIMESTEP_OUT
-
-BATCH_SIZE = 256
-NUM_BATCH = 100
-NB_EPOCH = 500
-PRED_BATCH_SIZE = 1
-
-WEIGHT_FILE = './weights/real_data.h5'
-PLOT = True
-DENSE = True #True if TimeDistributedDense layer is used
-
-PROCESSED_DATA_PATH = './processed_data/'
+import config as cf
 
 def define_network(batch_size, time_in, time_out, input_dim, n_neurons, load_weight=False):
     model = Sequential()
@@ -131,10 +114,10 @@ def fit_lstm(model, x_train, x_test, y_train, y_test):
     pyplot.plot(y[0,:,:,5])
     pyplot.show()
 
-    for epoch in range(NB_EPOCH):
+    for epoch in range(cf.NB_EPOCH):
         #train
         mean_tr_loss = []
-        for i in range(0,x_train.shape[0]*NUM_BATCH*2, BATCH_SIZE): #x_train.shape=BATCH_SIZE
+        for i in range(0,x_train.shape[0]*cf.NUM_BATCH*2, cf.BATCH_SIZE): #x_train.shape=BATCH_SIZE
             #per window
             seq_tr_loss = []
             # x = x_train[i:i+BATCH_SIZE]
@@ -159,7 +142,7 @@ def fit_lstm(model, x_train, x_test, y_train, y_test):
         # This loop is for taking a batch from a large test data
         # Currently just using same data
         mean_te_loss = []
-        for i in xrange(0, x_test.shape[0]*NUM_BATCH, BATCH_SIZE):
+        for i in xrange(0, x_test.shape[0]*cf.NUM_BATCH, cf.BATCH_SIZE):
             seq_te_loss = []
             # x = x_test[i:i+BATCH_SIZE]
             # y = y_test[i:i+BATCH_SIZE]
@@ -184,7 +167,7 @@ def fit_lstm(model, x_train, x_test, y_train, y_test):
             wait         = 0
             plateau_wait = 0
             print 'saving model'
-            model.save_weights(WEIGHT_FILE) 
+            model.save_weights(cf.WEIGHT_FILE) 
         else:
             if wait > patience:
                 print "Over patience!"
@@ -223,10 +206,10 @@ def main():
     dataset.shape:: (num_window, batch x N, window_size, dim)
     '''
     print ('Loading training data')
-    X_train = np.load(PROCESSED_DATA_PATH + 'X_train.npy')
-    y_train = np.load(PROCESSED_DATA_PATH + 'y_train.npy')
-    X_test = np.load(PROCESSED_DATA_PATH + 'X_test.npy')
-    y_test = np.load(PROCESSED_DATA_PATH + 'y_test.npy')
+    X_train = np.load(cf.PROCESSED_DATA_PATH + 'X_train.npy')
+    y_train = np.load(cf.PROCESSED_DATA_PATH + 'y_train.npy')
+    X_test = np.load(cf.PROCESSED_DATA_PATH + 'X_test.npy')
+    y_test = np.load(cf.PROCESSED_DATA_PATH + 'y_test.npy')
     print ('Finished loading training data')
     print(X_train.shape)
     print(y_train.shape)
@@ -235,7 +218,7 @@ def main():
     
     np.random.seed(3334)
     print('creating model...')
-    lstm_model = define_network(BATCH_SIZE, TIMESTEP_IN, TIMESTEP_OUT, INPUT_DIM, N_NEURONS, False)
+    lstm_model = define_network(cf.BATCH_SIZE, cf.TIMESTEP_IN, cf.TIMESTEP_OUT, cf.INPUT_DIM, cf.N_NEURONS, False)
     print('training model...')
     lstm_model = fit_lstm(lstm_model, X_train, X_test, y_train, y_test)
 