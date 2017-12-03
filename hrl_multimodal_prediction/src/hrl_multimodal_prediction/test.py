4c4f1d5f671468771541bfe8f7881b2acd7b5fc9
Michael Park <michael.j.park@gatech.edu>
2017-12-03 03:23:01 -0500

    pyAudio stream works (in test.py), not playing sounds in full length..why?

--- hrl_multimodal_prediction/src/hrl_multimodal_prediction/test.py
+++ hrl_multimodal_prediction/src/hrl_multimodal_prediction/test.py
@@ -16,21 +16,21 @@ import scipy.interpolate
 import rospy
 from std_msgs.msg import String
 
-def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
-        rate.sleep()
+# def talker():
+#     pub = rospy.Publisher('chatter', String, queue_size=10)
+#     rospy.init_node('talker', anonymous=True)
+#     rate = rospy.Rate(10) # 10hz
+#     while not rospy.is_shutdown():
+#         hello_str = "hello world %s" % rospy.get_time()
+#         rospy.loginfo(hello_str)
+#         pub.publish(hello_str)
+#         rate.sleep()
 
-if __name__ == '__main__':
-    try:
-        talker()
-    except rospy.ROSInterruptException:
-        pass
+# if __name__ == '__main__':
+#     try:
+#         talker()
+#     except rospy.ROSInterruptException:
+#         pass
 
 
 # Sent for figure
@@ -60,16 +60,46 @@ if __name__ == '__main__':
 # 	plt.show()
 
 
-# 	y, sr = librosa.load('DO2.wav', mono=True)	
-# 	librosa.output.write_wav('aa.wav', y, sr)
-# 	print y.shape, y.max(), y.min(), type(y[0])
-# 	pya = pyaudio.PyAudio()
-# 	stream = pya.open(format=pyaudio.paFloat32, channels=1, rate=44100/2, output=True)
-# 	stream.write(y)
-# 	stream.stop_stream()
-# 	stream.close()
-# 	pya.terminate()
-	# print("* Preview completed!")
+y, sr = librosa.load('./bagfiles/unpacked/data1.wav', mono=True)	
+print y.shape, y.max(), y.min(), type(y[0])
+p = pyaudio.PyAudio()
+# deviceIndex = self.find_input_device()
+device_index = None
+for i in range(p.get_device_count()):
+    devinfo = p.get_device_info_by_index(i)
+    print('Device %d: %s'%(i, devinfo['name']))
+
+    for keyword in ['HDA', 'Intel', 'PCH', 'ALC662']:
+        if keyword in devinfo['name'].lower():
+            print('Found an output: device %d - %s'%(i, devinfo['name']))
+            device_index = i
+
+if device_index is None:
+    print('No preferred input found; using default input device.')
+
+
+deviceIndex = 0
+devInfo = p.get_device_info_by_index(deviceIndex)
+print 'Audio device:', deviceIndex
+print 'Sample rate:', devInfo['defaultSampleRate']
+print 'Max input channels:',  devInfo['maxInputChannels']
+
+stream = p.open(format=p.get_format_from_width(wave.getsampwidth()), channels=1, rate=44100/2, output=True, input_device_index=deviceIndex)
+stream.write(y)
+stream.close()
+p.terminate()
+print("* Preview completed!")
+
+## self.stream.start_stream()
+
+
+
+# stream = pya.open(format=pyaudio.paFloat32, channels=1, rate=44100/2, output=True)
+# stream.write(y)
+# stream.stop_stream()
+# stream.close()
+# pya.terminate()
+# print("* Preview completed!")
 
 # if __name__ == '__main__':
 # 	main()    
