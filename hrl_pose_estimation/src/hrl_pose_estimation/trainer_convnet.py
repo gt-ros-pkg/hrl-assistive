1aae6f43a79f3ee52f3beaa305ed4b5a3332b434
Henry M. Clever <henryclever@gmail.com>
2018-01-30 00:43:35 -0500

    minor

--- hrl_pose_estimation/src/hrl_pose_estimation/trainer_convnet.py
+++ hrl_pose_estimation/src/hrl_pose_estimation/trainer_convnet.py
@@ -93,7 +93,7 @@ class PhysicalTrainer():
         print test_file
         #Entire pressure dataset with coordinates in world frame
 
-        self.save_name = '_2to8_angles_5x5_dropout1_' + str(self.batch_size) + 'b_' + str(self.num_epochs) + 'e_4'
+        self.save_name = '_2to8_angles_5x5_dropout1_2_' + str(self.batch_size) + 'b_' + str(self.num_epochs) + 'e_4'
         self.loss_vector_type = 'angles'  # 'arms_cascade'#'upper_angles' #this is so you train the set to joint lengths and angles
 
         #we'll be loading this later
