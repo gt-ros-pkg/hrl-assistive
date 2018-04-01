789228129b8769fb38dce6af9c93c4d87f3e0137
Ari Kapusta <akapusta@gatech.edu>
2018-04-01 14:52:06 -0400

    controller approaching working state

---
+++ hrl_dressing/scripts/change_gains_pr2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+
+rosrun pr2_controller_manager pr2_controller_manager stop r_arm_controller
+rosrun pr2_controller_manager pr2_controller_manager stop l_arm_controller
+rosrun pr2_controller_manager pr2_controller_manager unload r_arm_controller
+rosrun pr2_controller_manager pr2_controller_manager unload l_arm_controller
+
+rosparam load pr2_arm_controllers_grasp.yaml
+
+rosrun pr2_controller_manager pr2_controller_manager load r_arm_controller
+rosrun pr2_controller_manager pr2_controller_manager load l_arm_controller
+
+echo "Hit any key to start the controllers"
+read inp
+rosrun pr2_controller_manager pr2_controller_manager start r_arm_controller
+rosrun pr2_controller_manager pr2_controller_manager start l_arm_controller
---
+++ hrl_dressing/scripts/kill_dressing.sh
@@ -0,0 +1,8 @@
+echo "Killing nodes started by base selection"
+rosnode kill /ft8
+rosnode kill /ft9
+rosnode kill /capacitive_sensor
+rosnode kill /toorad_dressing_pr2
+echo "Nodes killed"
+echo "Nodes killed"
+
